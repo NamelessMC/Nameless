05936b1e8433f9ad528d3b0a1ffeb335045d6f57
Acharige <acharige@cubition.net>
2015-11-08 17:17:24 -0500

    Spice up the 404.php page!
    Enhanced the 404 page :).

--- 404.php
+++ 404.php
@@ -18,22 +18,25 @@ header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="/core/assets/favicon.ico">
+    <link rel="icon" href="assets/favicon.ico">
 
-    <title>404</title>
+    <title>404-Page Not Found</title>
 	
-	<link href="/core/assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/assets/css/1.css" rel="stylesheet">
+	<?php require('inc/templates/header.php'); ?>
+	<?php require('inc/templates/navbar.php'); ?>
 
   </head>
   <body>
     <br /><br /><br />
     <div class="container">
       <div class="jumbotron">
-	    <h1>404</h1>
-		<h4>Sorry, we couldn't find that page.</h4>
+	<center><img src="http://kootenaycaninelovers.weebly.com/uploads/3/4/3/1/3431427/8458915.jpg?433"/>
+	    <h1>Sorry dude,</h1>
+		<h4>The page that you're looking doesn't exist. Seeya around though my brutha!</h4>
 		<div class="btn-group" role="group" aria-label="...">
-		  <a href="#" class="btn btn-primary btn-lg" onclick="window.history.back()">Go back</a>
-		  <a href="/" class="btn btn-success btn-lg">Homepage</a>
+		  <a href="#" class="btn btn-primary btn-lg" onclick="window.history.back()">Go back</a>&nbsp
+		  <a href="/" class="btn btn-success btn-lg">Homepage</a></center>
 	    </div>
 	  </div>
 	</div>
