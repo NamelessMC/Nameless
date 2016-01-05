15e97e814af56f17d88cea3aaabb9fa383dd49ed
samerton <timmy437@hotmail.co.uk>
2016-01-05 16:23:52 +0000

    Add "remember me" check
    Check to see if the user has checked "remember me", and if so, log them
    in.

--- core/init.php
+++ core/init.php
@@ -70,6 +70,17 @@ if($page !== 'install'){
 	}
 }
 
+// Process if user has checked "remember me"
+if(Cookie::exists(Config::get('remember/cookie_name')) && !Session::exists(Config::get('session/session_name'))) {
+	$hash = Cookie::get(Config::get('remember/cookie_name'));
+	$hashCheck = DB::getInstance()->get('users_session', array('hash', '=', $hash));
+	
+	if($hashCheck->count()){
+		$user = new User($hashCheck->first()->user_id);
+		$user->login();
+	}
+}
+
 if($page !== 'query_alerts' && $page !== 'query_pms' && $page !== 'install' && $page !== 'api' && $page !== 'query_apps' && $page !== 'banner'){
 	// Set path for Smarty
 	$smarty->setCompileDir('cache/templates_c');
