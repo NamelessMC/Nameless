137ca2961006070b5dd06c1ffaaeffc16aa4b24a
samerton <timmy437@hotmail.co.uk>
2016-01-08 12:26:08 +0000

    Add update file for 1.0.1
    Add update file for 1.0.0 -> 1.0.1

---
+++ core/includes/updates/100.php
@@ -0,0 +1,28 @@
+<?php
+// 1.0.0 -> 1.0.1 updater
+
+// Database changes
+$queries->create('settings', array(
+	'name' => 'mc_status_module',
+	'value' => 'false'
+));
+
+$queries->create('settings', array(
+	'name' => 'recaptcha_secret',
+	'value' => 'null'
+));
+
+// Update version number
+$version_number_id = $queries->getWhere('settings', array('name', '=', 'version'));
+$version_number_id = $version_number_id[0]->id;
+
+$queries->update('settings', $version_number_id, array(
+	'value' => '1.0.1'
+));
+
+$version_update_id = $queries->getWhere('settings', array('name', '=', 'version_update'));
+$version_update_id = $version_update_id[0]->id;
+
+$queries->update('settings', $version_update_id, array(
+	'value' => 'false'
+));
