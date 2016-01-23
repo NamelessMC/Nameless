dd5546d169ca59dd4654ae2ca9780b757a657b7f
samerton <timmy437@hotmail.co.uk>
2016-01-23 12:57:15 +0000

    Fix potential fatal error during installation
    Fix query to a non-existent table which could prevent installation

--- core/init.php
+++ core/init.php
@@ -71,13 +71,15 @@ if($page !== 'install'){
 }
 
 // Process if user has checked "remember me"
-if(Cookie::exists(Config::get('remember/cookie_name')) && !Session::exists(Config::get('session/session_name'))) {
-	$hash = Cookie::get(Config::get('remember/cookie_name'));
-	$hashCheck = DB::getInstance()->get('users_session', array('hash', '=', $hash));
-	
-	if($hashCheck->count()){
-		$user = new User($hashCheck->first()->user_id);
-		$user->login();
+if($page !== 'install'){
+	if(Cookie::exists(Config::get('remember/cookie_name')) && !Session::exists(Config::get('session/session_name'))) {
+		$hash = Cookie::get(Config::get('remember/cookie_name'));
+		$hashCheck = DB::getInstance()->get('users_session', array('hash', '=', $hash));
+		
+		if($hashCheck->count()){
+			$user = new User($hashCheck->first()->user_id);
+			$user->login();
+		}
 	}
 }
 
