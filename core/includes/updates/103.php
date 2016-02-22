7b51b3c58a261fb177ad3513a2c3dcdf54a20c0c
samerton <timmy437@hotmail.co.uk>
2016-02-22 19:47:33 +0000

    Release version 1.0.4
    Release update files and update installer for version 1.0.4

---
+++ core/includes/updates/103.php
@@ -0,0 +1,17 @@
+<?php
+// 1.0.3 -> 1.0.4 updater
+
+// Update version number
+$version_number_id = $queries->getWhere('settings', array('name', '=', 'version'));
+$version_number_id = $version_number_id[0]->id;
+
+$queries->update('settings', $version_number_id, array(
+	'value' => '1.0.4'
+));
+
+$version_update_id = $queries->getWhere('settings', array('name', '=', 'version_update'));
+$version_update_id = $version_update_id[0]->id;
+
+$queries->update('settings', $version_update_id, array(
+	'value' => 'false'
+));
--- pages/install.php
+++ pages/install.php
@@ -610,7 +610,7 @@ if(isset($_GET["step"])){
 							// Version update
 							$version_id = $queries->getWhere('settings', array('name', '=', 'version'));
 							$queries->update('settings', $version_id[0]->id, array(
-								'value' => '1.0.3'
+								'value' => '1.0.4'
 							));
 							
 							
@@ -979,7 +979,7 @@ if(isset($_GET["step"])){
 					),
 					28 => array(
 						'name' => 'version',
-						'value' => '1.0.3'
+						'value' => '1.0.4'
 					),
 					29 => array(
 						'name' => 'version_checked',
