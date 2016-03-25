97572546ba22a7d5646ca6fe67b5054b93ab2856
samerton <timmy437@hotmail.co.uk>
2016-03-25 13:15:46 +0000

    Add Finnish Members addon translation
    Add Finnish translation for Members addon, thanks to Spigot member
    -kakara-

---
+++ addons/Members/language_finnish.php
@@ -0,0 +1,19 @@
+<?php 
+/*
+ *	Made by Partydragen
+ *  http://partydragen.com/
+ *
+ *  Modified by Samerton
+ *  https://worldscapemc.co.uk
+ *
+ */
+
+// Language file for "members" addon
+
+$members_language = array(
+	'members' => 'Jäsenet',
+	'members_icon' => '', // Icon to display before the text in the navbar
+	'username' => 'Käyttäjänimi',
+	'group' => 'Ryhmä',
+	'created' => 'Liittynyt',
+);
