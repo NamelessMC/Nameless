622b4a1a159065281ea1fe08dbb2a30dcd2971ef
samerton <timmy437@hotmail.co.uk>
2016-04-07 12:46:11 +0100

    Add custom DB class
    Add custom DB class which allows for database connections to specified
    hosts

--- core/classes/DB_Custom.php
+++ core/classes/DB_Custom.php
@@ -1,25 +1,34 @@
 <?php
-class DB_Bungee {
+/*
+ *	Made by Samerton
+ *  http://worldscapemc.co.uk
+ *
+ *  License: MIT
+ */
+ 
+class DB_Custom {
 	private static $_instance = null;
 	private $_pdo, 
 			$_query, 
 			$_error = false, 
 			$_results, 
+			$_prefix,
 			$_count = 0;
 
-	private function __construct() {
+	private function __construct($host, $database, $username, $password) {
 		try {
-			$this->_pdo = new PDO('mysql:host=' . $GLOBALS['mcdb']['inf_db']['host'] . ';dbname=' . $GLOBALS['mcdb']['inf_db']['db'], $GLOBALS['mcdb']['inf_db']['username'], $GLOBALS['mcdb']['inf_db']['password']);
-
+			$this->_pdo = new PDO('mysql:host=' . $host . ';dbname=' . $database, $username, $password);
+			$this->_pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+			$this->_prefix = '';
 		} catch(PDOException $e) {
-			die("<strong>Error:<br /></strong><div class=\"alert alert-danger\">" . $e->getMessage() . "</div>Please check your Bungee database connection settings.");
+			die("<strong>Error:<br /></strong><div class=\"alert alert-danger\">" . $e->getMessage() . "</div>Please check your database connection settings.");
 		}
 		
 	}
 
-	public static function getInstance() {
+	public static function getInstance($host, $database, $username, $password) {
 		if(!isset(self::$_instance)) {
-			self::$_instance = new DB_Bungee();
+			self::$_instance = new DB_Custom($host, $database, $username, $password);
 		}
 	
 		return self::$_instance;
@@ -41,6 +50,32 @@ class DB_Bungee {
 				$this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
 				$this->_count = $this->_query->rowCount();
 			} else {
+				print_r($this->_pdo->errorInfo());
+				$this->_error = true;
+			}
+			
+			
+		}
+		
+		return $this;
+		
+	}
+	
+	public function createQuery($sql, $params = array()) {
+		$this->_error = false;
+		if($this->_query = $this->_pdo->prepare($sql)) {
+			$x = 1;
+			if(count($params)) {
+				foreach($params as $param) {
+					$this->_query->bindValue($x, $param);
+					$x++;
+				}
+			}
+			
+			if($this->_query->execute()) {
+				$this->_count = $this->_query->rowCount();
+			} else {
+				print_r($this->_pdo->errorInfo());
 				$this->_error = true;
 			}
 			
@@ -52,8 +87,9 @@ class DB_Bungee {
 	}
 
 	public function createTable($name, $table_data, $other){
+		$name = $this->_prefix . $name;
 		$sql = "CREATE TABLE `{$name}` ({$table_data}) {$other}";
-			if(!$this->query($sql)->error()) {
+			if(!$this->createQuery($sql)->error()) {
 				return $this;
 			}
 		return false;
@@ -67,6 +103,8 @@ class DB_Bungee {
 			$operator 	= $where[1];
 			$value 		= $where[2];
 			
+			$table = $this->_prefix . $table;
+			
 			if(in_array($operator, $operators)) {
 				$sql = "{$action} FROM {$table} WHERE {$field} {$operator} ?";
 				
@@ -78,12 +116,43 @@ class DB_Bungee {
 		return false;
 	}
 	
+	public function deleteAction($action, $table, $where = array()) {
+		if(count($where) === 3) {
+			$operators = array('=', '>', '<', '>=', '<=', '<>');
+			
+			$field 		= $where[0];
+			$operator 	= $where[1];
+			$value 		= $where[2];
+			
+			$table = $this->_prefix . $table;
+			
+			if(in_array($operator, $operators)) {
+				$sql = "{$action} FROM {$table} WHERE {$field} {$operator} ?";
+				
+				if(!$this->createQuery($sql, array($value))->error()) {
+					return $this;
+				}
+			}
+		}
+		return false;
+	}
+	
 	public function get($table, $where) {
 		return $this->action('SELECT *', $table, $where);
 	}
+	
+	public function like($table, $column, $like) {
+		$table = $this->_prefix . $table;
+		$sql = "SELECT * FROM {$table} WHERE {$column} LIKE '{$like}'";
+
+		if(!$this->query($sql)->error()) {
+			return $this;
+		}
+		return false;
+	}
 
 	public function delete($table, $where) {
-		return $this->action('DELETE', $table, $where);
+		return $this->deleteAction('DELETE', $table, $where);
 	}
 	
 	public function insert($table, $fields = array()) {
@@ -99,10 +168,10 @@ class DB_Bungee {
 				$x++;
 			}
 			
-			
+			$table = $this->_prefix . $table;
 			$sql = "INSERT INTO {$table} (`" . implode('`,`', $keys) . "`) VALUES ({$values})";
 			
-			if(!$this->query($sql, $fields)->error()){
+			if(!$this->createQuery($sql, $fields)->error()){
 				return true;
 			}
 			return false;
@@ -120,20 +189,10 @@ class DB_Bungee {
 			}
 			$x++;
 		}
-				
+		$table = $this->_prefix . $table;
 		$sql = "UPDATE {$table} SET {$set} WHERE id = {$id}";
 		
-		if(!$this->query($sql, $fields)->error()) {
-			return true;
-		}
-		
-		return false;
-	}
-	
-	public function increment($table, $id, $field) {
-		$sql = "UPDATE {$table} SET {$field} = {$field} + 1 WHERE id = {$id}";
-		
-		if(!$this->query($sql)->error()) {
+		if(!$this->createQuery($sql, $fields)->error()) {
 			return true;
 		}
 		
@@ -156,12 +215,9 @@ class DB_Bungee {
 	public function count() {
 		return $this->_count;
 	}
-
-	public function lastid() {
-		return $this->_pdo->lastInsertId();
-	}
 	
 	public function orderAll($table, $order, $sort) {
+		$table = $this->_prefix . $table;
 		if(isset($sort)){
 			$sql = "SELECT * FROM {$table} ORDER BY {$order} {$sort}";
 		} else {
@@ -175,6 +231,7 @@ class DB_Bungee {
 	}
 
 	public function orderWhere($table, $where, $order, $sort) {
+		$table = $this->_prefix . $table;
 		if(isset($sort)){
 			$sql = "SELECT * FROM {$table} WHERE {$where} ORDER BY {$order} {$sort}";
 		} else {
@@ -186,4 +243,5 @@ class DB_Bungee {
 		}
 		return false;
 	}
+
 }
