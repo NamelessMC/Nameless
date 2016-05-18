19ac2ca4cc15981a6b0adea60f0b4dff9d7faa5a
samerton <timmy437@hotmail.co.uk>
2016-05-18 19:27:20 +0100

    Fix potential directory separator issues
    Fix potential directory separator issues whilst using namespaces

--- core/init.php
+++ core/init.php
@@ -23,7 +23,6 @@ if(!isset($path)){
 	// Normal autoloader
 	spl_autoload_register(function($class) {
 		if(strpos($class, 'TeamSpeak3') === false){
-			require_once 'core/classes/' . $class . '.php';
 		}
 	});
 	
@@ -32,14 +31,12 @@ if(!isset($path)){
 	require_once '../../includes/smarty/Smarty.class.php';
 	require_once '../../includes/sanitize.php';
 	spl_autoload_register(function($class) {
-		require_once '../../classes/' . $class . '.php';
 	});
 } else if($path === "../../"){
 	// For alerts/PMs
 	require_once '../includes/smarty/Smarty.class.php';
 	require_once '../includes/sanitize.php';
 	spl_autoload_register(function($class) {
-		require_once '../classes/' . $class . '.php';
 	});
 }
 
