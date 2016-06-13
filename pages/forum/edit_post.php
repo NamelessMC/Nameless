cfbd9ae38b5f2d74a3a32b8b4f89483165af9287
samerton <timmy437@hotmail.co.uk>
2016-06-13 11:17:02 +0100

    Fix SQL error whilst editing posts
    Fix SQL error whilst editing a post with no labels defined. Closes #117

--- pages/forum/edit_post.php
+++ pages/forum/edit_post.php
@@ -95,15 +95,20 @@ if($user->data()->id === $post_editing[0]->post_creator || $user->canViewMCP($us
 			
 			if($validation->passed()){
 				try {
-					// update post content
+					// Update post content
 					$queries->update("posts", $post_id, array(
 						'post_content' => htmlspecialchars(Input::get('content'))
 					));
+					
 					if(isset($edit_title)){
-						// update title
+						// Update title and label
+						// Check a label has been set..
+						if(!isset($_POST['topic_label'])) $topic_label = 0;
+						else $topic_label = $_POST['topic_label'];
+						
 						$queries->update("topics", $topic_id, array(
 							'topic_title' => htmlspecialchars_decode(Input::get('title')),
-							'label' => Input::get('topic_label')
+							'label' => $topic_label
 						));
 					}
 					Session::flash('success_post', '<div class="alert alert-info alert-dismissable"> <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span></button>Post edited.</div>');
