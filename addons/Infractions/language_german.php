5ba6287db2fe79f7ad91c27856caaab6e944979a
Sam <timmy437@hotmail.co.uk>
2016-10-04 14:24:57 +0100

    Add German Infractions translation
    Add German translation for Infractions addon, thanks to @monjo44 in #420

---
+++ addons/Infractions/language_german.php
@@ -0,0 +1,33 @@
+<?php 
+/*
+ *	Translation by monjo44
+ */
+
+// Language file for "Infractions" addon
+
+$infractions_language = array(
+	'infractions' => 'Bestrafungen',
+	'infractions_icon' => '', // Icon to display before "Infractions" in navbar
+	'view' => 'View &raquo;',
+	'staff_member' => 'Team Mitglied',
+	'action' => 'Aktion',
+	'actions' => 'Aktionen',
+	'ban' => 'Ban',
+	'temp_ban' => 'Temp Ban',
+	'warning' => 'Verwarnung',
+	'mute' => 'Stumm',
+	'kick' => 'Kick',
+	'created' => 'Erstellt',
+	'reason' => 'Grund',
+	'expires_x' => 'Läuft ab: {x}', // Don't replace {x}
+	'expired' => 'Abgelaufen',
+	'expired_x' => 'Abgelaufen am: {x}', // Don't replace {x}
+	'active' => 'Aktiv',
+	'permanent' => 'Permanent',
+	'viewing_infraction' => 'Bestrafung ansehen',
+	'user' => 'Benutzer:',
+	'status' => 'Status:',
+	'no_reason' => 'Kein Grund angegeben.',
+	'revoked' => 'Zurückgezogen',
+	'revoked_by' => 'Zurückgezogen von {x}' // Don't replace {x}
+);
