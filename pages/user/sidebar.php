3560bed1a7c2f3f051bf765e6be9ea99ce1f8ad2
partydragen <adrian-kili@hotmail.no>
2016-11-02 20:51:08 +0100

    Allow custom user sidebar links
    (This is a copy of Mod Sidebar but modifyed to be on user sidebar instant)

--- pages/user/sidebar.php
+++ pages/user/sidebar.php
@@ -4,5 +4,12 @@
 	<li<?php if($user_page == 'messaging'){ ?> class="active"<?php } ?>><a href="/user/messaging"><?php echo $user_language['private_messages']; ?></a></li>
 	<li<?php if($user_page == 'alerts'){ ?> class="active"<?php } ?>><a href="/user/alerts"><?php echo $user_language['alerts']; ?></a></li>
 	<li<?php if($user_page == 'settings'){ ?> class="active"<?php } ?>><a href="/user/settings"><?php echo $user_language['profile_settings']; ?></a></li>
+	<?php 
+	if(isset($custom_user_sidebar)){
+        foreach($custom_user_sidebar as $key => $item){
+		  echo '<li' . ($user_page == $key ? ' class="active"' : '') . '><a href="' . $item['url'] . '">' . $item['title'] . '</a></li>';
+		}
+	}
+	?>
   </ul>
 </div>
