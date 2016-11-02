a0917837c89a01b2a6612e077c396621753e63c6
partydragen <adrian-kili@hotmail.no>
2016-11-02 19:17:52 +0100

    Update move_thread.php

--- pages/forum/move_thread.php
+++ pages/forum/move_thread.php
@@ -96,7 +96,7 @@ $token = Token::generate();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="<?php echo $sitename; ?> Forum - Move Thread">
-    <meta name="author" content="Samerton">
+    <meta name="author" content="<?php echo $sitename; ?>">
 	<meta name="robots" content="noindex">
     <?php if(isset($custom_meta)){ echo $custom_meta; } ?>
 	
