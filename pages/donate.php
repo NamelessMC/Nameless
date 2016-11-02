a337070f9591621719ccc24256cc54533bf8073a
partydragen <adrian-kili@hotmail.no>
2016-11-02 19:06:30 +0100

    Update donate.php

--- pages/donate.php
+++ pages/donate.php
@@ -26,7 +26,7 @@ require('core/includes/htmlpurifier/HTMLPurifier.standalone.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Donation page for the <?php echo $sitename; ?> community">
-    <meta name="author" content="Samerton">
+    <meta name="author" content="<?php echo $sitename; ?>">
     <meta name="theme-color" content="#454545" />
 	<?php if(isset($custom_meta)){ echo $custom_meta; } ?>
 	
