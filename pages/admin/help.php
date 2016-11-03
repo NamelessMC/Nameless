9ff7f91debd3470d5ff16c98bb7142a26acac90e
partydragen <adrian-kili@hotmail.no>
2016-11-03 19:05:31 +0100

    Update help.php

--- pages/admin/help.php
+++ pages/admin/help.php
@@ -43,6 +43,51 @@ $adm_page = "help";
 	$title = $admin_language['help'];
 	
 	require('core/includes/template/generate.php');
+<?php
+/*
+ *	Made by Partydragen
+ *  http://partydragen.com/
+ *
+ *  License: MIT
+ */
+
+// Ensure user is logged in, and is admin
+if($user->isLoggedIn()){
+	if($user->canViewACP($user->data()->id)){
+		if($user->isAdmLoggedIn()){
+			// Can view
+		} else {
+			Redirect::to('/admin');
+			die();
+		}
+	} else {
+		Redirect::to('/');
+		die();
+	}
+} else {
+	Redirect::to('/');
+	die();
+}
+ 
+$adm_page = "help";
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Admin panel">
+    <meta name="author" content="<?php echo $sitename; ?>">
+	<meta name="robots" content="noindex">
+	<?php if(isset($custom_meta)){ echo $custom_meta; } ?>
+	
+	<?php
+	// Generate header and navbar content
+	// Page title
+	$title = $admin_language['help'];
+	
+	require('core/includes/template/generate.php');
 	?>
 	
 	<link href="/core/assets/plugins/switchery/switchery.min.css" rel="stylesheet">	
@@ -75,10 +120,18 @@ $adm_page = "help";
 				* <a href="https://www.spigotmc.org/threads/88001/" target="_blank">Spigot Resource</a></br>
 
 				<h3>FAQs</h3>
-				* <a href="https://github.com/NamelessMC/Nameless/wiki/FAQs" target="_blank">FAQs</a>
+				* <a href="https://github.com/NamelessMC/Nameless/wiki/FAQs" target="_blank">FAQs</a></br>
 
 				<h3>Open Source</h3>
 				* <a href="https://github.com/NamelessMC/Nameless" target="_blank">Source Code on GitHub</a></br>
+
+				<h3>NamelessMC Plugin</h3>
+				* <a href="#">There is no official NamelessMC Plugin yet (Under development)</a></br>
+				* <a href="https://www.spigotmc.org/resources/23337/" target="_blank">Temporary NamelessMC Registration Plugin (Unofficial)</a></br>
+
+				<h3>Video Tutorial</h3>
+				* <a href="https://youtu.be/BlRz9gpS-Ew?t=3m12s" target="_blank">Setup Forums (v1.0.2)</a></br>
+				* <a href="https://youtu.be/BlRz9gpS-Ew?t=5m56s" target="_blank">Connect your Minecraft server (v1.0.2)</a></br>
 			</div>
 	    </div>
 	  </div>
