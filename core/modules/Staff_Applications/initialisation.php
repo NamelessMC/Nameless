3774f70e718dcb8219db1e50c90087b5b60bc76b
Sam <timmy437@hotmail.co.uk>
2016-11-10 20:49:00 +0000

    Allow moving Staff Application link
    Allow moving Staff Application link from within the module settings

--- core/modules/Staff_Applications/initialisation.php
+++ core/modules/Staff_Applications/initialisation.php
@@ -9,4 +9,35 @@
 // Initialise the staff application module
 // We've already checked to see if it's enabled
 
-$navbar_array[] = array('apply' => $navbar_language['staff_apps_icon'] . $navbar_language['staff_apps']);
+if($user->isLoggedIn()){
+	// Check cache for link location
+	$c->setCache('staffapps');
+	if($c->isCached('linklocation')){
+		$link_location = $c->retrieve('linklocation');
+	} else {
+		$c->store('linklocation', 'navbar');
+		$link_location = 'navbar';
+	}
+
+	// Enabled, add links to navbar
+	switch($link_location){
+		case 'navbar':
+			$navbar_array[] = array('apply' => $navbar_language['staff_apps_icon'] . $navbar_language['staff_apps']);
+		break;
+		
+		case 'footer':
+			$footer_nav_array['apply'] = $navbar_language['staff_apps_icon'] . $navbar_language['staff_apps'];
+		break;
+		
+		case 'more':
+			$nav_more_dropdown[$navbar_language['staff_apps_icon'] . $navbar_language['staff_apps']] = '/apply';
+		break;
+		
+		case 'none':
+		break;
+		
+		default:
+			$navbar_array[] = array('apply' => $navbar_language['staff_apps_icon'] . $navbar_language['staff_apps']);
+		break;
+	}
+}
