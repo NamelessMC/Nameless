b6c3ce46fbb84f37df74f5ff1d31522aa7ba6ffb
Sam <timmy437@hotmail.co.uk>
2016-12-07 00:52:44 +0000

    Fix Buycraft synchronisation
    Fix Buycraft API synchronisation issue within Donate addon

--- addons/Donate/sync.php
+++ addons/Donate/sync.php
@@ -38,129 +38,110 @@ if($webstore == 'bc'){
 	require('integration/buycraft.php');
 	
 	/*
-	 * PACKAGES SYNC
+	 *  Categories and packages
 	 */
 	
-	// Categories first
-	foreach($bc_categories['payload'] as $item){
-		// Does it already exist in the database?
-		$category_name = htmlspecialchars($item['name']);
-		$category = $queries->getWhere('donation_categories', array('name', '=', $category_name));
-		
-		if(!count($category)){
-			// No, it doesn't exist
-			$category_id = $item['id'];
-			$queries->create('donation_categories', array(
-				'name' => $category_name,
-				'cid' => $category_id,
-				'order' => 0
-			));
-		}
-	}
-	
-	// Delete any categories which don't exist on the web store anymore
-	$categories = $queries->getWhere('donation_categories', array('id', '<>', 0));
-	foreach($categories as $category){
-		if(!in_array_r($category->cid, $bc_categories['payload'])){
-			// It doesn't exist anymore
-			$queries->delete('donation_categories', array('id', '=', $category->id));
-		}
-	}
-	
-	// Packages next
-	foreach($bc_packages['payload'] as $item){
-		// Does it already exist in the database?
-		$package_name = htmlspecialchars($item['name']);
-		$package = $queries->getWhere('donation_packages', array('name', '=', $package_name));
-		
-		if(!count($package)){
-			// No, it doesn't exist
-			$package_id = $item['id'];
-			$package_category = $item['category'];
-			$package_description = htmlspecialchars($item['description']);
-			$package_price = $item['price'];
-			$package_order = $item['order'];
+	$packages = array();
+	$categories = array();
+
+	if(count($bc_categories)){
+		foreach($bc_categories['categories'] as $item){
+			// Does it already exist in the database?
+			$category = $queries->getWhere('donation_categories', array('cid', '=', htmlspecialchars($item['id'])));
 			
-			$queries->create('donation_packages', array(
-				'name' => $package_name,
-				'description' => $package_description,
-				'cost' => $package_price,
-				'package_id' => $package_id,
-				'active' => 1,
-				'package_order' => $package_order,
-				'category' => $package_category,
-				'url' => ''
-			));
-		}
-	}
-	
-	// Delete any packages which don't exist on the web store anymore
-	$packages = $queries->getWhere('donation_packages', array('id', '<>', 0));
-	foreach($packages as $package){
-		if(!in_array_r($package->package_id, $bc_packages['payload'])){
-			// It doesn't exist anymore
-			$queries->delete('donation_packages', array('id', '=', $package->id));
-		}
-	}
-	
-	/*
-	 * LATEST DONORS SYNC
-	 */
-	 
-	foreach($bc_payments["payload"] as $donation){
-		$existing = $queries->getWhere('donation_cache', array("time", "=", $donation["time"]));
-		if(!count($existing)){
-			try {
-				$queries->create('donation_cache', array(
-					"time" => $donation["time"],
-					"uuid" => $donation["uuid"],
-					"ign" => $donation["ign"],
-					"price" => $donation["price"],
-					"package" => $donation["packages"][0]
+			if(!count($category)){
+				// Input it now
+				$queries->create('donation_categories', array(
+					'name' => htmlspecialchars($item['name']),
+					'cid' => $item['id'],
+					'order' => $item['order']
 				));
-			} catch(Exception $e){
-				die($e->getMessage());
+			} else {
+				// Update category
+				$queries->update('donation_categories', $category[0]->id, array(
+					'name' => htmlspecialchars($item['name']),
+					'cid' => $item['id'],
+					'`order`' => $item['order']
+				));
 			}
+			
+			// Packages
+			if(count($item['packages'])){
+				foreach($item['packages'] as $package){
+					// Does it already exist?
+					$package_exists = $queries->getWhere('donation_packages', array('package_id', '=', $package['id']));
+					
+					if(!count($package_exists)){
+						// Input it now
+						$queries->create('donation_packages', array(
+							'name' => htmlspecialchars($package['name']),
+							'description' => 'No description available',
+							'cost' => $package['price'],
+							'package_id' => $package['id'],
+							'active' => 1,
+							'package_order' => $package['order'],
+							'category' => $item['id'],
+							'url' => 'none'
+						));
+					} else {
+						// Update package
+						$queries->update('donation_packages', $package_exists[0]->id, array(
+							'name' => htmlspecialchars($package['name']),
+							'description' => 'No description available',
+							'cost' => $package['price'],
+							'package_id' => $package['id'],
+							'active' => 1,
+							'package_order' => $package['order'],
+							'category' => $item['id'],
+							'url' => 'none'
+						));
+					}
+					
+					// Add to array containing all packages
+					$packages[] = $package['id'];
+				}
+			}
+			
+			// Add to array containing all categories
+			$categories[] = $item['id'];
+		}
+	}
+	
+	// Delete categories and packages no longer on web store
+	$package_query = $queries->getWhere('donation_packages', array('package_id', '<>', 0));
+	foreach($package_query as $item){
+		if(!in_array($item->package_id, $packages)){
+			$queries->delete('donation_packages', array('package_id', '=', $item->package_id));
+		}
+	}
+	
+	$category_query = $queries->getWhere('donation_categories', array('cid', '<>', 0));
+	foreach($category_query as $item){
+		if(!in_array($item->cid, $categories)){
+			$queries->delete('donation_categories', array('cid', '=', $item->cid));
 		}
 	}
 
 	/*
-	 * GROUP SYNC
-	 * 1 - import donor groups from database
-	 * 2 - loop through donors
-	 * 3 - for each donor, check if they already have a DONOR/STANDARD group (ie not staff)
-	 * 4 - if the user is a staff member, do nothing, else:
-	 *       a - check if the user is a donor already, if so:
-	 *       		i - add most valuable package to the user
-			 b - if not, add the most valuable package (if they've bought multiple) to the user
+	 *  Latest donors
 	 */
 
-	$donor_groups = $queries->getWhere('groups', array("buycraft_id", "<>", "NULL"));
-
-	foreach($bc_payments["payload"] as $donor){
-		$donor_user = $queries->getWhere('users', array("uuid", "=", $donor["uuid"]));
-		if(count($donor_user)){ // see if the user has registered
-			// Are they a staff member?
-			$user_group = $queries->getWhere('groups', array('id', '=', $donor_user[0]->group_id));
-			if($user_group[0]->staff == 1){ 
-				// Don't do anything as they're a staff member - we want them to keep their staff rank
-			} else {
-				if(count($donor["packages"]) === 1){
-					$donor_group = $queries->getWhere("groups", array("buycraft_id", "=", $donor["packages"][0]));
-					$package_group_id = $donor_group[0]->id;
-					if($donor_user[0]->group_id < $package_group_id){
-						try {
-							$queries->update('users', $donor_user[0]->id, array(
-								'group_id' => $package_group_id
-							));
-						} catch(Exception $e){
-							die($e->getMessage());
-						}
-					}
-				}
-				/*
-				 * TODO: Run check if user has purchased multiple packages
-				 */
+	if(count($bc_payments)){
+		// Get latest payment already stored in cache
+		$latest_payment = $queries->orderWhere('donation_cache', 'id <> 0', 'time', 'DESC');
+		if(count($latest_payment)) $latest_payment = $latest_payment[0]->time;
+		else $latest_payment = 0;
+		
+		foreach($bc_payments as $item){
+			if($item['date'] < strtotime($item['date'])){
+				// Input into database
+				$queries->create('donation_cache', array(
+					'time' => strtotime($item['date']),
+					'uuid' => htmlspecialchars($item['player']['uuid']),
+					'ign' => htmlspecialchars($item['player']['name']),
+					'price' => $item['amount'],
+					'package' => (isset($item['packages'][0]) ? $item['packages'][0]['id'] : 0) // TODO: multiple packages
+				));
 			}
 		}
 	}
