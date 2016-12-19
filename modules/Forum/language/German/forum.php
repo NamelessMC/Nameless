598686b0b2577123e7ba452bb902357f72cd2a92
BukkitTNT <BukkitTNT@users.noreply.github.com>
2016-12-19 22:29:52 +0100

    translations stuff (#497)
    * Translations GERMAN & EnglishUS for Forum module
    
    * Update forum.php
    
    * Update forum.php
    
    * Update forum.php
    
    * Auto Install Languages on Installation

---
+++ modules/Forum/language/German/forum.php
@@ -0,0 +1,137 @@
+<?php 
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-dev
+ *
+ *  License: MIT
+ *
+ *  German Language for Forum module
+ */
+
+$language = array(
+	/*
+	 *  Forums
+	 */ 
+	'forum' => 'Forum',
+	'forums' => 'Foren',
+	'forum_index' => 'Startseite',
+	'no_users_online' => 'Keine Benutzer online.',
+	'online_users' => 'Benutzer Online',
+	'discussion' => 'Diskussion',
+	'topic' => 'Thema',
+	'stats' => 'Statistiken',
+	'topics' => 'Themen',
+	'views' => 'Aufrufe',
+	'posts' => 'Beiträge',
+	'by' => 'von',
+	'in' => 'in',
+	'last_reply' => 'Letzte Antwort',
+	'latest_discussions' => 'Neueste Diskussionen',
+	'users_registered' => '<strong>Benutzer registriert:</strong> {x}', // Don't replace "{x}"
+	'latest_member' => '<strong>Neuester Benutzer:</strong> {x}', // Don't replace "{x}"
+	'subforums' => 'Unterforen',
+	'subforum' => 'Unterforum',
+	'no_topics' => 'Es wurden noch keine Themen erstellt.',
+	'no_topics_short' => 'Keine Themen',
+	'new_topic' => 'Neues Thema',
+	'post_successful' => 'Beitrag erfolgreich veröffentlicht.',
+	'post_edited_successfully' => 'Beitrag erfolgreich editiert.',
+	'user_tag' => 'Du wurdest in einem Beitrag erwähnt.',
+	'user_tag_info' => 'Du wurdest in einem Beitrag von {x} erwähnt.', // Don't replace "{x}"
+	'creating_topic_in_x' => 'Erstelle in Thema in {x}', // Don't replace "{x}"
+	'topic_title' => 'Titel des Themas',
+	'new_reply' => 'Neue Antwort',
+	're' => 'RE: ',
+	'topic_locked' => 'Thema geschlossen.',
+	'mod_actions' => 'Moderation',
+	'lock_topic' => 'Thema schließen',
+	'unlock_topic' => 'Thema Öffnen',
+	'merge_topic' => 'Thema zusammenfassen',
+	'merge_topics' => 'Themen zusammenfassen',
+	'merge_instructions' => 'Der Thread zum Zusammenführen <strong>muss</strong> im selben Forum sein. Verschieben Sie gegebenenfalls einen Thread.',
+	'confirm_delete_short' => 'Löschen bestätigen',
+	'confirm_delete_topic' => 'Bist du sicher, dass du dieses Thema löschen willst?',
+	'confirm_delete_post' => 'Bist du sicher, dass du diesen Beitrag löschen willst?',
+	'delete_topic' => 'Thema löschen',
+	'move_topic' => 'Thema verschieben',
+	'move_topic_to' => 'Thema verschieben nach:',
+	'stick_topic' => 'Thema anheften',
+	'unstick_topic' => 'Thema abheften',
+	'share' => 'Teilen',
+	'share_twitter' => 'Auf Twitter teilen',
+	'share_facebook' => 'Aut Facebook teilen',
+	'edit' => 'Editieren',
+	'edit_post' => 'Beitrag editieren',
+	'last_edited' => 'Letzte änderung: {x}', // Don't replace "{x}"
+	'quote' => 'Zitat',
+	'topic_locked_notice' => 'Dieses Thema ist geschlossen, du bist trotzdem dazu berechtigt Beiträge erstellen.',
+	'title_required' => 'Gib bitte einen Titel an.',
+	'content_required' => 'Gib bitte Inhalt an',
+	'title_min_2' => 'Dein Titel muss mindestens 2 Zeichen lang sein.',
+	'title_max_64' => 'Dein Titel darf nicht länger als 64 Zeichen sein.',
+	'content_min_2' => 'Dein Inhalt muss mindestens 2 Zeichen beihnalten.',
+	'content_max_20480' => 'Dein Inhalt darf nicht länger als 20480 Zeichen lang sein.',
+	'post_already_reported' => 'Du hast diesen Beitrag bereits gemeldet!',
+	'quoted_post' => 'Beitrag wurde als Zitat zur Antwort hinzugefügt.',
+	'removed_quoted_post' => 'Beitrag wurde aus den Zitaten der Antwort entfernt.',
+	'quoting_posts' => 'Füge Zitat ein...',
+	'error_quoting_posts' => 'Beim einfügen des Zitats ist etwas fehlgeschlagen!',
+	'error_rating_post' => 'Entschuldige du kannst diesen Beitrag nicht bewerten.',
+	'topic_stuck' => 'Thema wurde angeheftet.',
+	'topic_unstuck' => 'Thema wurde abgeheftet.',
+	'spam_wait' => 'Bitte warte {x} Sekunden bis du wieder Beiträge erstellen kannst.',
+	
+	// Homepage
+	'latest_announcements' => 'Neueste Ankündigungen',
+	'read_full_post' => 'Ganzen Beitrag lesen',
+	
+	// Admin tab
+	'labels' => 'Labels', //Means 'Etiketten' but Labels is in Germany the same word
+	'new_forum' => '<i class="fa fa-plus-circle"></i> Neues Forum',
+	'new_label' => '<i class="fa fa-plus-circle"></i> Neues Label',
+	'creating_label' => 'Erstelle neues Label',
+	'editing_label' => 'Editiere Label',
+	'label_name' => 'Label Name',
+	'label_type' => 'Label Typ',
+	'label' => 'Label',
+	'label_forums' => 'Label Foren',
+	'no_forums' => 'Keine Foren',
+	'no_labels_defined' => 'Es wurde bis her keine Labels definiert.',
+	'label_edit_success' => 'Label erfolgreich editiert.',
+	'label_creation_success' => 'Label erfolgreich erstellt.',
+	'label_creation_error' => 'Fehler beim erstellen des Labels',
+	'creating_forum' => 'Erstelle neues Forum',
+	'forum_name' => 'Forum Name',
+	'forum_description' => 'Forum Beschreibung',
+	'delete_forum' => 'Forum löschen',
+	'move_topics_and_posts_to' => 'Verschiebe Themen und Beiträge zu',
+	'delete_topics_and_posts' => 'Lösche Themen und Beiträge',
+	'forum_permissions' => 'Forum Berechtigungen',
+	'select_a_parent_forum' => 'Wähle ein übergeordnetes Forum',
+	'parent_forum' => 'Übergeordnetes Forum',
+	'has_no_parent' => 'Kein übergeordnetes Forum',
+	'guests' => 'Gäste',
+	'group' => 'Gruppe',
+	'can_view_forum' => 'Kann dieses Forum gesehen werden?',
+	'can_view_other_topics' => 'Können Nutzer andere Themen sehen?',
+	'can_create_topic' => 'Können Themen erstellt werden?',
+	'can_post_reply' => 'Können Antworten erstellt werden?',
+	'can_moderate_forum' => 'Kann das Forum moderiert werden?',
+	'display_topics_as_news' => 'Sollen Themen auf der Startseite angezeigt werden?',
+	'forum_created_successfully' => 'Forum wurde erfolgreich erstellt.',
+	'forum_layout' => 'Forum Layout',
+	'table_view' => 'Tabellenansicht',
+	'latest_discussions_view' => '"Letzte Diskussionen" Ansicht',
+	'input_forum_title' => 'Gib einen Forennamen ein.',
+	'input_forum_description' => 'Gib eine Forenbeschreibung ein.',
+	'forum_name_minimum' => 'Der Forenname muss mindestens 2 Zeichen lang sein.',
+	'forum_description_minimum' => 'Die Forenbeschreibung muss mindestens 2 Zeichen lang sein.',
+	'forum_name_maximum' => 'Der Forenname darf maximal 150 Zeichen lang sein.',
+	'forum_description_maximum' => 'Die Forenbeschreibung darf maximal 255 Zeichen lang sein.',
+	'forum_type' => 'Forum Typ',
+	'forum_type_forum' => 'Forum',
+	'forum_type_category' => 'Kategorie',
+	'invalid_action' => 'Ungültige Aktion',
+	'use_reactions' => '"Reaktionen" Funktion verwenden?'
+);
