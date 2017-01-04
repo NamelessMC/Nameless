35bfae9c906b710b31416a16e19039969f895605
Oscar W. H <ohalland@gmail.com>
2017-01-04 18:00:23 +0100

    Fix for issue #509 (#510)
    * Add icon to navbar.php
    
    * Update queries.php to enable icons on custom pages
    
    * Update Queries.php
    
    * Update pages.php
    
    * Update pages.php
    
    * Update navbar.php

--- core/includes/template/navbar.php
+++ core/includes/template/navbar.php
@@ -62,25 +62,33 @@ foreach($custom_pages as $item){
 		if(isset($page) && $page == $item->title){
 			$navbar_links .= ' class="active"';
 		}
-		$navbar_links .= '><a href="' . htmlspecialchars($item->url) . '">' . $item->title . '</a></li>';
-	} else if($item->link_location == 2){
-		// More dropdown
-		$nav_more_dropdown[$item->title] = $item->url;
-	}
+		
+		if(isset($item->icon)) {
+        	    $navbar_links .= '><a href="' . htmlspecialchars($item->url) . '">' . $item->icon . $item->title . '</a></li>';
+        	} else {
+        	    $navbar_links .= '><a href="' . htmlspecialchars($item->url) . '">' . $item->title . '</a></li>';
+        	}
+    	} else if($item->link_location == 2){
+    	    array_push($nav_more_dropdown, $item);
+    	}
 }
 
-// More dropdown
-if(isset($nav_more_dropdown) && !empty($nav_more_dropdown)){
-	$navbar_links .= '<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">' . $navbar_language['more'] . ' <span class="caret"></span></a>
-						  <ul class="dropdown-menu">';
-	
-	foreach($nav_more_dropdown as $key => $item){
-		$navbar_links .= '<li><a href="' . htmlspecialchars($item) . '">' . $key . '</a></li>';
+	// More dropdown
+	if(isset($nav_more_dropdown) && !empty($nav_more_dropdown)){
+    		$navbar_links .= '<li class="dropdown">
+    		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">' . $navbar_language['more'] . ' <span class="caret"></span></a>
+    		<ul class="dropdown-menu">';
+
+    		foreach($nav_more_dropdown as $key => $item){
+        		if(isset($item->icon)) {
+            			$navbar_links .= '<li><a href="' . htmlspecialchars($item->url) . '">' . $item->icon . $item->title . '</a></li>';
+        		} else {
+            			$navbar_links .= '<li><a href="' . htmlspecialchars($item->url) . '">' . $item->title . '</a></li>';
+        		}
+    		}
+
+    		$navbar_links .= '</ul></li>';
 	}
-	
-	$navbar_links .= '</ul></li>';
-}
 
 $navbar_links .= '</ul>';
  
