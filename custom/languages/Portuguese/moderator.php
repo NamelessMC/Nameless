e568fa4907935c46b2946641a8a417190e834f74
dgateles <dga.teles@gmail.com>
2017-01-05 09:02:04 -0200

    Moderator Translated

---
+++ custom/languages/Portuguese/moderator.php
@@ -0,0 +1,51 @@
+<?php 
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-dev
+ *
+ *  License: MIT
+ *
+ *  EnglishUS Language - Moderator terms
+ */
+$language = array(
+	'mod_cp' => 'ModCP',
+	'overview' => 'Visão Geral',
+	
+	// Spam
+	'spam' => 'Spam',
+	'mark_as_spam' => 'Marcar como Spam',
+	'confirm_spam' => '<p>Tem certeza de que deseja marcar esse usuário como spam?</p><p>O IP do usuário será banido e todo seu conteúdo será removido.</p>',
+	'user_marked_as_spam' => 'Usuário marcado como spam com sucesso.',
+	'cant_ban_admin' => 'Você não pode banir um administrador!',
+	
+	// IP Lookup
+	'ip_lookup' => 'Pesquisar IP',
+	'search_for_ip' => 'Procurar um nome de usuário ou um endereço IP:',
+	'no_ips_with_username' => 'Nenhum endereço IP para esse usuário encontrado.',
+	'no_accounts_with_that_ip' => 'Nenhuma conta para esse endereço IP encontrado.',
+	'count_accounts_with_ip' => 'Encontrados {x} contas com o endereço de IP {y}', // Don't replace "{x}" or "{y}"
+	'count_ips_with_name' => 'Encontrados {x} endereços de IP para o usuário {y}', // Don't replace "{x}" or "{y}"
+	'no_users_or_ips_found' => 'Não foram encontrados usuários ou endereços IP.',
+	
+	// Reports
+	'reports' => 'Alertas',
+	'report_alert' => 'Novo alerta enviado',
+	'user_reported' => 'Usuário Alertado',
+	'comments' => 'Comentários',
+	'updated_by' => 'Atualizado Por',
+	'actions' => 'Ações',
+	'view_closed' => 'Visualizar Fechado',
+	'view_open' => 'Visualizar Aberto',
+	'viewing_report' => 'Visualizando Alertas',
+	'view_content' => 'Visualizar conteúdo alertado',
+	'no_comments' => 'Sem comentários',
+	'new_comment' => 'Novo comentário',
+	'report_comment_invalid' => 'Conteúdo do comentário inválido. Certifique-se de que introduziu um comentário entre 1 e 10000 caracteres.',
+	'close_report' => 'Fechar alerta',
+	'reopen_report' => 'Reabrir alerta',
+	'open_reports' => 'Há atualmente <strong>{x}</strong> alertas abertos.', // Don't replace {x}
+	
+	// Punishments
+	'punishments' => 'Punições'
+);
