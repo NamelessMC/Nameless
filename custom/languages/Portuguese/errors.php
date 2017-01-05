a4daab2835290d9bba8c2665b010b7b61fab3510
dgateles <dga.teles@gmail.com>
2017-01-05 08:43:41 -0200

    Erros Translated

---
+++ custom/languages/Portuguese/errors.php
@@ -0,0 +1,22 @@
+<?php 
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-dev
+ *
+ *  License: MIT
+ *
+ *  Portuguese Language - Admin
+ *  Translation By Douglas Teles
+ *  Last Update: 04/01/2017
+ */
+$language = array(
+	/*
+	 *  Errors
+	 */
+	'404_title' => 'Desculpe!',
+	'404_content' => 'Nós não encontramos essa página.',
+	'404_back' => 'Voltar',
+	'404_home' => 'Home',
+	'404_error' => 'Se você acredita que isso seja um erro, {x}entre em contato com um administrador{y}' // Don't replace {x} or {y}
+);
