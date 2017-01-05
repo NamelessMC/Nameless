d1fb5d7540cdbc492766c8fd8b03c750ed2958d6
dgateles <dga.teles@gmail.com>
2017-01-05 08:50:56 -0200

    General Translated

---
+++ custom/languages/Portuguese/general.php
@@ -0,0 +1,69 @@
+<?php 
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-dev
+ *
+ *  License: MIT
+ *
+ *  Portuguese Language - Admin
+ *  Translation By Douglas Teles
+ *  Last Update: 04/01/2017
+ */
+$language = array(
+	/*
+	 *  General terms
+	 */ 
+	'submit' => 'Enviar',
+	'register' => 'Cadastrar',
+	'sign_in' => 'Entrar',
+	'log_out' => 'Sair',
+	'profile' => 'Perfil',
+	'invalid_token' => 'Token inválido, tente novamente.',
+	'close' => 'Fechar',
+	'back' => 'Voltar',
+	'warning' => 'Atenção',
+	'date' => 'Data',
+	'search' => 'Procurar',
+	'edit' => 'Editar',
+	'next' => 'Próximo',
+	'confirm_cancel' => 'Tem certeza de que deseja cancelar?',
+	'confirm_delete' => 'Confirmar exclusão',
+	'confirm_deletion' => 'Tem certeza de que deseja excluir isso?',
+	'cancel' => 'Cancelar',
+	'delete' => 'Excluir',
+	'none' => 'Nenhum',
+	'view' => 'Visualizar',
+	'change' => 'Alterar',
+	'remove' => 'Remover',
+	'info' => 'Info',
+	'confirm' => 'Confirmar',
+	'actions' => 'Ações',
+	'alerts' => 'Alertas',
+	'view_alerts' => 'Visualizar alertas',
+	'no_alerts' => 'Sem alertas',
+	'x_new_alerts' => 'Você possuí {x} novos alertas.', // Don't replace {x}
+	'messages' => 'Mensagens',
+	'view_messages' => 'Visualizar mensagens',
+	'no_messages' => 'Sem mensagens',
+	'x_new_messages' => 'Você possuí {x} novas mensagens.', // Don't replace {x}
+	'loading' => 'Carregando...',
+	'report' => 'Relatório',
+	'page_loaded_in' => 'Página gerada em {x}s', // Don't replace {x}, time unit is seconds
+	
+	/* 
+	 *  Navbar
+	 */
+	'home' => 'Home',
+	
+	/*
+	 *  Homepage
+	 */
+	'social' => 'Social',
+	
+	
+	/*
+	 *  Help dialogues
+	 */
+	'markdown_help' => 'Você pode formatar sua postagem usando o Markdown. Para mais informações, veja <a href=\'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet\' target=\'_blank\'>aqui</a>.'
+);
