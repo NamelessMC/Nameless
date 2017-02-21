ee7c6ad936ec489368810e2d16f1e5e421e3af9f
Rody Molenaar <rody.molenaar@xtrada.nl>
2017-02-21 13:36:45 +0100

    New configuration method

---
+++ core/installation/views/already_installed.view.php
@@ -0,0 +1,33 @@
+<html lang="en">
+<head>
+    <!-- Page Title -->
+    <title>Install &bull; NamelessMC</title>
+
+    <!-- Global CSS -->
+    <link rel="stylesheet" href="core/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="core/assets/css/custom.css">
+    <link rel="stylesheet" href="core/assets/css/font-awesome.min.css">
+
+    <style>
+    html {
+        overflow-y: scroll;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="jumbotron jumbotron-fluid" style="height:100vh; margin-bottom:0rem !important;">
+        <center>
+            <h1>NamelessMC v2 <sup><span style="font-size: small;">pre-release</span></sup></h1>
+
+            <hr />
+
+            <p>Welcome to NamelessMC version 2.0 pre-release.</p>
+
+            <p>It appears Nameless has already been installed. If you want to re-install Nameless, you should remove the database tables and the configuration file located at: <code>core/config.php</code></p>
+
+            <a href="<?php echo URL::build('/login'); ?>" class="btn btn-primary">Login</a>
+
+        </div>
+    </body>
+    </html>
