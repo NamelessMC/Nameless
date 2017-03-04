7b406371df9d5e74c35371ee734a389924c8c6d2
Sam <timmy437@hotmail.co.uk>
2017-03-04 21:41:53 +0000

    Allow non-3306 database ports
    Allow database connections through non 3306 ports

--- core/classes/DB.php
+++ core/classes/DB.php
@@ -10,7 +10,7 @@ class DB {
 
 	private function __construct() {
 		try {
-			$this->_pdo = new PDO('mysql:host=' . Config::get('mysql/host') . ';dbname=' . Config::get('mysql/db'), Config::get('mysql/username'), Config::get('mysql/password'));
+			$this->_pdo = new PDO('mysql:host=' . Config::get('mysql/host') . ';port=' . Config::get('mysql/port') . ';dbname=' . Config::get('mysql/db'), Config::get('mysql/username'), Config::get('mysql/password'));
 			$this->_pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 			$this->_prefix = Config::get('mysql/prefix');
 		} catch(PDOException $e) {
