1bb78f0657cea513f800e2f36c2edd457a9ef35f
ArisC <achardalis@gmail.com>
2017-03-23 19:23:50 +0200

    Added Greek Language for errors.php

---
+++ custom/languages/Greek/errors.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ *	Made by ArisC
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-dev
+ *
+ *  License: MIT
+ *
+ *  Greek Language - Errors
+ */
+
+$language = array(
+	/*
+	 *  Errors
+	 */
+	'404_title' => 'Σύγνωμη!',
+	'404_content' => 'Δεν μπορέσαμε να βρούμε αυτή τη σελίδα.',
+	'404_back' => 'Πήγαινε πίσω',
+	'404_home' => 'Αρχική',
+	'404_error' => 'Αν πιστεύετε ότι πρόκειται για σφάλμα, {x}παρακαλούμε επικοινωνήστε με έναν διαχειριστή{y}', // Don't replace {x} or {y}
+    'maintenance_title' => 'Λειτουργία συντήρησης.',
+    'maintenance_retry' => 'Ξαναδοκιμάσετε'
+);
