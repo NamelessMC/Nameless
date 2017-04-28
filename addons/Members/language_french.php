a127efde235e2d42b4389ee21920ce5bf4389216
Avior <2bazert@gmail.com>
2017-04-28 18:05:46 +0200

    Create language_french.php
    french translation

---
+++ addons/Members/language_french.php
@@ -0,0 +1,21 @@
+
+<?php 
+/*
+ *	Made by Partydragen
+ *  http://partydragen.com/
+ *
+ *  Modified by Samerton
+ *  https://worldscapemc.co.uk/
+ *
+ *  Translated by Aviortheking
+ *  https://delta-wings.net/
+ *
+ */
+// Language file for "members" addon
+$members_language = array(
+	'members' => 'Membres',
+	'members_icon' => '', // Icon to display before the text in the navbar
+	'username' => 'Nom d\'utilisateur',
+	'group' => 'Groupe',
+	'created' => 'Rejoins le',
+);
