a4abf24f72b2165c854ef905a078badbef402236
RobinMC <robinslot@hotmail.nl>
2017-06-27 17:09:57 +0200

    je

--- custom/languages/Dutch/errors.php
+++ custom/languages/Dutch/errors.php
@@ -16,7 +16,7 @@ $language = array(
 	 *  Problemen
 	 */
 	'404_title' => 'Het spijt ons!',
-	'404_content' => 'We konden de pagina die u zocht niet vinden.',
+	'404_content' => 'We konden de pagina die je zocht niet vinden.',
 	'404_back' => 'Terug?',
 	'404_home' => 'Start',
 	'404_error' => 'Als je denkt dat dit een error is, {x}neem dan contact op met een beheerder van de website{y}', // Verander "{x}" of "{y}" niet
