9cc0500720e041736e59e7998bf51593ed62adb8
RobinMC <robinslot@hotmail.nl>
2017-06-27 17:07:26 +0200

    Update version to pr2

--- custom/languages/Dutch/moderator.php
+++ custom/languages/Dutch/moderator.php
@@ -4,7 +4,7 @@
  *  en vertaald door Sander Lambrechts
  *  
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-dev
+ *  NamelessMC version 2.0.0-pr2
  *
  *  License: MIT
  *
