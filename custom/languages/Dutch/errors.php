8c0ab07ae309d534eff109e1948f66c34aabe7b2
RobinMC <robinslot@hotmail.nl>
2017-06-27 17:10:12 +0200

    Add spaces

--- custom/languages/Dutch/errors.php
+++ custom/languages/Dutch/errors.php
@@ -19,7 +19,7 @@ $language = array(
 	'404_content' => 'We konden de pagina die je zocht niet vinden.',
 	'404_back' => 'Terug?',
 	'404_home' => 'Start',
-	'404_error' => 'Als je denkt dat dit een error is, {x}neem dan contact op met een beheerder van de website{y}', // Verander "{x}" of "{y}" niet
+	'404_error' => 'Als je denkt dat dit een error is, {x} neem dan contact op met een beheerder van de website {y}', // Verander "{x}" of "{y}" niet
 	'maintenance_title' => 'Onderhoud modus.',
  	'maintenance_retry' => 'Probeer opnieuw'
 );
