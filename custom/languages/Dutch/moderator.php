5766f055570b3343a43f8b1dd9a766ed0fd79ffc
RobinMC <robinslot@hotmail.nl>
2017-06-27 17:09:42 +0200

    Credits

--- custom/languages/Dutch/moderator.php
+++ custom/languages/Dutch/moderator.php
@@ -1,7 +1,7 @@
 <?php 
 /*
  *	Gemaakt door Samerton
- *  en vertaald door Sander Lambrechts
+ *  en vertaald door Sander Lambrechts en Derkades
  *  
  *  https://github.com/NamelessMC/Nameless/
  *  NamelessMC version 2.0.0-pr2
