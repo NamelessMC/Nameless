f9e495ebf9aaf33367acf028007ccabd8e094611
Sam <samerton@users.noreply.github.com>
2017-07-09 22:32:46 +0100

    Fix Czech translation typo, thanks to Renzotom

--- custom/languages/Czech/errors.php
+++ custom/languages/Czech/errors.php
@@ -2,11 +2,11 @@
 /*
  *	Made by Samerton
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-dev
+ *  NamelessMC version 2.0.0-pr2
  *
  *  License: MIT
  *
- *  EnglishUS Language - Errors
+ *  Czech Language - Errors
  */
 
 $language = array(
@@ -16,8 +16,8 @@ $language = array(
 	'404_title' => 'Promň!',
 	'404_content' => 'Ale tuto stránku nemůžeme najít :(',
 	'404_back' => 'Vrátit zpět',
-	'404_home' => 'Donů',
+	'404_home' => 'Domů',
 	'404_error' => 'Pokud se domníváte, že je to chyba, {x}prosím kontaktujte administrátora{y}', // Don't replace {x} or {y}
 	'maintenance_title' => 'Maintenance mode.',
-    'maintenance_retry' => 'Retry'
+	'maintenance_retry' => 'Retry'
 );
