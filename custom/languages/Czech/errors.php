3ffbfdc08c0855f2aff5728184b3cde83ede1b13
Sam <samerton@users.noreply.github.com>
2017-07-10 12:22:11 +0100

    Add missing terms to Czech translation

--- custom/languages/Czech/errors.php
+++ custom/languages/Czech/errors.php
@@ -1,6 +1,6 @@
 <?php 
 /*
- *	Made by Samerton
+ *	Made by Samerton, translated by Zemos
  *  https://github.com/NamelessMC/Nameless/
  *  NamelessMC version 2.0.0-pr2
  *
--- custom/languages/Czech/table.php
+++ custom/languages/Czech/table.php
@@ -1,12 +1,12 @@
 <?php 
 /*
- *	Made by Samerton
+ *	Made by Samerton, translated by Zemos
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-dev
+ *  NamelessMC version 2.0.0-pr2
  *
  *  License: MIT
  *
- *  EnglishUS Language - Table
+ *  Czech Language - Table
  */
 
 $language = array(
