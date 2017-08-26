decd1039476fe03db8eba3244530224c6a09ab36
Eftimie Andrei <bax@baxandrei.ro>
2017-08-26 18:51:50 +0300

    Update general.php
    Aligned all variables with same spacing.

--- custom/languages/Romanian/general.php
+++ custom/languages/Romanian/general.php
@@ -1,6 +1,6 @@
 <?php 
 /*
- *	Made by Samerton
+ *  Made by Samerton
  *  https://github.com/NamelessMC/Nameless/
  *  NamelessMC version 2.0.0-dev
  *
@@ -12,45 +12,45 @@
  */
 
 $language = array(
-	/*
-	 *  General terms
-	 */ 
-	'submit' => 'Trimite',
-	'register' => 'Înregistrează-te ',
-	'sign_in' => 'Conectează-te',
-	'log_out' => 'Deconectează-te',
-	'profile' => 'Profil',
-	'invalid_token' => 'Token invalid, încercați din nou.',
-	'close' => 'Închide',
-	'back' => 'Înapoi',
-	'warning' => 'Avertisment',
-	'date' => 'Data',
-	'search' => 'Căutare',
-	'edit' => 'Editați',
-	'next' => 'Înainte',
-	'confirm_cancel' => 'Sigur doriți să anulați?',
-	'confirm_delete' => 'Confirmă ștergerea',
-	'confirm_deletion' => 'Sigur doriți să ștergeți acest lucru?',
-	'cancel' => 'Anulare',
-	'delete' => 'Șterge',
-	'none' => 'Nimic',
-	'view' => 'Vezi',
-	'change' => 'Modifică',
-	'remove' => 'Şterge',
-	'info' => 'Informații',
-	'confirm' => 'Confirmă',
-	'actions' => 'Acţiuni',
-	'alerts' => 'Alerte',
-	'view_alerts' => 'Vizualizați alertele',
-	'no_alerts' => 'Nu există alerte',
-	'x_new_alerts' => 'Aveți {x} alerte noi.', // Don't replace {x}
-	'messages' => 'Mesaje',
-	'view_messages' => 'Vizualizați mesajele',
-	'no_messages' => 'Nu există mesaje',
-	'x_new_messages' => 'Aveți {x} mesaje noi.', // Don't replace {x}
-	'loading' => 'Se încarcă...',
-	'report' => 'Raportează',
-	'page_loaded_in' => 'Pagină incărcată în  {x} secunde.', // Don't replace {x}, time unit is seconds
+    /*
+     *  General terms
+     */ 
+    'submit' => 'Trimite',
+    'register' => 'Înregistrează-te ',
+    'sign_in' => 'Conectează-te',
+    'log_out' => 'Deconectează-te',
+    'profile' => 'Profil',
+    'invalid_token' => 'Token invalid, încercați din nou.',
+    'close' => 'Închide',
+    'back' => 'Înapoi',
+    'warning' => 'Avertisment',
+    'date' => 'Data',
+    'search' => 'Căutare',
+    'edit' => 'Editați',
+    'next' => 'Înainte',
+    'confirm_cancel' => 'Sigur doriți să anulați?',
+    'confirm_delete' => 'Confirmă ștergerea',
+    'confirm_deletion' => 'Sigur doriți să ștergeți acest lucru?',
+    'cancel' => 'Anulare',
+    'delete' => 'Șterge',
+    'none' => 'Nimic',
+    'view' => 'Vezi',
+    'change' => 'Modifică',
+    'remove' => 'Şterge',
+    'info' => 'Informații',
+    'confirm' => 'Confirmă',
+    'actions' => 'Acţiuni',
+    'alerts' => 'Alerte',
+    'view_alerts' => 'Vizualizați alertele',
+    'no_alerts' => 'Nu există alerte',
+    'x_new_alerts' => 'Aveți {x} alerte noi.', // Don't replace {x}
+    'messages' => 'Mesaje',
+    'view_messages' => 'Vizualizați mesajele',
+    'no_messages' => 'Nu există mesaje',
+    'x_new_messages' => 'Aveți {x} mesaje noi.', // Don't replace {x}
+    'loading' => 'Se încarcă...',
+    'report' => 'Raportează',
+    'page_loaded_in' => 'Pagină incărcată în  {x} secunde.', // Don't replace {x}, time unit is seconds
     'page_x' => 'Pagina {x}',
     'browse' => 'Caută',
 
@@ -63,22 +63,22 @@ $language = array(
     'contact_message_failed' => 'Mesaj imposibil de trimis. Asigurați-vă că ați introdus un mesaj între 10 și 5000 de caractere.',
     'contact_message_sent' => 'Mesaj trimis cu succes.',
     'contact_message_limit' => 'Puteți trimite doar un mesaj pe oră. Aveți posibilitatea să trimiteți un nou mesaj în {x} minute.', // Don't replace {x}
-	
-	/* 
-	 *  Navbar
-	 */
-	'home' => 'Acasă',
-	
-	/*
-	 *  Homepage
-	 */
-	'social' => 'Social',
-	
-	
-	/*
-	 *  Help dialogues
-	 */
-	'markdown_help' => 'Puteți să vă formatați postarea folosind Markdown. Pentru mai multe informații, vedeți <a href=\'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet\' target=\'_blank\'>aici</a>.',
+    
+    /* 
+     *  Navbar
+     */
+    'home' => 'Acasă',
+    
+    /*
+     *  Homepage
+     */
+    'social' => 'Social',
+    
+    
+    /*
+     *  Help dialogues
+     */
+    'markdown_help' => 'Puteți să vă formatați postarea folosind Markdown. Pentru mai multe informații, vedeți <a href=\'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet\' target=\'_blank\'>aici</a>.',
 
     /*
      *  Server query
