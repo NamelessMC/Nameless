e9504be3934ebfd401c4e64d1481e0daa7bdbd6d
Eftimie Andrei <bax@baxandrei.ro>
2017-08-26 18:55:23 +0300

    Update time.php
    Aligned all variables with same spacing.

--- custom/languages/Romanian/time.php
+++ custom/languages/Romanian/time.php
@@ -1,6 +1,6 @@
 <?php 
 /*
- *	Made by Samerton
+ *  Made by Samerton
  *  https://github.com/NamelessMC/Nameless/
  *  NamelessMC version 2.0.0-dev
  *
@@ -12,20 +12,20 @@
  */
 
 $language = array(
-	/*
-	 *  Time
-	 */
-	'time' => 'time', // Placeholder, no need to translate
-	'seconds_short' => 's', // Shortened "seconds", eg "s"
-	'less_than_a_minute' => 'în urmă cu mai puțin de un minut',
-	'1_minute' => 'Acum 1 minut',
-	'_minutes' => 'Acum {x} minute',
-	'about_1_hour' => 'Acum aproximativ o oră',
-	'_hours' => 'Acum {x} ore',
-	'1_day' => 'Acum o zi',
-	'_days' => 'Acum {x} zile',
-	'about_1_month' => 'Acum aproximativ o lună',
-	'_months' => 'Acum {x} luni',
-	'about_1_year' => 'Acum aproximativ un an',
-	'over_x_years' => 'Acum {x} ani'
+    /*
+     *  Time
+     */
+    'time' => 'time', // Placeholder, no need to translate
+    'seconds_short' => 's', // Shortened "seconds", eg "s"
+    'less_than_a_minute' => 'în urmă cu mai puțin de un minut',
+    '1_minute' => 'Acum 1 minut',
+    '_minutes' => 'Acum {x} minute',
+    'about_1_hour' => 'Acum aproximativ o oră',
+    '_hours' => 'Acum {x} ore',
+    '1_day' => 'Acum o zi',
+    '_days' => 'Acum {x} zile',
+    'about_1_month' => 'Acum aproximativ o lună',
+    '_months' => 'Acum {x} luni',
+    'about_1_year' => 'Acum aproximativ un an',
+    'over_x_years' => 'Acum {x} ani'
 );
