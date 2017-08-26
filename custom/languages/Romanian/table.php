152ccd1adcac23e5842503b928b7920fdd22fdef
Eftimie Andrei <bax@baxandrei.ro>
2017-08-26 18:54:47 +0300

    Update table.php
    Aligned all variables with same spacing.

--- custom/languages/Romanian/table.php
+++ custom/languages/Romanian/table.php
@@ -1,6 +1,6 @@
 <?php 
 /*
- *	Made by Samerton
+ *  Made by Samerton
  *  https://github.com/NamelessMC/Nameless/
  *  NamelessMC version 2.0.0-dev
  *
@@ -12,12 +12,12 @@
  */
 
 $language = array(
-	/*
-	 *  Tables
-	 */
-	'display_records_per_page' => ' Afiseaza&nbsp; _MENU_ &nbsp;rezultate per pagină ', // Don't replace "_MENU_"
-	'nothing_found' => 'Nu a fost gasit nici un rezultat',
-	'page_x_of_y' => 'Afişare pagina _PAGE_ din _PAGES_', // Don't replace "_PAGE_" or "_PAGES_"
-	'no_records' => 'Nu există înregistrări disponibile',
-	'filtered' => '(filtrat din _MAX_ înregistrări totale)' // Don't replace "_MAX_"
+    /*
+     *  Tables
+     */
+    'display_records_per_page' => ' Afiseaza&nbsp; _MENU_ &nbsp;rezultate per pagină ', // Don't replace "_MENU_"
+    'nothing_found' => 'Nu a fost gasit nici un rezultat',
+    'page_x_of_y' => 'Afişare pagina _PAGE_ din _PAGES_', // Don't replace "_PAGE_" or "_PAGES_"
+    'no_records' => 'Nu există înregistrări disponibile',
+    'filtered' => '(filtrat din _MAX_ înregistrări totale)' // Don't replace "_MAX_"
 );
