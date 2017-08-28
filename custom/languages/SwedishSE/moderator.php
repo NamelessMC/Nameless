dd03efb4903ee8f2af52f2007ebffbf64d14a902
IsS127 <saker.web1@gmail.com>
2017-08-29 00:05:59 +0200

    Swedish Language [Not complete]
    I started on the swedish language, its not 100% completed.

---
+++ custom/languages/SwedishSE/moderator.php
@@ -0,0 +1,54 @@
+<?php 
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-dev
+ *
+ *  License: MIT
+ *
+ *  Translated by IsS127
+ *  SwedishSE Language - Moderator terms
+ */
+
+$language = array(
+	'mod_cp' => 'ModKP',
+	'overview' => 'Översikt',
+	
+	// Spam
+	'spam' => 'Spam',
+	'mark_as_spam' => 'Markera som Spam',
+	'confirm_spam' => '<p>Är du säker på att du vill markera den här användaren som spam?</p><p>Användaren kommer att vara IP bannat och allt innehåll kommer att tas bort.</p>',
+	'user_marked_as_spam' => 'Användaren markerad som spam.',
+	'cant_ban_admin' => 'Du kan inte banna en administrator',
+	
+	// IP Lookup
+	'ip_lookup' => 'IP Sökning',
+	'search_for_ip' => 'Sök efter en användarnamn eller ett IP adress:',
+	'no_ips_with_username' => 'Inga IP Adresser hittades för den användraren.',
+	'no_accounts_with_that_ip' => 'Inga konton för den IP Adressen hittades.',
+	'count_accounts_with_ip' => 'Hittat {x} kontor med IP {y}', // Don't replace "{x}" or "{y}"
+	'count_ips_with_name' => 'Hittat {x} IP Adress(er) för användraren, {y}', // Don't replace "{x}" or "{y}"
+	'no_users_or_ips_found' => 'Inga användare eller IP Adresser hittades.',
+	
+	// Reports
+	'reports' => 'Anmälningar',
+	'report_alert' => 'Ny anmälning skickad',
+	'user_reported' => 'Anmält Användrare',
+	'comments' => 'Kommentarer',
+	'updated_by' => 'Updaterad av',
+	'actions' => 'Handlingar',
+	'view_closed' => 'Visa Stängda',
+	'view_open' => 'Visa Öppna',
+	'viewing_report' => 'Visar Anmälning',
+	'view_content' => 'Visa Anmälnings innehåll',
+	'no_comments' => 'Inga kommentar',
+	'new_comment' => 'Ny Kommentar',
+	'report_comment_invalid' => 'Ogiltigt kommentar innehåll. Se till att du har skrivit en kommentar mellan 1 och 10000 tecken.',
+	'close_report' => 'Stäng Anmälning',
+	'reopen_report' => 'Öppna Anmälning',
+	'open_reports' => 'Det finns för närvarande <strong>{x}</strong> öppnade anmälningar.', // Don't replace {x}
+	
+	// Punishments
+	'punishments' => 'Straffar'
+
+);
---
+++ custom/languages/SwedishSE/time.php
@@ -0,0 +1,30 @@
+<?php 
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-dev
+ *
+ *  License: MIT
+ *
+ *  Translated by IsS127
+ *  SwedishSE Language - Time
+ */
+
+$language = array(
+	/*
+	 *  Time
+	 */
+	'time' => 'Tid', // Placeholder, no need to translate
+	'seconds_short' => 's', // Shortened "seconds", eg "s"
+	'less_than_a_minute' => 'mindre än en minut sedan',
+	'1_minute' => '1 minut sen',
+	'_minutes' => '{x} minuter sen',
+	'about_1_hour' => 'ungefär 1 timme sen',
+	'_hours' => '{x} timmar sen',
+	'1_day' => '1 dag sen',
+	'_days' => '{x} dagar sen',
+	'about_1_month' => 'ungefär 1 månad sen',
+	'_months' => '{x} månader sen',
+	'about_1_year' => 'ungefär ett år sedan',
+	'over_x_years' => 'över {x} år sen'
+);
