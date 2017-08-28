df82eb032ace640a50722fb9c8a058cced363ab3
IsS127 <saker.web1@gmail.com>
2017-08-29 00:05:59 +0200

    Swedish Language [Not complete]
    I started on the swedish language, its not 100% completed.

---
+++ custom/languages/SwedishSE/general.php
@@ -0,0 +1,91 @@
+<?php 
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-dev
+ *
+ *  License: MIT
+ *
+ *  EnglishUK Language - General terms
+ */
+
+$language = array(
+	/*
+	 *  General terms
+	 */ 
+	'submit' => 'Submit',
+	'register' => 'Register',
+	'sign_in' => 'Log In',
+	'log_out' => 'Log Out',
+	'profile' => 'Profile',
+	'invalid_token' => 'Invalid token, please try again.',
+	'close' => 'Close',
+	'back' => 'Back',
+	'warning' => 'Warning',
+	'date' => 'Date',
+	'search' => 'Search',
+	'edit' => 'Edit',
+	'next' => 'Next',
+	'confirm_cancel' => 'Are you sure you wish to cancel?',
+	'confirm_delete' => 'Confirm deletion',
+	'confirm_deletion' => 'Are you sure you wish to delete this?',
+	'cancel' => 'Cancel',
+	'delete' => 'Delete',
+	'none' => 'None',
+	'view' => 'View',
+	'change' => 'Change',
+	'remove' => 'Remove',
+	'info' => 'Info',
+	'confirm' => 'Confirm',
+	'actions' => 'Actions',
+	'alerts' => 'Alerts',
+	'view_alerts' => 'View alerts',
+	'no_alerts' => 'No alerts',
+	'x_new_alerts' => 'You have {x} new alerts.', // Don't replace {x}
+	'messages' => 'Messages',
+	'view_messages' => 'View messages',
+	'no_messages' => 'No messages',
+	'x_new_messages' => 'You have {x} new messages.', // Don't replace {x}
+	'loading' => 'Loading...',
+	'report' => 'Report',
+	'page_loaded_in' => 'Page loaded in {x}s', // Don't replace {x}, time unit is seconds
+	'page_x' => 'Page {x}',
+	'browse' => 'Browse',
+
+	/*
+	 *  Contact form
+	 */
+    'contact' => 'Contact',
+    'message' => 'Message',
+	'contact_email_subject' => 'New contact form submission',
+	'contact_message_failed' => 'Unable to send message. Please ensure you have entered a message between 10 and 5000 characters long.',
+	'contact_message_sent' => 'Message sent successfully.',
+	'contact_message_limit' => 'You can only send one message per hour. You can next send a message in {x} minutes.', // Don't replace {x}
+	
+	/* 
+	 *  Navbar
+	 */
+	'home' => 'Home',
+	
+	/*
+	 *  Homepage
+	 */
+	'social' => 'Social',
+	
+	/*
+	 *  Help dialogues
+	 */
+	'markdown_help' => 'You can format your post using Markdown. For more information, see <a href=\'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet\' target=\'_blank\'>here</a>.',
+
+    /*
+     *  Server query
+     */
+    'online' => 'Online',
+    'offline' => 'Offline',
+    'player_count' => '{x}/{y}', // Don't replace {x} or {y}
+    'ping' => 'Ping: {x}', // Don't replace {x}
+    'version' => 'Version: {x}', // Don't replace {x}
+    'currently_x_players_online' => 'There are currently <strong>{x}</strong> players online.', // Don't replace {x}
+    'server_offline' => 'The server is currently offline.',
+    'connect_with_ip_x' => 'Connect now using the IP <strong>{x}</strong>' // Don't replace {x}
+);
