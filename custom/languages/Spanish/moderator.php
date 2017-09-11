caacc008e2a447e83f677eb60dcce13c52aade46
Sam <samerton@users.noreply.github.com>
2017-09-12 00:09:27 +0100

    Add Spanish translation
    Add Spanish translation, credit to TheSuperSkills

---
+++ custom/languages/Spanish/moderator.php
@@ -0,0 +1,53 @@
+<?php 
+/*
+ *	Made by TheSuperSkills
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr2
+ *
+ *  License: MIT
+ *
+ *  Spanish Language - Moderator terms
+ */
+
+$language = array(
+	'mod_cp' => 'Panel de Moderacion',
+	'overview' => 'Vision de Conjunto',
+	
+	// Spam
+	'spam' => 'Spam',
+	'mark_as_spam' => 'Marcar como correo no deseado',
+	'confirm_spam' => '<p>Estás seguro de que quieres marcar a este usuario como spam?</p><p>El usuario se prohibirá IP, y todo su contenido se eliminará.</p>',
+	'user_marked_as_spam' => 'Usuario marcado como spam correctamente.',
+	'cant_ban_admin' => 'No se puede prohibir a un administrador!',
+	
+	// IP Lookup
+	'ip_lookup' => 'Buscar IP',
+	'search_for_ip' => 'Buscar un nombre de usuario o una dirección IP:',
+	'no_ips_with_username' => 'No hay direcciones IP para ese usuario.',
+	'no_accounts_with_that_ip' => 'No accounts for that IP address found.',
+	'count_accounts_with_ip' => 'Encontró {x} cuentas con la IP {y}', // Don't replace "{x}" or "{y}"
+	'count_ips_with_name' => 'Encontró {x} Dirección(es) IP para el usuario {y}', // Don't replace "{x}" or "{y}"
+	'no_users_or_ips_found' => 'No se han encontrado usuarios ni direcciones IP.',
+	
+	// Reports
+	'reports' => 'Reportes',
+	'report_alert' => 'Nuevo informe presentado',
+	'user_reported' => 'Usuario notificado',
+	'comments' => 'Comentarios',
+	'updated_by' => 'Actualizado por',
+	'actions' => 'Comportamiento',
+	'view_closed' => 'Ver cerrado',
+	'view_open' => 'Ver abierto',
+	'viewing_report' => 'Visualización de reportes',
+	'view_content' => 'Ver contenido reportado',
+	'no_comments' => 'Sin comentarios',
+	'new_comment' => 'Nuevos comentarios',
+	'report_comment_invalid' => 'Contenido de comentario no válido. Asegúrate de haber ingresado un comentario entre 1 y 10000 caracteres.',
+	'close_report' => 'Cerrar reporte',
+	'reopen_report' => 'Reabrir reporte',
+	'open_reports' => 'Hay actualmente <strong>{x}</strong> reportes abiertos.', // Don't replace {x}
+	
+	// Punishments
+	'punishments' => 'Castigos'
+
+);
---
+++ custom/languages/Spanish/version.php
@@ -0,0 +1,14 @@
+<?php 
+/*
+ *	Made by TheSuperSkills
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr2
+ *
+ *  License: MIT
+ *
+ *  Spanish Language - Language version
+ */
+
+// Which version of NamelessMC is this language file updated to?
+$language_version = '2.0.0-pr2';
+$language_html = 'es';
