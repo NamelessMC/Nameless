3337c46fb6a8c6bb0daf9705ef32a2ce4be74bb1
Sam <samerton@users.noreply.github.com>
2017-10-11 13:19:12 +0100

    Allow spaces in alphanumeric check

--- modules/Forum/classes/Forum.php
+++ modules/Forum/classes/Forum.php
@@ -516,7 +516,7 @@ class Forum {
 	// Transform a topic title to URL-ify it
 	public function titleToURL($topic = null){
 		if($topic){
-		    $topic = preg_replace("/[^A-Za-z0-9]/", '', $topic);
+		    $topic = preg_replace("/[^A-Za-z0-9 ]/", '', $topic);
             return Output::getClean(strtolower(urlencode(str_replace(' ', '-', htmlspecialchars_decode($topic)))));
         }
 
