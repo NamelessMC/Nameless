50145600b2e36635d1ce387b0c6a052887411a53
Sam <samerton@users.noreply.github.com>
2017-11-22 23:43:04 +0000

    Release version 1.0.19
    Add update files and update installer for v1.0.19

--- pages/install.php
+++ pages/install.php
@@ -662,7 +662,7 @@ if(isset($_GET["step"])){
 							// Version update
 							$version_id = $queries->getWhere('settings', array('name', '=', 'version'));
 							$queries->update('settings', $version_id[0]->id, array(
-								'value' => '1.0.18'
+								'value' => '1.0.19'
 							));
 
 
@@ -1043,7 +1043,7 @@ if(isset($_GET["step"])){
 					),
 					28 => array(
 						'name' => 'version',
-						'value' => '1.0.18'
+						'value' => '1.0.19'
 					),
 					29 => array(
 						'name' => 'version_checked',
