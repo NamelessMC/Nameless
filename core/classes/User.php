e9bcbb747e89926019ba703a983a3068e2f56672
TortleWortle <bas.kuipers@me.com>
2017-12-15 21:26:04 +0100

    Now supports ip ranges.

--- core/classes/User.php
+++ core/classes/User.php
@@ -90,14 +90,51 @@ class User {
             $usingproxy = true;
 		}
 		if($usingproxy) {
-            $proxyList = explode(',', Config::get("allowedProxies"));
-            if(in_array($_SERVER['REMOTE_ADDR'], $proxyList)) {
-                return $proxiedIp;
+		    $proxyTextList = Config::get("allowedProxies");
+		    $hasRanges = false;
+            if ( strpos( $proxyTextList, '/' ) == true ) {
+                $hasRanges = true;
             }
+            $proxyList = explode(',', $proxyTextList);
+            if($hasRanges) { // Check if we're using ranges (needed for cloudflare)
+                foreach($proxyList as $proxy) {
+                    if ( strpos( $proxy, '/' ) == true ) { // check if it's an actual range (we're lazy so we just check for an / in it)
+                        if($this->ip_in_range($ip, $proxy)) {
+                            return $proxiedIp;
+                        }
+                    }
+                }
+            }else {
+                if (in_array($_SERVER['REMOTE_ADDR'], $proxyList)) {
+                    // Return proxied ip if it's in the list and there's no ranges
+                    return $proxiedIp;
+                }
+            }
+            // Return normal ip if it's not in the list
             return $ip;
         }
+        // Return normal ip if not using proxy
 		return $ip;
 	}
+    /**
+     * Check if a given ip is in a network
+     * @param  string $ip    IP to check in IPV4 format eg. 127.0.0.1
+     * @param  string $range IP/CIDR netmask eg. 127.0.0.0/24, also 127.0.0.1 is accepted and /32 assumed
+     * @return boolean true if the ip is in this range / false if not.
+     * https://gist.github.com/tott/7684443
+     */
+    function ip_in_range( $ip, $range ) {
+        if ( strpos( $range, '/' ) == false ) {
+            $range .= '/32';
+        }
+        // $range is in IP/CIDR format eg 127.0.0.1/24
+        list( $range, $netmask ) = explode( '/', $range, 2 );
+        $range_decimal = ip2long( $range );
+        $ip_decimal = ip2long( $ip );
+        $wildcard_decimal = pow( 2, ( 32 - $netmask ) ) - 1;
+        $netmask_decimal = ~ $wildcard_decimal;
+        return ( ( $ip_decimal & $netmask_decimal ) == ( $range_decimal & $netmask_decimal ) );
+    }
 
 	// Update a user's data
 	public function update($fields = array(), $id = null) {
