90871cee11188df3d5f98b56291f09abfa8f8cce
Sam <samerton@users.noreply.github.com>
2017-12-19 12:35:31 +0000

    Start Slovak translation
    Start Slovak translation, thanks to JozefSorokac

---
+++ custom/languages/Slovak/moderator.php
@@ -0,0 +1,85 @@
+<?php 
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr2
+ *
+ *  License: MIT
+ *
+ *  EnglishUK Language - Moderator terms
+ */
+
+$language = array(
+	'mod_cp' => 'ModCP',
+	'overview' => 'Overview',
+	
+	// Spam
+	'spam' => 'Spam',
+	'mark_as_spam' => 'Mark as Spam',
+	'confirm_spam' => '<p>Are you sure you want to mark this user as spam?</p><p>The user will be IP banned, and all their content will be removed.</p>',
+	'user_marked_as_spam' => 'User marked as spam successfully.',
+	'cant_ban_admin' => 'You can\'t ban an administrator!',
+	
+	// IP Lookup
+	'ip_lookup' => 'IP Lookup',
+	'search_for_ip' => 'Search for a username or IP address:',
+	'no_ips_with_username' => 'No IP addresses for that user found.',
+	'no_accounts_with_that_ip' => 'No accounts for that IP address found.',
+	'count_accounts_with_ip' => 'Found {x} accounts with the IP {y}', // Don't replace "{x}" or "{y}"
+	'count_ips_with_name' => 'Found {x} IP address(es) for user {y}', // Don't replace "{x}" or "{y}"
+	'no_users_or_ips_found' => 'No users or IP addresses found.',
+	
+	// Reports
+	'reports' => 'Reports',
+	'report_alert' => 'New report submitted',
+	'user_reported' => 'User Reported',
+	'comments' => 'Comments',
+	'updated_by' => 'Updated By',
+	'actions' => 'Actions',
+	'view_closed' => 'View Closed',
+	'view_open' => 'View Open',
+	'viewing_report' => 'Viewing Report',
+	'view_content' => 'View reported content',
+	'no_comments' => 'No comments',
+	'new_comment' => 'New comment',
+	'report_comment_invalid' => 'Invalid comment content. Please ensure you have entered a comment between 1 and 10000 characters.',
+	'close_report' => 'Close report',
+	'reopen_report' => 'Reopen report',
+	'open_reports' => 'There are currently <strong>{x}</strong> open reports.', // Don't replace {x}
+	
+	// Punishments
+	'punishments' => 'Punishments',
+    'view_punishments' => 'View Punishments',
+    'banned' => 'Banned',
+    'groups' => 'Groups',
+    'punish' => 'Punish',
+    'ban' => 'Ban',
+    'warn' => 'Warn',
+    'ban_ip' => 'Ban IP',
+    'viewing_user_x' => 'Viewing user {x}', // Don't replace {x}
+    'previous_punishments' => 'Previous punishments',
+    'no_previous_punishments' => 'No previous punishments',
+    'warning' => 'Warning',
+    'ip_ban' => 'IP Ban',
+    'reason' => 'Reason',
+    'warn_user' => 'Warn User',
+    'ban_user' => 'Ban User',
+    'enter_valid_punishment_reason' => 'Please enter a valid reason for your punishment, between 5 and 5000 characters.',
+    'user_punished' => 'User punished.',
+    'user_punished_alert' => '{x} has punished user {y}', // Don't replace {x} (staff member) or {y} (user punished)
+    'revoke' => 'Revoke',
+    'revoked' => 'Revoked',
+    'acknowledged' => 'Acknowledged',
+    'confirm_revoke_warning' => 'Are you sure you want to revoke this warning?',
+    'confirm_revoke_ban' => 'Are you sure you want to revoke this ban? The user will be unbanned, even if they have a more recent ban!',
+    'punishment_revoked' => 'Punishment revoked.',
+    'punishment_revoked_alert' => '{x} has revoked a punishment for user {y}', // Don't replace {x} (staff member) or {y} (user with revoked punishment)
+    'cant_punish_admin' => 'You can\'t punish an administrator!',
+    'viewing_all_punishments' => 'Viewing all punishments',
+    'no_punishments_found' => 'No punishments found.',
+    'view_user' => 'View User',
+    'when' => 'When',
+    'staff' => 'Staff',
+    'type' => 'Type'
+
+);
