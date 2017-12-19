be8b441d435ac23030845d65762f6ff9305106e0
Sam <samerton@users.noreply.github.com>
2017-12-19 12:35:31 +0000

    Start Slovak translation
    Start Slovak translation, thanks to JozefSorokac

---
+++ custom/languages/Slovak/errors.php
@@ -0,0 +1,23 @@
+<?php 
+/*
+ *	Made by JozefSorokac
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr3
+ *
+ *  License: MIT
+ *
+ *  Slovak Language - Errors
+ */
+
+$language = array(
+	/*
+	 *  Errors
+	 */
+	'404_title' => 'Ospravedlňujeme sa!',
+	'404_content' => 'Ale túto stránku nemôžeme nájsť :(',
+	'404_back' => 'Vrátiť späť',
+	'404_home' => 'Domov',
+	'404_error' => 'Pokiaľ sa domnievate, že je to chyba, {x}kontaktujte prosím administrátora{y}', // Don't replace {x} or {y}
+	'maintenance_title' => 'Údržba.',
+    'maintenance_retry' => 'Opakovať'
+);
---
+++ custom/languages/Slovak/general.php
@@ -0,0 +1,95 @@
+<?php 
+/*
+ *	Made by JozefSorokac
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr3
+ *
+ *  License: MIT
+ *
+ *  Czech Language - General terms
+ */
+
+$language = array(
+	/*
+	 *  General terms
+	 */ 
+	'submit' => 'Potvrdiť',
+	'register' => 'Registrovať sa',
+	'sign_in' => 'Prihlásiť sa',
+	'log_out' => 'Odhlásiť sa',
+	'profile' => 'Profil',
+	'invalid_token' => 'Chyba, skuste to prosím znovu.',
+	'close' => 'Zavrieť',
+	'back' => 'Späť',
+	'warning' => 'Varovanie',
+	'date' => 'Dátum',
+	'search' => 'Hľadať',
+	'edit' => 'Upraviť',
+	'next' => 'Ďalšie',
+	'confirm_cancel' => 'Naozaj to chcete zrušiť?',
+	'confirm_delete' => 'Potvrdiť zmazanie',
+	'confirm_deletion' => 'Naozaj toto chcete zmazať?',
+	'cancel' => 'Zavrieť',
+	'delete' => 'Zmazať',
+	'none' => 'Žiadný',
+	'view' => 'Zobraziť',
+	'change' => 'Zmeniť',
+	'remove' => 'Odstrániť',
+	'info' => 'Informácia',
+	'confirm' => 'Potvrdiť',
+	'actions' => 'Akcia',
+	'alerts' => 'Upozornenie',
+	'view_alerts' => 'Zobraziť upozornenia',
+	'no_alerts' => 'Nemáte žiadné upozornenia',
+	'x_new_alerts' => 'Máte {x} nových upozornění.', // Don't replace {x}
+	'messages' => 'Správy',
+	'view_messages' => 'Zobraziť správy',
+	'no_messages' => 'Nemáte žiadné správy',
+	'x_new_messages' => 'Máte {x} nových zpráv.', // Don't replace {x}
+	'loading' => 'Načítavanie...',
+	'report' => 'Nahlásiť',
+	'page_loaded_in' => 'Stránka se načetla za {x} sekund', // Don't replace {x}, time unit is seconds
+    'page_x' => 'Strán {x}',
+    'browse' => 'Prehliadať',
+	'bypass' => 'Bypass',
+
+    /*
+     *  Contact form
+     */
+    'contact' => 'Kontakt',
+    'message' => 'Správa',
+    'contact_email_subject' => 'Nový kontaktný formulár ',
+    'contact_message_failed' => 'Nepodarilo sa odoslať zprávu. Zajistěte, aby jste vložil zprávu mezi 10 a 5000 znaky dlouhou.',
+    'contact_message_sent' => 'Správa bola úspešne odoslaná.',
+    'contact_message_limit' => 'Můžete odeslat pouze jednu zprávu za hodinu. Další můžete odeslat za {x} minut.', // Don't replace {x}
+	
+	/* 
+	 *  Navbar
+	 */
+	'home' => 'Domov',
+	'more' => 'Viac',
+	
+	/*
+	 *  Homepage
+	 */
+	'social' => 'Sociálné siete',
+	
+	
+	/*
+	 *  Help dialogues
+	 */
+	'markdown_help' => 'Formát môžete naformátovať pomocou značky Markdown. Dalšie informácie nájdete v časti <a href=\'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet\' target=\'_blank\'>Tu</a>.',
+
+    /*
+     *  Server query
+     */
+    'online' => 'Online',
+    'offline' => 'Offline',
+    'player_count' => '{x}/{y}', // Don't replace {x} or {y}
+    'ping' => 'Ping: {x}', // Don't replace {x}
+    'version' => 'Verzia: {x}', // Don't replace {x}
+    'currently_x_players_online' => 'Aktuálne je na serveri <strong>{x}</strong> online hráčov.', // Don't replace {x}
+    'server_offline' => 'Server je aktuálne vypnutý (offline).',
+    'connect_with_ip_x' => 'Pripojte sa teraz pomocou IP adresy <strong>{x}</strong>' // Don't replace {x}
+);
+
