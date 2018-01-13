2e665a35d7253b59e3a0329b7309ea59bcc26cd7
Sam <samerton@users.noreply.github.com>
2018-01-13 14:47:35 +0000

    Add Chinese translation (credit @haer0248)
    Add Chinese translation, thanks to @haer0248 (https://github.com/haer0248/NamelessMC-v2-Traditional-Chinese)

---
+++ custom/languages/Chinese/time.php
@@ -0,0 +1,33 @@
+<?php 
+ /*
+ *	Made by Samerton
+ *	Translation  by Hi_Michael
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr3
+ *
+ *  License: MIT
+ *
+ *  Chinese Language - Time
+ *  Translation progress : 100%
+ *  翻譯有誤請使用GitHun回報issues 
+ *  https://github.com/haer0248/NamelessMC-v2-Traditional-Chinese/issues
+ */
+
+$language = array(
+	/*
+	 *  Time
+	 */
+	'time' => '時間', // Placeholder, no need to translate
+	'seconds_short' => '秒', // Shortened "seconds", eg "s"
+	'less_than_a_minute' => '分鐘以前',
+	'1_minute' => '1 分鐘以前',
+	'_minutes' => '{x} 分鐘以前',
+	'about_1_hour' => '大約 1 小時以前',
+	'_hours' => '{x} 小時以前',
+	'1_day' => '1 天以前',
+	'_days' => '{x} 天以前',
+	'about_1_month' => '大約 1 個月以前',
+	'_months' => '{x} 個月以前',
+	'about_1_year' => '大約 1 年以前',
+	'over_x_years' => '超過 {x} 年'
+);
---
+++ modules/Forum/language/Chinese/forum.php
@@ -0,0 +1,167 @@
+<?php 
+/*
+ *	Made by Samerton
+ *  Translate By Hi_Michael
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr3
+ *
+ *  License: MIT
+ *
+ *  Chinese Language - Forum
+ *  Translation progress : 100%
+ *  翻譯有誤請使用GitHun回報issues 
+ *  https://github.com/haer0248/NamelessMC-v2-Traditional-Chinese/issues
+ */
+
+$language = array(
+	/*
+	 *  Forums
+	 */ 
+	'forum' => '論壇',
+	'forums' => '論壇',
+	'forum_index' => '首頁',
+	'no_users_online' => '沒有使用者在線上.',
+	'online_users' => '線上使用者',
+	'discussion' => '討論串',
+	'topic' => '主題',
+	'stats' => '統計',
+	'topics' => '主題',
+	'views' => '觀看數',
+	'posts' => '文章',
+	'by' => 'by',
+	'in' => 'in',
+	'last_reply' => '最後回覆',
+	'latest_discussions' => '最新的討論串',
+	'users_registered' => '<strong>使用者已註冊:</strong> {x}', // Don't replace "{x}"
+	'latest_member' => '<strong>歡迎新會員:</strong> {x}', // Don't replace "{x}"
+	'subforums' => '子論壇',
+	'subforum' => '子論壇',
+	'no_topics' => '沒有主題被建立.',
+	'no_topics_short' => '沒有主題',
+	'new_topic' => '新主題',
+	'post_successful' => '成功發文.',
+	'post_edited_successfully' => '文章編輯成功.',
+	'user_tag' => '你被標註在一則文章中.',
+	'user_tag_info' => '你已被標註在 {x} 的文章中.', // Don't replace "{x}"
+	'creating_topic_in_x' => '在 {x} 建立主題', // Don't replace "{x}"
+	'topic_title' => '主題標題',
+	'new_reply' => '新回覆',
+	're' => 'RE: ',
+	'topic_locked' => '主題鎖定',
+	'mod_actions' => '審核',
+	'lock_topic' => '鎖定主題',
+	'unlock_topic' => '解鎖主題',
+	'merge_topic' => '合併主題',
+	'merge_topics' => '合併文章',
+	'merge_instructions' => '合併指令必須只能在一個論壇中. 如有必要請移動一整個線程.',
+	'confirm_delete_short' => '確認移除',
+	'confirm_delete_topic' => '確認移除主題?',
+	'confirm_delete_post' => '確認移除文章?',
+	'delete_topic' => '移除主題',
+	'move_topic' => '移動主題',
+	'move_topic_to' => '移動主題至:',
+	'stick_topic' => '置頂主題',
+	'unstick_topic' => '取消置頂主題',
+	'share' => '分享',
+	'share_twitter' => '在 Twitter 上分享',
+	'share_facebook' => '在 Facebook 上分享',
+	'edit' => '編輯',
+	'edit_post' => '編輯文章',
+	'last_edited' => '最後編輯: {x}', // Don't replace "{x}"
+	'quote' => '引用',
+	'topic_locked_notice' => '主題已鎖定，但是你的權限可以建立回覆.',
+	'title_required' => '請輸入主題標題',
+	'content_required' => '請輸入文章內容',
+	'title_min_2' => '主題標題最低限制 2 字元',
+	'title_max_64' => '主題標題最低限制 64 字元',
+	'content_min_2' => '文章內容最低限制 2 字元',
+	'content_max_20480' => '文章內容最高限制 20480 字元',
+	'post_already_reported' => '你已經回報過這篇文章!',
+	'quoted_post' => '文章已新增至引用.',
+	'removed_quoted_post' => '文章已從引用中移除.',
+	'quoting_posts' => '插入引用..',
+	'error_quoting_posts' => '抱歉，引用文章時出了錯.',
+	'error_rating_post' => '抱歉，文章發生錯誤.',
+	'topic_stuck' => '文章已被置頂.',
+	'topic_unstuck' => '文章已被取消置頂.',
+	'spam_wait' => '請等待 {x} 秒後再發言.',
+	'overview' => '總覽',
+	'no_label' => '沒有標籤',
+	
+	// Homepage
+	'latest_announcements' => '最新公告',
+	'read_full_post' => '閱讀全部',
+	
+	// Admin tab
+	'labels' => '標籤',
+	'new_forum' => '<i class="fa fa-plus-circle"></i> 新論壇',
+	'new_label' => '<i class="fa fa-plus-circle"></i> 新標籤',
+	'new_label_type' => '<i class="fa fa-plus-circle"></i> 新標籤類型',
+	'label_types' => '標籤類型',
+	'creating_label' => '建立新標籤',
+	'creating_label_type' => '建立新標籤類型',
+	'editing_label' => '編輯標籤',
+	'editing_label_type' => '編輯標籤類型',
+	'label_name' => '標籤名稱',
+	'label_type' => '標籤類型',
+	'label_type_name' => '標籤類型名稱',
+	'label_type_html' => '標籤類型HTML',
+	'label_type_html_help' => '標籤必須包含 {x} 做為標籤的佔位符',
+	'label' => '標籤',
+	'label_forums' => '標籤論壇',
+	'label_groups' => '標籤群組',
+	'no_forums' => '沒有論壇',
+	'no_labels_defined' => '無定義標籤.',
+	'no_label_types_defined' => '無定義標籤類型.',
+	'label_edit_success' => '標籤編輯成功.',
+	'label_type_edit_success' => '標籤類型編輯成功.',
+	'label_type_creation_success' => '標籤類型建立成功.',
+	'label_creation_success' => '標籤建立成功.',
+	'label_creation_error' => '無法建立標籤. 請確認輸入的內容沒有超過 32 字元.',
+	'label_type_creation_error' => '無法建立標籤類型. 請確認輸入的內容沒有超過 32 字元，HTML沒有超過 64 字元.',
+	'creating_forum' => '建立新論壇',
+	'forum_name' => '論壇名稱',
+	'forum_description' => '論壇說明',
+	'delete_forum' => '移除論壇',
+	'move_topics_and_posts_to' => '移動主題和文章到',
+	'delete_topics_and_posts' => '移除主題和文章',
+	'forum_permissions' => '論壇權限',
+	'select_a_parent_forum' => '選擇父論壇',
+	'parent_forum' => '父論壇',
+	'has_no_parent' => '沒有父論壇',
+	'guests' => '遊客',
+	'group' => '群組',
+	'can_view_forum' => '可以查看論壇?',
+	'can_view_other_topics' => '可以查看其他人的文章?',
+	'can_create_topic' => '可以建立主題?',
+	'can_post_reply' => '可以建立回覆?',
+	'can_moderate_forum' => '可以審核論壇?',
+	'display_topics_as_news' => '顯示此主題的文章在首頁?',
+	'forum_created_successfully' => '論壇建立成功.',
+	'forum_layout' => '論壇佈局',
+	'table_view' => '統計圖',
+	'latest_discussions_view' => '最新的討論統計圖',
+	'input_forum_title' => 'P請輸入論壇標題.',
+	'input_forum_description' => '請輸入論壇說明.',
+	'forum_name_minimum' => '論壇名稱最低限制 2 字元.',
+	'forum_description_minimum' => '論壇說明最低限制 2 字元.',
+	'forum_name_maximum' => '論壇名稱最高限制 150 字元.',
+	'forum_description_maximum' => '論壇說明最高限制 255 字元.',
+	'forum_type' => '論壇類型',
+	'forum_type_forum' => '論壇',
+	'forum_type_category' => '類別',
+	'invalid_action' => '無效操作',
+	'use_reactions' => '使用回應?',
+
+	// Search
+    'forum_search' => '論壇搜尋',
+	'search_again_in_x_seconds' => '請等待 {x} 秒後再次搜尋.',
+	'search_results' => '搜尋結果',
+	'new_search' => '新搜尋',
+	'invalid_search_query' => '搜尋限制 3~128 字元.',
+	'no_results_found' => '找不到資料.',
+	
+	// Profile tab
+	'user_no_posts' => '使用者尚未在論壇發布貼文.',
+	'latest_posts' => '最新文章'
+);
