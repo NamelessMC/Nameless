da0be0910cac62c3cf41f4b8d9f5e7bf4f892b4e
Sam <samerton@users.noreply.github.com>
2018-01-14 20:09:50 +0000

    Allow custom HTML lang attribute on 404 page

--- 404.php
+++ 404.php
@@ -15,7 +15,7 @@ define('PAGE', 404);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php echo (defined('HTML_LANG') ? HTML_LANG : 'en'); ?>">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
