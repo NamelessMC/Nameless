a97bfae860af1a1feebca79a942d100a776b1a31
Sam <samerton@users.noreply.github.com>
2018-03-13 20:58:18 +0000

    Add Spanish Forum module translation, credit AllPlayed
    Add Spanish translation of the Forum module, thanks to AllPlayed

---
+++ modules/Forum/language/Spanish/forum.php
@@ -0,0 +1,178 @@
+<?php
+/*
+ *	   Made by Samerton
+ *  Translated by AllPlayed
+ *
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr3
+ *
+ *  License: MIT
+ *
+ *  Spanish Language for Forum module
+ */
+
+$language = array(
+	/*
+	 *  Forums
+	 */
+	'forum' => 'Foro',
+	'forums' => 'Foros',
+	'forum_index' => 'Inicio',
+	'no_users_online' => 'No hay usuarios conectados.',
+	'online_users' => 'Usuarios Conectados',
+	'discussion' => 'Discusión',
+	'topic' => 'Tema',
+	'stats' => 'Estadísticas',
+	'topics' => 'temas',
+	'views' => 'vistas',
+	'posts' => 'posts',
+	'x_posts' => '{x} posts', // Don't replace {x}
+	'x_topics' => '{x} temas', // Don't replace {x}
+	'registered_x' => 'Registrado el: {x}', // Don't replace {x}
+	'in' => 'en',
+	'last_reply' => 'Última Respuesta',
+	'latest_discussions' => 'Última Discusión',
+	'users_registered' => '<strong>Usuarios Registrados:</strong> {x}', // Don't replace "{x}"
+	'latest_member' => '<strong>Último Miembro:</strong> {x}', // Don't replace "{x}"
+	'subforums' => 'Subforos',
+	'subforum' => 'Subforo',
+	'no_topics' => 'Aún no hay temas creados aquí.',
+	'no_topics_short' => 'No hay Temas',
+	'new_topic' => 'Nuevo Tema',
+	'post_successful' => 'Post creado correctamente.',
+	'post_edited_successfully' => 'Post editado correctamente.',
+	'user_tag' => 'Has sido tageado en un Post.',
+	'user_tag_info' => 'Has sido tageado en un Post por {x}.', // Don't replace "{x}"
+	'creating_topic_in_x' => 'Creando un tema en {x}', // Don't replace "{x}"
+	'topic_title' => 'Título del Tema',
+	'new_reply' => 'Escribir una nueva Respuesta',
+	're' => 'RE: ',
+	'topic_locked' => 'Tema Bloqueado',
+	'mod_actions' => 'Moderación',
+	'lock_topic' => 'Bloquear Tema',
+	'unlock_topic' => 'Desbloquear Tema',
+	'merge_topic' => 'Juntar Temas',
+	'merge_topics' => 'Juntar el Tema',
+	'merge_instructions' => 'Para juntar los temas es <strong>necesario</strong> que estén en el mismo Foro/Subforo. Mover un Tema es innececesario.',
+	'confirm_delete_short' => 'Confirmar borrado',
+	'confirm_delete_topic' => '¿Estás seguro de que quieres borrar este Tema?',
+	'confirm_delete_post' => '¿Estás seguro de que quieres borrar este Post?',
+	'delete_topic' => 'Borrar Tema',
+	'move_topic' => 'Mover Tema',
+	'move_topic_to' => 'Mover tema a:',
+	'stick_topic' => 'Colocar este tema primero:',
+	'unstick_topic' => 'Quitar el primer tema (no es borrarlo)',
+	'share' => 'Share',
+	'share_twitter' => 'Compartir en Twitter',
+	'share_facebook' => 'Compartir en Facebook',
+	'edit' => 'Editar',
+	'edit_post' => 'Editar Post',
+	'last_edited' => 'Última edición: {x}', // Don't replace "{x}"
+	'quote' => 'Citar',
+	'topic_locked_notice' => 'El tema está bloqueado, pero tus permisos te permiten volver a abrirlo y escribir una nueva respuesta en el.',
+	'title_required' => 'Porfavor, escribe el título del Tema',
+	'content_required' => 'Porfavor, escribe o coloca algo de contenido en el Post/Tema',
+	'title_min_2' => 'El título de tu tema debe tener <strong>mínimo</strong>  2 caracteres',
+	'title_max_64' => 'El título de su tema no debe tener más de 64 caracteres',
+	'content_min_2' => 'El contenido de tu publicación debe tener un mínimo de 2 caracteres',
+	'content_max_20480' => 'El contenido de la publicación no debe superar los 20480 caracteres',
+	'post_already_reported' => '¡Ya has reportado este post!',
+	'quoted_post' => 'Post añadido a posts citados',
+	'removed_quoted_post' => 'Post quitado junto a las citas.',
+	'quoting_posts' => 'Insertando citas...',
+	'error_quoting_posts' => 'Lo sentimos, ha habido un error al citar este post',
+	'error_rating_post' => 'Lo sentimos, ha habido un problema al calificar el post',
+	'topic_stuck' => 'El tema se ha colocado en el primer puesto',
+	'topic_unstuck' => 'El tema ha sido quitado del primer puesto.',
+	'spam_wait' => 'Porfavor, espera {x} segundos antes de postear.',
+	'overview' => 'Vista General',
+	'no_label' => 'Sin Etiqueta',
+
+	// Homepage
+	'latest_announcements' => 'Últimas Noticias',
+	'read_full_post' => 'Seguir leyendo...',
+
+	// Admin tab
+	'labels' => 'Etiquetas',
+	'new_forum' => '<i class="fa fa-plus-circle"></i> Nuevo Foro',
+	'new_label' => '<i class="fa fa-plus-circle"></i> Nueva Etiqueta',
+	'new_label_type' => '<i class="fa fa-plus-circle"></i> Nuevo tipo de etiqueta',
+	'label_types' => 'Tipos de Etiquetas',
+	'creating_label' => 'Creando una nueva Etiqueta',
+	'creating_label_type' => 'Creando un nuevo tipo de Etiqueta',
+	'editing_label' => 'Editando la etiqueta',
+	'editing_label_type' => 'Editando el tipo de etiqueta',
+	'label_name' => 'Nombre de la Etiqueta',
+	'label_type' => 'Tipo de Etiqueta',
+	'label_type_name' => 'Nombre de el tipo de etiqueta',
+	'label_type_html' => 'Tipo de etiqueta HTML',
+	'label_type_html_help' => 'El HTML debe incluir {x} como marcador de posición para el nombre de la etiqueta',
+	'label' => 'Etiqueta',
+	'label_forums' => 'Foros de Etiquetas',
+    'label_groups' => 'Grupos de Etiquetas',
+	'no_forums' => 'No hay Foros',
+	'no_labels_defined' => 'Aún no se han definido las etiquetas',
+	'no_label_types_defined' => 'Aún no se han definido los tipos de etiquetas.',
+	'label_edit_success' => 'Etiqueta modificada correctamente',
+	'label_type_edit_success' => 'Tipo de etiqueta modificada correctamente.',
+	'label_creation_success' => 'Etiqueta creada correctamente.',
+	'label_type_creation_success' => 'Tipo de etiqueta creada correctamente.',
+	'label_creation_error' => 'Error al crear una etiqueta. Asegúrese de que el nombre no tenga más de 32 caracteres y de que haya especificado un tipo.',
+	'label_type_creation_error' => 'Error creando un tipo de etiqueta. Asegúrese de que el nombre no tenga más de 32 caracteres y que el HTML no tenga más de 64 caracteres.',
+	'creating_forum' => 'Creando nuevo Foro',
+	'forum_name' => 'Nombre del Foro',
+	'forum_description' => 'Descripción del Foro',
+	'delete_forum' => 'Borrar Foro',
+	'move_topics_and_posts_to' => 'Mover temas y posts a',
+	'delete_topics_and_posts' => 'Borrar temas y posts',
+	'forum_permissions' => 'Permisos del foro',
+	'select_a_parent_forum' => 'Selecciona un foro padre',
+	'parent_forum' => 'Foro Padre',
+	'has_no_parent' => 'No tiene Padre',
+	'guests' => 'Invitados',
+	'group' => 'Grupo',
+	'can_view_forum' => '¿Puede ver el foro?',
+	'can_view_other_topics' => '¿Puede mirar los temas de los otros usuarios?',
+	'can_create_topic' => '¿Puede crear temas?',
+	'can_post_reply' => '¿Puede responder a los temas?',
+	'can_moderate_forum' => '¿Puede moderar el foro?',
+	'display_topics_as_news' => '¿Mostrar posts como Noticias en la página principal?',
+	'forum_created_successfully' => 'Foro creado correctamente.',
+	'forum_layout' => 'Layout del Foro',
+	'table_view' => 'Vista de Tablas',
+	'latest_discussions_view' => 'Ver las últimas discusiones',
+	'input_forum_title' => 'Porfavor, escribe un nombre al Foro.',
+	'input_forum_description' => 'Porfavor, escribe una descripción al Foro.',
+	'forum_name_minimum' => 'El nombre del foro debe de tener un mínimo de 2 caracteres.',
+	'forum_description_minimum' => 'La descripción del foro debe de tener un mínimo de 2 caracteres.',
+	'forum_name_maximum' => 'El nombre del foro debe de tener un máximo de 150 caracteres.',
+	'forum_description_maximum' => 'La descripción del foro debe de tener un máximo de 255 caracteres.',
+	'forum_type' => 'Tipo de Foro',
+	'forum_type_forum' => 'Foro',
+	'forum_type_category' => 'Categoría',
+	'invalid_action' => 'Acción inválida',
+	'use_reactions' => 'Usar reacciones?',
+
+    // Search
+    'forum_search' => 'Busqueda en el Foro',
+    'search_again_in_x_seconds' => 'Por favor, espera {x} segundos antes de volver a buscar.',
+    'search_results' => 'Resúltados de tu busqueda',
+    'new_search' => 'Hacer una nueva busqueda',
+    'invalid_search_query' => 'Por favor, haz una busqueda entre 3 y 128 caracteres.',
+    'no_results_found' => 'No se han encontrado resultados',
+
+	// Profile tab
+	'user_no_posts' => 'Este usuario, todavía no ha hecho ningun post/tema',
+	'latest_posts' => 'Últimos Posts',
+
+	// UserCP
+	'last_7_days_posts' => 'Posts del Foro (últimos 7 días)',
+	'your_posts' => 'Tu conteo de publicaciones',
+	'average_posts' => 'Recuento promedio de publicaciones de usuarios',
+	'total_posts' => 'Recuento total de publicaciones',
+
+    // Hooks
+    'new_topic_hook_info' => 'Nuevo Tema',
+    'new_topic_text' => 'Tema creado en {x} por {y}', // Don't replace {x} (forum name), optional variable {y} (topic author)
+    'include_in_hook' => '¿Incluye nuevos temas de este foro en el webhook?'
+);
