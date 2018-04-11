578c5c4ab3e932259fd83f06c248edac5c86973d
Sam <samerton@users.noreply.github.com>
2018-04-11 15:48:10 +0100

    Fix signin details being remembered if tfa is cancelled
    Fix signin details being remembered if tfa is cancelled

--- core/includes/tfa_signin.php
+++ core/includes/tfa_signin.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 /*
  *	Made by Samerton
  *  http://worldscapemc.co.uk
  *
  *  License: MIT
  */
- 
+
 // Two Factor Auth signin
 $_SESSION['username'] = Input::get('username');
 $_SESSION['password'] = Input::get('password');
@@ -20,22 +20,22 @@ $_SESSION['remember'] = Input::get('remember');
     <meta name="description" content="<?php echo $sitename; ?> sign in page">
     <meta name="author" content="Samerton">
 	<?php if(isset($custom_meta)){ echo $custom_meta; } ?>
-	
+
 	<?php
 	// Generate header and navbar content
 	// Page title
 	$title = $user_language['sign_in'];
-	
+
 	require('core/includes/template/generate.php');
 	?>
-	
+
 	<!-- Custom style -->
 	<style>
 	html {
 		overflow-y: scroll;
 	}
 	</style>
-	
+
   </head>
   <body>
     <div class="container">
@@ -45,6 +45,7 @@ $_SESSION['remember'] = Input::get('remember');
 		  <?php if(Session::exists('tfa_signin')) echo Session::flash('tfa_signin'); ?>
 		  <p><?php if($user_query[0]->tfa_type == 1) echo $user_language['tfa_enter_code']; else echo $user_language['tfa_enter_email_code']; ?></p>
 		  <input type="text" class="form-control" name="tfa_code">
+		  <input type="hidden" name="tfa" value="true">
 		  <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
 		  <br />
 		  <input type="submit" value="<?php echo $general_language['submit']; ?>" class="btn btn-primary">
