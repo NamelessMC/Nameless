5ba482db231aab7e83ac4559451287d34310563e
Sam <samerton@users.noreply.github.com>
2018-04-25 18:33:32 +0100

    Update 404 back button to work on Safari
    Update 404 back button to work on Safari

--- 404.php
+++ 404.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
  *	Made by Samerton
  *  http://worldscapemc.co.uk
@@ -7,7 +7,7 @@
  *  Copyright (c) 2016 Samerton
  */
 
-header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found"); 
+header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
 
 ?>
 
@@ -22,7 +22,7 @@ header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
     <link rel="icon" href="/core/assets/favicon.ico">
 
     <title>404 &bull; <?php echo $sitename; ?></title>
-	
+
 	<link href="/core/assets/css/bootstrap.min.css" rel="stylesheet">
 
   </head>
@@ -33,7 +33,7 @@ header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
 	    <h1>404</h1>
 		<h4><?php echo $general_language['404']; ?></h4>
 		<div class="btn-group" role="group" aria-label="...">
-		  <a href="#" class="btn btn-primary btn-lg" onclick="window.history.back()"><?php echo $general_language['back']; ?></a>
+		  <button class="btn btn-primary btn-lg" onclick="javascript:history.go(-1)"><?php echo $general_language['back']; ?></button>
 		  <a href="/" class="btn btn-success btn-lg"><?php echo $navbar_language['home']; ?></a>
 	    </div>
 	  </div>
