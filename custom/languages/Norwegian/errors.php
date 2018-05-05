fadc04075386b907e792777167532907e8b1a9d6
Thesevs <shekabbi@hotmail.no>
2018-05-05 14:41:33 +0200

    translated everything

--- custom/languages/Norwegian/errors.php
+++ custom/languages/Norwegian/errors.php
@@ -1,12 +1,12 @@
 <?php 
 /*
- *	Oversatt av Thesevs
+ *  Translations by Maiu | Discord: Maiu#1152
  *  https://github.com/NamelessMC/Nameless/
  *  NamelessMC versjon 2.0.0-pr3
  *
  *  License: MIT
  *
- *  Norsk språk - Error
+ *  Norsk oversettelse - Error
  */
 
 $language = array(
@@ -17,13 +17,13 @@ $language = array(
 	'404_content' => 'Vi kunne ikke finne denne siden!',
 	'404_back' => 'Tilbake',
 	'404_home' => 'Hjem',
-	'404_error' => 'Hvis du mener dette er en feil, {x}vennligst kontakt administrasjonen{y}', // Don't replace {x} or {y}
+	'404_error' => 'Hvis du mener dette er en feil, {x}ber vi deg kontakte en administrator{y}', // Don't replace {x} or {y}
 	'maintenance_title' => 'Vedlikeholdsmodus',
 	'maintenance_retry' => 'Prøv igjen',
 	'fatal_error' => 'Fatal Error',
 	'fatal_error_title' => 'Sorry!',
-	'fatal_error_message_user' => 'Sorry, but something went wrong while loading the page. Please contact an administrator.',
-	'fatal_error_message_admin' => 'Sorry, but something went wrong while loading the page. Error details:',
-	'in_file' => 'in file <strong>{x}</strong>', // Don't replace {x}
-	'on_line' => 'on line <strong>{x}</strong>' // Don't replace {x}
+	'fatal_error_message_user' => 'Beklager, men det oppstod en feil. Vennligst kontakt en administrator.',
+	'fatal_error_message_admin' => 'Beklager, men det oppstod en feil. Errordetaljer:',
+	'in_file' => 'i filen <strong>{x}</strong>', // Don't replace {x}
+	'on_line' => 'på linje <strong>{x}</strong>' // Don't replace {x}
 );
