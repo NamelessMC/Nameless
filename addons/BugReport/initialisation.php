d3dfa72e29639a5acdb3cfaaf362c13873e89953
Sam <samerton@users.noreply.github.com>
2018-06-05 13:25:05 +0100

    Update bug report addon language loading

--- addons/BugReport/initialisation.php
+++ addons/BugReport/initialisation.php
@@ -12,7 +12,14 @@
 // Initialise the bug report addon
 // We've already checked to see if it's enabled
 
-require('addons/BugReport/language.php');
+// Check language
+$c->setCache('languagecache');
+$language = $c->retrieve('language');
+
+if(file_exists('addons/BugReport/' . $language . '.php'))
+	require('addons/BugReport/' . $language . '.php');
+else
+	require('addons/BugReport/language.php');
 
 if($user->isLoggedIn()){
 	// Check cache for link location
