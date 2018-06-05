e602cdf8f1789ab0cb15598a030c90fe50449229
Sam <samerton@users.noreply.github.com>
2018-06-05 12:24:58 +0100

    Update maintenance back button to work on Safari
    Update forum maintenance back button to work on Safari

--- pages/forum/maintenance.php
+++ pages/forum/maintenance.php
@@ -57,7 +57,7 @@ $page = 'forum'; // for navbar
 	    <h1><?php echo $admin_language['maintenance_mode']; ?></h1>
 		<h4><?php echo $admin_language['forum_in_maintenance']; ?></h4>
 		<div class="btn-group" role="group" aria-label="...">
-		  <a href="#" class="btn btn-primary btn-lg" onclick="window.history.back()"><?php echo $general_language['back']; ?></a>
+		  <button class="btn btn-primary btn-lg" onclick="javascript:history.go(-1)"><?php echo $general_language['back']; ?></button>
 		  <a href="/" class="btn btn-success btn-lg"><?php echo $navbar_language['home']; ?></a>
 	    </div>
 	  </div>
