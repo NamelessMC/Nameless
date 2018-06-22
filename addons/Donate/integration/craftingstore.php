899196bc2eaf997f7ff3b4836f620b9b5ccded0a
Tim Kwakman <kwakmantim@gmail.com>
2018-06-22 11:56:24 +0200

    CraftingStore: Use V4 API that uses safer (header based auth), and handle category name changes correctly.

--- addons/Donate/integration/craftingstore.php
+++ addons/Donate/integration/craftingstore.php
@@ -10,21 +10,22 @@ $API = $queries->getWhere('donation_settings', array("name", "=", "api_key"));
 $API = $API[0]->value;
 
 $ch = curl_init();
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('token: ' . $API));
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 // Packages
-curl_setopt($ch, CURLOPT_URL, 'https://api.craftingstore.net/v2/' . $API . '/packages');
+curl_setopt($ch, CURLOPT_URL, 'https://api.craftingstore.net/v4/general/packages');
 $cs_packages = curl_exec($ch);
 $cs_packages = json_decode($cs_packages, true);
 
 // Categories
-curl_setopt($ch, CURLOPT_URL, 'https://api.craftingstore.net/v2/' . $API . '/categories');
+curl_setopt($ch, CURLOPT_URL, 'https://api.craftingstore.net/v4/general/categories');
 $cs_categories = curl_exec($ch);
 $cs_categories = json_decode(str_replace("&quot;", "\"", strip_tags($cs_categories)), true);
 
 // Payments
-curl_setopt($ch, CURLOPT_URL, 'https://api.craftingstore.net/v2/' . $API . '/payments');
+curl_setopt($ch, CURLOPT_URL, 'https://api.craftingstore.net/v4/general/payments');
 $cs_donors = curl_exec($ch);
 $cs_donors = json_decode(str_replace("&quot;", "\"", strip_tags($cs_donors)), true);
 
