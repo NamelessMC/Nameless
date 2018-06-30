59908d633567b9b71640872ccd49605056036ef2
samerton <samerton@users.noreply.github.com>
2018-06-30 10:07:41 +0100

    Add missing emoji include to UserCP messaging

--- modules/Core/pages/user/messaging.php
+++ modules/Core/pages/user/messaging.php
@@ -566,6 +566,7 @@ require(ROOT_PATH . '/core/templates/cc_navbar.php');
 	<script src="<?php if(defined('CONFIG_PATH')) echo CONFIG_PATH . '/'; else echo '/'; ?>core/assets/plugins/emoji/js/emojione.min.js"></script>
 	<script src="<?php if(defined('CONFIG_PATH')) echo CONFIG_PATH . '/'; else echo '/'; ?>core/assets/plugins/ckeditor/plugins/spoiler/js/spoiler.js"></script>
 	<script src="<?php if(defined('CONFIG_PATH')) echo CONFIG_PATH . '/'; else echo '/'; ?>core/assets/plugins/ckeditor/ckeditor.js"></script>
+	<script src="<?php if(defined('CONFIG_PATH')) echo CONFIG_PATH . '/'; else echo '/'; ?>core/assets/plugins/ckeditor/plugins/emojione/dialogs/emojione.json"></script>
 	
 	<script type="text/javascript">
 		<?php echo Input::createEditor('reply'); ?>
