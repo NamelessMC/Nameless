dca1eb2e349888c8e101a4236ab46dc524469b30
samerton <samerton@users.noreply.github.com>
2018-07-13 12:27:13 +0100

    Allow switching between light + dark editor skins

--- core/assets/plugins/ckeditor/config.js
+++ core/assets/plugins/ckeditor/config.js
@@ -8,7 +8,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// %REMOVE_START%
 	// The configuration options below are needed when running CKEditor from source files.
 	config.plugins = 'dialogui,dialog,about,a11yhelp,dialogadvtab,basicstyles,bidi,blockquote,notification,button,toolbar,clipboard,panelbutton,panel,floatpanel,colorbutton,colordialog,templates,menu,contextmenu,copyformatting,div,resize,elementspath,enterkey,entities,popup,filetools,filebrowser,find,fakeobjects,flash,floatingspace,listblock,richcombo,font,forms,format,horizontalrule,htmlwriter,iframe,wysiwygarea,image,indent,indentblock,indentlist,smiley,justify,menubutton,language,link,list,liststyle,magicline,maximize,newpage,pagebreak,pastetext,pastefromword,preview,print,removeformat,save,selectall,showblocks,showborders,sourcearea,specialchar,scayt,stylescombo,tab,table,tabletools,tableselection,undo,lineutils,widgetselection,widget,notificationaggregator,uploadwidget,uploadimage,wsc,ckawesome,codesnippet,xml,ajax,codesnippetgeshi,divarea,emojione,image2,markdown,spoiler,tliyoutube2';
-	config.skin = 'moono-lisa';
+	//config.skin = 'moono-lisa';
 	// %REMOVE_END%
 
 	// Define changes to default configuration here. For example:
