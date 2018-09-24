cf3b3092eed07e4cec9e2e0270d1509cdc654c4a
Eftimie Andrei <bax@baxandrei.ro>
2018-09-24 13:46:20 +0300

    Update moderator.php

--- custom/languages/Dutch/moderator.php
+++ custom/languages/Dutch/moderator.php
@@ -94,6 +94,9 @@ $language = array(
    	'staff' => 'Staff',
     'type' => 'Type',
     'recent_punishments' => 'Recent Punishments',
+    'created' => 'Created:',
+    'staff:' => 'Staff:',
+    'reason:' => 'Reason:',
 	
     // Users
     'recent_registrations' => 'Recent Registrations'
