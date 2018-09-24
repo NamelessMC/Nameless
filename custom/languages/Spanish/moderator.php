edf89e8f03be48c76162262a794e06f12b526d79
Eftimie Andrei <bax@baxandrei.ro>
2018-09-24 13:49:53 +0300

    Update moderator.php

--- custom/languages/Spanish/moderator.php
+++ custom/languages/Spanish/moderator.php
@@ -90,6 +90,13 @@ $language = array(
     'view_user' => 'Ver usuario',
     'when' => 'Cuando',
     'staff' => 'Staff',
-    'type' => 'Tipo'
+    'type' => 'Tipo',
+    'recent_punishments' => 'Recent Punishments',
+    'created' => 'Created:',
+    'staff:' => 'Staff:',
+    'reason:' => 'Reason:',
+	
+    // Users
+    'recent_registrations' => 'Recent Registrations'
 
 );
