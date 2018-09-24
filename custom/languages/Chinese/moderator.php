08795b24c46c9e491b410a48545bff8e284e8af1
Eftimie Andrei <bax@baxandrei.ro>
2018-09-24 13:43:40 +0300

    Update moderator.php

--- custom/languages/Chinese/moderator.php
+++ custom/languages/Chinese/moderator.php
@@ -94,6 +94,10 @@ $language = array(
     'view_user' => '查看使用者',
     'when' => '何時',
     'staff' => '管理',
-    'type' => '類型'
+    'type' => '類型',
+    'recent_punishments' => 'Recent Punishments',
+	
+    // Users
+    'recent_registrations' => 'Recent Registrations'
 
 );
