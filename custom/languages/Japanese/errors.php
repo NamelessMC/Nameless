f254e5c8138c9b2f27ee4f7ae9f8df0b5bf7eed1
Mari0914 <40509799+Mari0914@users.noreply.github.com>
2018-09-30 13:13:26 +0900

    Japanese Language Update(New Version)

--- custom/languages/Japanese/errors.php
+++ custom/languages/Japanese/errors.php
@@ -2,9 +2,10 @@
 /*
  *	Made by Samerton
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-pr3
+ *  NamelessMC version 2.0.0-pr5
  *
- *  Translation by SimplyRin(@SimplyRin_, https://www.simplyrin.net)
+ *  Translation by SimplyRin( @SimplyRin_, https://www.simplyrin.net )
+ *  Additional translation by Mari0914( @Mari0914_Main, https://mari0914.japanminigame.net )
  *
  *  License: MIT
  *
@@ -15,17 +16,17 @@ $language = array(
 	/*
 	 *  Errors
 	 */
-	'404_title' => '申し訳ありません！',
-	'404_content' => 'そのページは見つかりませんでした。',
+	'404_title' => '<i class="fa fa-exclamation-triangle" aria-hidden="true"></i>',
+	'404_content' => '=指定したページは存在しません=',
 	'404_back' => '戻る',
 	'404_home' => 'ホーム',
-	'404_error' => 'これがエラーと思われる場合は、{x} 管理者に連絡してください: {y}', // Don't replace {x} or {y}
+	'404_error' => 'URLを確認後再試行してみてください。<br>正常に指定されている場合、削除されている可能性があります。<br>詳しくは {x}管理者に連絡してください{y}', // Don't replace {x} or {y}
 	'maintenance_title' => 'メンテナンスモード',
-	'maintenance_retry' => 'リトライ',
-	'fatal_error' => '致命的なエラー',
-	'fatal_error_title' => '申し訳ありません！',
-	'fatal_error_message_user' => '申し訳ありませんが、ページの読み込み中に何か問題が発生しました。管理者に連絡してください。',
-	'fatal_error_message_admin' => '申し訳ありませんが、ページの読み込み中に何か問題が発生しました。エラーの詳細:',
+	'maintenance_retry' => '再試行',
+	'fatal_error' => '=内部エラーが発生しました=',
+	'fatal_error_title' => '致命的なエラー',
+	'fatal_error_message_user' => 'ページ読み込み中に内部エラーが発生しました。<br>詳しくは {x}管理者に連絡してください{y}',
+	'fatal_error_message_admin' => 'ページ読み込み中に内部エラーが発生しました。エラーの詳細:',
 	'in_file' => 'ファイル: <strong>{x}</strong>', // Don't replace {x}
 	'on_line' => 'ライン(行): <strong>{x}</strong>' // Don't replace {x}
 );
