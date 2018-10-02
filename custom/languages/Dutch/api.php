c932c86830a4bf4ca150ad034a2d522bc480ab9e
smessie <smessie@users.noreply.github.com>
2018-10-02 20:14:57 +0200

    Update api.php
    Translate message to Dutch

--- custom/languages/Dutch/api.php
+++ custom/languages/Dutch/api.php
@@ -41,12 +41,12 @@ $language = array(
     'invalid_code' => 'Ongeldige code voorzien',
 
     // Success messages
-    'finish_registration_link' => 'Klik alstublieft de volgende link om de registratie te voltooien:',
+    'finish_registration_link' => 'Klik alstublieft op de volgende link om de registratie te voltooien:',
     'finish_registration_email' => 'Bekijkt u alstublieft uw email om de registratie te voltooien.',
     'group_updated' => 'Groep succesvol bijgewerkt',
     'report_created' => 'Rapport succesvol gemaakt',
     'new_private_message_from' => 'Nieuw privé bericht van {x}', // Don't replace {x}
     'username_updated' => 'Gebruikersnaam succesvol bijgewerkt',
     'server_info_updated' => 'Server informatie succesvol bijgwerkt',
-	'account_validated' => 'Account validated successfully'
+    'account_validated' => 'Account succesvol gevalideerd'
 );
