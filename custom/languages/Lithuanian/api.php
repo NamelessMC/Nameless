fd34a9fafd2418ec81a35ac8a8a1b43d03e1d548
Justas <44247810+justasofficial@users.noreply.github.com>
2018-11-11 02:05:05 +0100

    Added Lithuanian language...

---
+++ custom/languages/Lithuanian/api.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ *	Made by Samerton
+ *  Translation by Locus
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr5
+ *
+ *  License: MIT
+ *
+ *  Lithuanian Language - API
+ */
+
+$language = array(
+  // Errors
+  'unknown_error' => 'Nežinoma klaida',
+  'invalid_api_key' => 'Netinkamas API raktas',
+  'invalid_language_file' => 'Netinkamas kalbos failas',
+  'invalid_api_method' => 'Netinkamas API metodas',
+  'no_unique_site_id' => 'Nėra unikalaus svetainės ID',
+  'unable_to_check_for_updates' => 'Nepavyko patikrinti, ar yra atnaujinimų',
+  'invalid_post_contents' => 'Neteisingas POST turinys',
+  'invalid_get_contents' => 'Neteisingas GET turinys',
+  'invalid_email_address' => 'Neteisingas el. Pašto adresas',
+  'invalid_username' => 'Neteisingas vartotojo vardas',
+  'invalid_uuid' => 'Neteisingas UUID',
+  'email_already_exists' => 'El. Pašto adresas jau egzistuoja',
+  'username_already_exists' => 'Vartotojo vardas jau egzistuoja',
+  'uuid_already_exists' => 'UUID jau egzistuoja',
+  'unable_to_create_account' => 'Neįmanoma sukurti paskyros',
+  'unable_to_send_registration_email' => 'Nepavyko išsiųsti registracijos el. Laiško, norėdami suaktyvinti savo paskyra susisiekite su administratoriumi.',
+  'unable_to_find_user' => 'Nepavyko surasti naudotojo',
+  'unable_to_find_group' => 'Nepavyko surasti grupės',
+  'unable_to_update_group' => 'Nepavyko atnaujinti vartotojo grupės',
+  'report_content_too_long' => 'Turinio ataskaita turi būti 255 ar mažiau simbolių',
+  'you_must_register_to_report' => 'Jūs turite užsiregistruoti mūsų svetainėje, kad galėtumėte kurti ataskaitą',
+  'you_have_been_banned_from_website' => 'Jūs esate użblokuotas svetainėje',
+  'you_have_open_report_already' => 'Jūs jau turite atvirą ataskaitą apie šį žaidėją',
+  'unable_to_create_report' => 'Nepavyko sukurti ataskaitos',
+  'unable_to_update_username' => 'Nepavyko atnaujinti vartotojo vardo',
+  'unable_to_update_server_info' => 'Nepavyko atnaujinti serverio informacijos',
+  'invalid_server_id' => 'Neteisingas serverio ID',
+  'invalid_code' => 'Pateiktas netinkamas kodas',
+
+  // Success messages
+  'finish_registration_link' => 'Norėdami užpildyti registraciją, paspauskite šią nuorodą:',
+  'finish_registration_email' => 'Patikrinkite savo el. Laiškus, kad užbaigtumėte registraciją.',
+  'group_updated' => 'Grupė sėkmingai atnaujinta',
+  'report_created' => 'Ataskaita sėkmingai sukurta',
+  'new_private_message_from' => 'Nauja privati žinutė iš {x}', // Don't replace {x}
+  'username_updated' => 'Vartotojo vardas atnaujintas sėkmingai',
+  'server_info_updated' => 'Serverio informacija atnaujinta sėkmingai',
+	'account_validated' => 'Paskyra patvirtinta sėkmingai'
+);
---
+++ custom/languages/Lithuanian/user.php
@@ -0,0 +1,224 @@
+<?php
+/*
+ *	Made by Samerton
+ *  Translation by Locus
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr5
+ *
+ *  License: MIT
+ *
+ *  Lithuanian Language - Users
+ */
+
+$language = array(
+	// Change this for the account validation message
+	'validate_account_command' => 'Norėdami užbaigti registraciją, vykdykite komandą <strong>/validate {x}</strong> žaidime.', // Don't replace {x}
+
+	// User Related
+	'guest' => 'Svečias',
+	'guests' => 'Svečiai',
+
+	// UserCP
+	'user_cp' => 'Paskyra',
+	'user_cp_icon' => '<i class="fa fa-cogs" aria-hidden="true"></i> <span class="mobile_only">Paskyra</span>',
+	'overview' => 'Apžvalga',
+	'user_details' => 'Vartotojo Duomenys',
+	'profile_settings' => 'Profilio Nustatymai',
+	'successfully_logged_out' => 'Jūs sėkmingai atsijungėte.',
+	'messaging' => 'Pranešimų Rašymas',
+	'click_here_to_view' => 'Spauskite čia norėdami peržiūrėti.',
+	'moderation' => 'Moderavimas',
+	'administration' => 'Administracija',
+	'alerts' => 'Perspėjimai',
+	'delete_all' => 'Ištrinti viską',
+	'private_profile' => 'Privatus Profilis',
+
+	// Profile settings
+	'field_is_required' => 'Reikalingas {x}.', // Don't replace {x}
+	'settings_updated_successfully' => 'Nustatymai atnaujinti sėkmingai.',
+	'password_changed_successfully' => 'Slaptažodis sėkmingai pakeistas.',
+	'change_password' => 'Pakeisti Slaptažodį',
+	'current_password' => 'Dabartinis Slaptažodis',
+	'new_password' => 'Naujas Slaptažodis',
+	'confirm_new_password' => 'Patvirtinti naują slaptažodį',
+	'incorrect_password' => 'Jūsų slaptažodis yra neteisingas.',
+	'two_factor_auth' => 'Dviejų Veiksnių Autentifikavimas',
+	'enabled' => 'Įjungtas',
+	'disabled' => 'Išjungtas',
+	'enable' => 'Įjungti',
+	'disable' => 'Išjungti',
+	'tfa_scan_code' => 'Prašome nuskaityti šį kodą savo autentifikavimo programoje:',
+	'tfa_code' => 'Jei jūsų įrenginyje nėra fotoaparato arba jūs negalite nuskaityti QR kodo, įveskite šį kodą:',
+	'tfa_enter_code' => 'Prašome įvesti kodą, rodoma jūsų autentifikavimo programoje:',
+	'invalid_tfa' => 'Neteisingas kodas, bandykite dar kartą.',
+	'tfa_successful' => 'Sėkmingai nustatytas dviejų veiksnių autentifikavimas. Jums reikės autentifikuoti kiekvieną kartą, kai nuo šiol prisijungsite.',
+	'active_language' => 'Aktyvi kalba',
+  'timezone' => 'Laiko Zona',
+  'upload_new_avatar' => 'Įkelkite naują avatarą',
+  'nickname_already_exists' => 'Jūsų pasirinktas slapyvardis jau egzistuoja.',
+  'change_email_address' => 'Pakeisti el. Pašto adresą',
+  'email_already_exists' => 'Jūsų įvestas el. Pašto adresas jau egzistuoja.',
+  'email_changed_successfully' => 'El. Pašto adresas sėkmingai pakeistas.',
+  'avatar' => 'Avataras',
+
+	// Alerts
+	'user_tag_info' => 'Jūs buvote pažymėtas {x} "post".', // Don't replace {x}
+	'no_alerts' => 'Naujų įspėjimų nėra',
+	'view_alerts' => 'Peržiūrėti įspėjimus',
+	'1_new_alert' => 'Turite 1 naują įspėjimą',
+	'x_new_alerts' => 'Turite {x} naujų įspėjimų', // Don't replace {x}
+	'no_alerts_usercp' => 'Jūs neturite jokių įspėjimų.',
+
+	// Registraton
+	'registration_check_email' => 'Dėkojame už registraciją! Norėdami užbaigti registraciją, patikrinkite savo el. Paštą. Jei negalite rasti el. Laiško, patikrinkite savo nepageidaujamu laišku aplanką.',
+	'username' => 'Vartotojo Vardas',
+	'nickname' => 'Slapyvardis',
+	'minecraft_username' => 'Minecraft Vartotojo Vardas',
+	'email_address' => 'El. Pašto Adresas',
+	'email' => 'El. Paštas',
+	'password' => 'Slaptažodis',
+	'confirm_password' => 'Patvirtinti slaptažodį',
+	'i_agree' => 'Aš Sutinku',
+	'agree_t_and_c' => 'Spustelėję <strong class="label label-primary">Registruokitės</strong> sutinkate su mūsų <a href="{x}" target="_blank">Terminais & Sąlygoms</a>.',
+	'create_an_account' => 'Sukurti paskyrą',
+	'terms_and_conditions' => 'Terminai & Sąlygos',
+	'validation_complete' => 'Jūsų paskyra patvirtinta, dabar galite prisijungti.',
+	'validation_error' => 'Patvirtinant jūsų paskyrą buvo nežinoma klaida, susisiekite su svetainės administratoriumi.',
+	'signature' => 'Parašas',
+	'signature_max_900' => 'Jūsų parašas turi būti ne daugiau kaip 900 simbolių.',
+
+  // Registration - AuthMe
+  'connect_with_authme' => 'Prijunkite savo paskyra su AuthMe',
+  'authme_help' => 'Įveskite savo žaidimo AuthMe paskyros duomenis. Jei dar neturite paskyros, prisijunkite prie serverio dabar ir vykdykite pateiktas instrukcijas.',
+  'unable_to_connect_to_authme_db' => 'Nepavyko prisijungti prie AuthMe duomenų bazės. Jei ši klaida išlieka, susisiekite su svetainės administratoriumi.',
+  'authme_account_linked' => 'Paskyra sėkmingai susieta.',
+  'authme_email_help_1' => 'Galiausiai, prašome įvesti savo el. Pašto adresą.',
+  'authme_email_help_2' => 'Galiausiai, prašome įvesti savo el. Pašto adresą, taip pat pasirinkti savo paskyros pavadinimą.',
+
+	// Registration errors
+	'username_required' => 'Vartotojo vardas yra privalomas.',
+	'email_required' => 'El. Pašto adresas yra privalomas.',
+	'password_required' => 'Slaptažodis yra privalomas.',
+	'mcname_required' => 'Minecraft vartotojo vardas yra privalomas.',
+	'accept_terms' => 'Prieš registruodamiesi turite sutikti su Terminais & Sąlygomis.',
+	'username_minimum_3' => 'Jūsų vartotojo vardas turi būti ne mažiau kaip 3 simboliai.',
+	'mcname_minimum_3' => 'Jūsų Minecraft vartotojo vardas turi būti ne mažiau kaip 3 simboliai.',
+	'password_minimum_6' => 'Jūsų slaptažodis turi būti mažiausiai 6 simboliai.',
+	'username_maximum_20' => 'Jūsų vartotojo vardas turi būti ne daugiau kaip 20 simbolių.',
+	'mcname_maximum_20' => 'Jūsų Minecraft vartotojo vardas turi būti ne daugiau kaip 20 simbolių.',
+	'password_maximum_30' => 'Jūsų slaptažodis turi būti ne ilgesnis nei 30 simbolių.',
+	'passwords_dont_match' => 'Jūsų slaptažodžiai nesutampa.',
+	'username_mcname_email_exists' => 'Jūsų vartotojo vardas arba el. Pašto adresas jau egzistuoja.',
+	'invalid_mcname' => 'Jūsų Minecraft naudotojo vardas yra negaliojantis.',
+	'invalid_email' => 'Jūsų el. Pašto adresas yra negaliojantis.',
+	'mcname_lookup_error' => 'Bendraujant su Mojang serveriais įvyko klaida patvirtinant jūsų vartotojo vardą. Pabandykite dar kartą vėliau.',
+	'invalid_recaptcha' => 'Netinkamas reCAPTCHA atsakymas.',
+	'verify_account' => 'Patvirtinkite Paskyrą',
+	'verify_account_help' => 'Prašome laikytis žemiau pateiktų instrukcijų, jog galėtume patvirtinti, kad turite minėtą Minecraft paskyrą.',
+	'validate_account' => 'Patikrinkite Paskyrą',
+	'verification_failed' => 'Patvirtinimas nepavyko, bandykite dar kartą.',
+	'verification_success' => 'Sėkmingai patvirtinta! Dabar galite prisijungti.',
+	'authme_username_exists' => 'Jūsų AuthMe paskyra jau buvo prijungta prie svetainės!',
+	'uuid_already_exists' => 'Jūsų UUID jau egzistuoja, tai reiškia, kad ši Minecraft paskyra jau užregistruota.',
+
+	// Login
+	'successful_login' => 'Jūs sėkmingai prisijungėte.',
+	'incorrect_details' => 'Įvedėte neteisingus duomenis.',
+	'inactive_account' => 'Jūsų paskyra yra neaktyvi. Patikrinkite savo el. Paštą, kad gautumėte patvirtinimo nuorodą, įskaitant savo nepageidaujamų laiškų aplanką.',
+	'account_banned' => 'Šita paskyra yra užblokota.',
+	'forgot_password' => 'Pamiršote slaptažodį?',
+	'remember_me' => 'Prisimink Mane',
+	'must_input_email' => 'Jūs turite įvesti el. Pašto adresą.',
+	'must_input_username' => 'Jūs turite įvesti vartotojo vardą.',
+	'must_input_password' => 'Jūs turite įvesti slaptažodį.',
+
+  // Forgot password
+  'forgot_password_instructions' => 'Įveskite savo el. Pašto adresą, kad galėtume atsiųsti jums daugiau instrukcijų, kaip iš naujo nustatyti slaptažodį.',
+  'forgot_password_email_sent' => 'Jei paskyra su tuo el. Pašto adreso jau egzistuoja, bus išsiųstas el. Laiškas su papildomomis instrukcijomis. Jei negalėsite jo rasti, pabandykite patikrinti savo nepageidaujamų laiškų aplanką.',
+  'unable_to_send_forgot_password_email' => 'Nepavyko išsiųsti pamiršto slaptažodžio el. Laišku. Susisiekite su administratoriumi.',
+  'enter_new_password' => 'Patvirtinkite savo el. Pašto adresą ir žemiau įveskite naują slaptažodį.',
+  'incorrect_email' => 'Įvestas el. Pašto adresas neatitinka prašymo.',
+  'forgot_password_change_successful' => 'Jūsų slaptažodis sėkmingai pakeistas. Dabar galite prisijungti.',
+
+	// Profile pages
+	'profile' => 'Profilis',
+	'follow' => 'Sekite',
+	'no_wall_posts' => 'Čia dar nėra sienos "post".',
+	'change_banner' => 'Keisti Banerį',
+	'post_on_wall' => '"Post" ant {x}\'s sienos', // Don't replace {x}
+	'invalid_wall_post' => 'Patikrinkite, ar jūsų "post" yra nuo 1 iki 10000 simbolių.',
+	'1_reaction' => '1 reakcija',
+	'x_reactions' => '{x} reakcijos', // Don't replace {x}
+	'1_like' => '1 "like"',
+	'x_likes' => '{x} "likes"', // Don't replace {x}
+	'1_reply' => '1 atsakymas',
+	'x_replies' => '{x} atsakymai', // Don't replace {x}
+	'no_replies_yet' => 'Dar nėra atsakymų',
+	'feed' => '"Feed"',
+	'about' => 'Apie',
+	'reactions' => 'Reakcijos',
+	'replies' => 'Atsakymai',
+	'new_reply' => 'Naujas Atsakymas',
+	'registered' => 'Prisiregistravo:',
+	'registered_x' => 'Prisiregistravo: {x}',
+	'last_seen' => 'Paskutinį kartą matytas:',
+	'new_wall_post' => '{x} paskelbė ant jūsų profilio.',
+	'couldnt_find_that_user' => 'Nepavyko rasti šio vartotojo.',
+	'block_user' => 'Užblokuoti Vartotoją',
+	'unblock_user' => 'Atblokuoti Vartotoją',
+	'confirm_block_user' => 'Ar tikrai norite blokuoti šį naudotoją? Jis negalės atsiųsti privačių žinučių arba pažymeti jus į “posts”.',
+	'confirm_unblock_user' => 'Ar tikrai norite atblokuoti šį naudotoją? Jis galės atsiųsti jums privačių žinučių ir pažymeti jus į “posts”',
+	'user_blocked' => 'Vartotojas užblokuotas.',
+	'user_unblocked' => 'Vartotojas atblokuotas.',
+	'views' => 'Profilio Peržiūros:',
+	'private_profile_page' => 'Tai yra privatus profilis!',
+	'new_wall_post_reply' => '{x} atsakė į jūsų "post" {y}\'s profilyje.', // Don't replace {x} or {y}
+	'new_wall_post_reply_your_profile' => '{x} atsakė į jūsų "post" jūsų profilyje.', // Don't replace {x}
+	'no_about_fields' => 'Šis vartotojas dar nepateikė jokių “apie” laukų.',
+
+	// Reports
+	'invalid_report_content' => 'Neįmanoma kurti ataskaitos. Patikrinkite, ar ataskaitos priežastis yra nuo 2 iki 1024 simbolių.',
+	'report_post_content' => 'Įveskite savo atąskaitos priežastis',
+	'report_created' => 'Ataskaita sėkmingai sukurta',
+
+	// Messaging
+	'no_messages' => 'Naujų žinučių nėra',
+	'no_messages_full' => 'Jūs neturite jokių žinučių',
+	'view_messages' => 'Peržiūrėti žinutes',
+	'1_new_message' => 'Turite 1 naują žinutę',
+	'x_new_messages' => 'Turite {x} naujų žinučių', // Don't replace {x}
+	'new_message' => 'Nauja žinutė',
+	'message_title' => 'Žinutės Antraštė',
+	'to' => 'To',
+	'separate_users_with_commas' => 'Atskirkite vartotojus su kableliais',
+	'title_required' => 'Įveskite antraštę',
+	'content_required' => 'Įveskite tam tikrą turinį',
+	'users_to_required' => 'Prašau įvesti žinutės gaveją(us)',
+	'cant_send_to_self' => 'Jūs negalite siųsti žinutės sau!',
+	'title_min_2' => 'Pavadinimas turi būti ne mažiau kaip 2 simboliai.',
+	'content_min_2' => 'Turinys turi būti bent 2 simboliai.',
+	'title_max_64' => 'Antraštė turi būti ne daugiau kaip 64 simboliai.',
+	'content_max_20480' => 'Turinys turi būti ne daugiau kaip 20480 simbolių.',
+	'max_pm_10_users' => 'Galite siųsti žinutę ne daugiau kaip 10 vartotojų.',
+	'message_sent_successfully' => 'Pranešimas sėkmingai išsiųstas',
+	'participants' => 'Dalyviai',
+	'last_message' => 'Paskutinė Žinutė',
+	'by' => 'By',
+	'leave_conversation' => 'Palikti Pokalbį',
+	'confirm_leave' => 'Ar tikrai norite palikti šį pokalbį?',
+	'one_or_more_users_blocked' => 'Jūs negalite siųsti žinučių bent vienam pokalbio nariui.',
+
+	// Infractions area
+	'you_have_been_banned' => 'Jūs buvot užblokuotas!',
+	'you_have_received_a_warning' => 'Jūs gavote įspėjimą!',
+	'acknowledge' => 'Pripažinti',
+
+	// Emails
+	'email_greeting' => 'Labas,',
+	'email_message' => 'Dėkojame už registraciją! Norėdami užbaigti registraciją, spauskite šią nuorodą:',
+	'forgot_password_email_message' => 'Jei norite iš naujo nustatyti slaptažodį, spauskite šią nuorodą. Jei to neprašėte patys, galite saugiai ištrinti šį el. Laišką.',
+	'email_thanks' => 'Ačiū,',
+
+	// Hooks
+	'user_x_has_registered' => '{x} prisijungė prie ' . SITE_NAME . '!'
+);
