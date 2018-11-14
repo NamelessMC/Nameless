b2eb1df829a51dc34ccac3c6ed3858d6c1c9bf98
samerton <samerton@users.noreply.github.com>
2018-11-14 13:08:23 +0000

    Allow popover hover in StaffCP

--- custom/panel_templates/Default/template.php
+++ custom/panel_templates/Default/template.php
@@ -45,7 +45,20 @@ class Default_Panel_Template extends TemplateBase {
 				$(\'[data-toggle="tooltip"]\').tooltip();
 			});
 			$(document).ready(function(){
-				$(\'[data-toggle="popover"]\').popover({trigger:\'hover\',html:true});
+				$(\'[data-toggle="popover"]\').popover({trigger:\'manual\',html:true}).on("mouseenter", function() {
+				  var _this = this;
+				  $(this).popover("show");
+				  $(".popover").on("mouseleave", function() {
+				    $(_this).popover(\'hide\');
+				  });
+				}).on("mouseleave", function() {
+				  var _this = this;
+				  setTimeout(function() {
+				    if (!$(".popover:hover").length) {
+				      $(_this).popover("hide")
+				    }
+				  }, 100);
+				});
 			});
 		');
 	}
