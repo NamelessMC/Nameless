eecbfea48ff41c564e7ee0ccc9dd5a663307564e
kobel169 <kobel169@gmail.com>
2019-03-10 21:24:48 +0100

    Initial russian l10n

---
+++ custom/languages/Russian/general.php
@@ -0,0 +1,141 @@
+<?php 
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr6
+ *
+ *  License: MIT
+ *
+ *  Russian Language - General terms
+ */
+
+$language = array(
+	/*
+	 *  General terms
+	 */ 
+	'submit' => 'Submit',
+	'register' => 'Register',
+	'sign_in' => 'Log In',
+	'log_out' => 'Log Out',
+	'profile' => 'Profile',
+	'invalid_token' => 'Invalid token, please try again.',
+	'close' => 'Close',
+	'back' => 'Back',
+	'warning' => 'Warning',
+	'date' => 'Date',
+	'search' => 'Search',
+	'edit' => 'Edit',
+	'next' => 'Next',
+	'previous' => 'Previous',
+	'confirm_cancel' => 'Are you sure you wish to cancel?',
+	'confirm_delete' => 'Confirm deletion',
+	'confirm_deletion' => 'Are you sure you wish to delete this?',
+	'cancel' => 'Cancel',
+	'delete' => 'Delete',
+	'none' => 'None',
+	'view' => 'View',
+	'change' => 'Change',
+	'remove' => 'Remove',
+	'info' => 'Info',
+	'confirm' => 'Confirm',
+	'actions' => 'Actions',
+	'alerts' => 'Alerts',
+	'view_alerts' => 'View alerts',
+	'no_alerts' => 'No alerts',
+	'x_new_alerts' => 'You have {x} new alerts.', // Don't replace {x}
+	'messages' => 'Messages',
+	'view_messages' => 'View messages',
+	'no_messages' => 'No messages',
+	'x_new_messages' => 'You have {x} new messages.', // Don't replace {x}
+	'loading' => 'Loading...',
+	'report' => 'Report',
+	'page_loaded_in' => 'Page loaded in {x}s', // Don't replace {x}, time unit is seconds
+	'page_x' => 'Page {x}',
+	'browse' => 'Browse',
+	'bypass' => 'Bypass',
+	'yes' => 'Yes',
+	'no' => 'No',
+	'content' => 'Content',
+	'full_content' => 'Full Content',
+	'url' => 'URL',
+	'success' => 'Success',
+	'error' => 'Error',
+	'are_you_sure' => 'Are you sure?',
+	'spoiler' => 'Spoiler',
+
+	/*
+	 *  Contact form
+	 */
+	'contact' => 'Contact',
+	'message' => 'Message',
+	'email_address' => 'Email Address',
+	'contact_email_subject' => 'New contact form submission',
+	'contact_message_failed' => 'Unable to send message. Please ensure you have entered a message between 10 and 5000 characters long.',
+	'contact_message_email' => 'The provided email is invalid.',
+	'contact_message_sent' => 'Message sent successfully.',
+	'contact_message_limit' => 'You can only send one message per hour. You can next send a message in {x} minutes.', // Don't replace {x}
+	
+	
+	/* 
+	 *  Navbar
+	 */
+	'home' => 'Home',
+	'more' => 'More',
+	'status' => 'Status',
+	
+	/*
+	 *  Homepage
+	 */
+	'social' => 'Social',
+	
+	
+	/*
+	 *  Help dialogues
+	 */
+	'markdown_help' => 'You can format your post using Markdown. For more information, see <a href=\'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet\' target=\'_blank\'>here</a>.',
+
+	/*
+	 *  Server query
+	 */
+	'online' => 'Online',
+	'offline' => 'Offline',
+	'player_count' => '{x}/{y}', // Don't replace {x} or {y}
+	'ping' => 'Ping: {x}', // Don't replace {x}
+	'version' => 'Version: {x}', // Don't replace {x}
+	'currently_1_player_online' => 'There is currently 1 player online.',
+	'currently_x_players_online' => 'There are currently <strong>{x}</strong> players online.', // Don't replace {x}
+	'server_offline' => 'The server is currently offline.',
+    'connect_with_ip_x' => 'Connect now using the IP {x}', // Don't replace {x}
+    'click_to_copy_tooltip' => 'Click to copy',
+    'copied' => 'Copied!',
+    'no_servers' => 'There are no servers to display!',
+    'bungee_instance' => 'This server is a Bungee instance.',
+    'no_players_online' => 'There are no players online.',
+    'and_x_more' => 'and {x} more', // Don't replace {x}
+
+	/*
+	 *  Cookie notice
+	 */
+	'cookie_notice' => 'This website uses cookies to enhance your browsing experience. By registering on our website or closing this notice you agree with their use.',
+	'cookie_agree' => 'Agree',
+
+    /*
+     *  Widgets
+     */
+    'statistics' => 'Statistics',
+    'users_registered' => 'Users Registered',
+    'latest_member' => 'Latest Member',
+    'forum_statistics' => 'Forum Statistics',
+    'total_threads' => 'Total Threads',
+    'total_posts' => 'Total Posts',
+    'online_staff' => 'Online Staff',
+    'no_online_staff' => 'There are no staff members online.',
+    'online_users' => 'Online Users',
+    'no_online_users' => 'There are no users online.',
+    'discord_widget_disabled' => 'The widget is disabled for the specified Discord server. Please go to the \'Widget\' tab in your Discord server settings, and ensure the Discord widget is enabled and that the ID is correct.',
+
+    /*
+     *  Privacy policy
+     */
+    'privacy_policy' => 'Privacy Policy'
+);
---
+++ custom/languages/Russian/version.php
@@ -0,0 +1,14 @@
+<?php 
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr6
+ *
+ *  License: MIT
+ *
+ *  Russian Language - Language version
+ */
+
+// Which version of NamelessMC is this language file updated to?
+$language_version = '2.0.0-pr6';
+$language_html = 'ru';
