5e088baa2b6d52b0257a5d37ada8ab652d31b5f1
JosGCS <imj0s3ph.gcs@gmail.com>
2019-03-29 12:15:45 -0600

    Update errors.php

--- custom/languages/Spanish/errors.php
+++ custom/languages/Spanish/errors.php
@@ -1,29 +1,142 @@
-<?php 
+<?php
 /*
  *	Made by TheSuperSkills (Edited by iMaykolRD_)
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-pr4
+ *  NamelessMC version 2.0.0-pr3/pr4
  *
  *  License: MIT
  *
- *  Spanish Language - Errors
+ *  Spanish Language - General terms
  */
 
 $language = array(
 	/*
-	 *  Errors
+	 *  General terms
 	 */
-	'404_title' => '¡Lo sentimos!',
-	'404_content' => 'No pudimos encontrar esa página.',
-	'404_back' => 'Regresar',
-	'404_home' => 'Inicio',
-	'404_error' => 'Si cree que se trata de un error, {x} póngase en contacto con un administrador{y}', // Don't replace {x} or {y}
-	'maintenance_title' => 'Modo de mantenimiento.',
-	'maintenance_retry' => 'Volver a procesar',
-	'fatal_error' => 'Error fatal',
-	'fatal_error_title' => '¡Lo sentimos!',
-	'fatal_error_message_user' => 'Se ha producido un error al cargar la página. Inténtelo nuevamente.',
-	'fatal_error_message_admin' => 'Se ha producido un error al cargar la página. Detalles del error:',
-	'in_file' => 'En el archivo <strong>{x}</strong>', // Don't replace {x}
-	'on_line' => 'En la línea <strong>{x}</strong>' // Don't replace {x}
+	'submit' => 'Enviar',
+	'register' => 'Registro',
+	'sign_in' => 'Iniciar sesión',
+	'log_out' => 'Cerrar sesión',
+	'profile' => 'Perfil',
+	'invalid_token' => 'Símbolo no válido, inténtalo de nuevo.',
+	'close' => 'Cerrar',
+	'back' => 'Regresar',
+	'warning' => 'Advertencia',
+	'date' => 'Fecha',
+	'search' => 'Buscar',
+	'edit' => 'Editar',
+	'next' => 'Siguiente',
+	'previous' => 'Anterior',
+	'confirm_cancel' => '¿Seguro que desea cancelar?',
+	'confirm_delete' => 'Confirmar la eliminación',
+	'confirm_deletion' => '¿Estás seguro de que deseas eliminarlo?',
+	'cancel' => 'Cancelar',
+	'delete' => 'Borrar',
+	'none' => 'Nínguna',
+	'view' => 'Ver',
+	'change' => 'Cambiar',
+	'remove' => 'Remover',
+	'info' => 'Informacion',
+	'confirm' => 'Confirmar',
+	'actions' => 'Acciones',
+	'alerts' => 'Alertas',
+	'view_alerts' => 'Ver alertas',
+	'no_alerts' => 'No alertas',
+	'x_new_alerts' => 'Tienes {x} nuevas alertas.', // Don't replace {x}
+	'messages' => 'Mensajes',
+	'view_messages' => 'Ver mensajes',
+	'no_messages' => 'No mensajes',
+	'x_new_messages' => 'Tienes {x} nuevos mensajes.', // Don't replace {x}
+	'loading' => 'Cargando...',
+	'report' => 'Reportar',
+	'page_loaded_in' => 'Página cargada en {x}s', // Don't replace {x}, time unit is seconds
+	'page_x' => 'Pagina {x}',
+	'browse' => 'Vistazo',
+	'bypass' => 'Saltarse',
+	'yes' => 'Sí',
+	'no' => 'No',
+	'content' => 'Contenido',
+	'full_content' => 'Contenido completo',
+	'url' => 'Enlace',
+	'success' => 'Exito',
+	'error' => 'Error',
+	'are_you_sure' => '¿Estás seguro?',
+	'spoiler' => 'Spoiler',
+
+	/*
+	 *  Contact form
+	 */
+	'contact' => 'Contacto',
+	'message' => 'Mensaje',
+	'email_address' => 'Correo electrónico',
+	'contact_email_subject' => 'Nuevo formulario de contacto',
+	'contact_message_failed' => 'No se puede enviar el mensaje. Asegúrate de haber ingresado un mensaje entre 10 y 5000 caracteres.',
+	'contact_message_email' => 'Ese e-Mail es inválido.',
+	'contact_message_sent' => 'Mensaje enviado con éxito.',
+	'contact_message_limit' => 'Sólo puede enviar un mensaje por hora. Puede enviar un mensaje en {x} minutos.', // Don't replace {x}
+
+	/*
+	 *  Navbar
+	 */
+	'home' => 'Inicio',
+	'more' => 'Más',
+	'status' => 'Estado',
+
+	/*
+	 *  Homepage
+	 */
+	'social' => 'Social',
+
+
+	/*
+	 *  Help dialogues
+	 */
+	'markdown_help' => 'Puedes redactar tu publicación usando Markdown. Para más información, mira <a href=\'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet\' target=\'_blank\'>aquí</a>.',
+
+	/*
+	 *  Server query
+	 */
+	'online' => 'En línea',
+	'offline' => 'Desconectado',
+	'player_count' => '{x}/{y}', // Don't replace {x} or {y}
+	'ping' => 'Conexión: {x}', // Don't replace {x}
+	'version' => 'Versión: {x}', // Don't replace {x}
+	'currently_1_player_online' => 'Hay 1 jugador conectado.',
+	'currently_x_players_online' => 'Hay actualmente <strong>{x}</strong> jugadores conectados.', // Don't replace {x}
+	'server_offline' => 'El servidor está actualmente sin conexión.',
+	'connect_with_ip_x' => 'Conéctate ahora {x}', // Don't replace {x}
+    'click_to_copy_tooltip' => 'Haga clic para copiar',
+    'copied' => '¡Copiada!',
+    'no_servers' => '¡No hay servidores para mostrar!',
+    'bungee_instance' => 'Este servidor es una instancia de BungeeCord.',
+    'no_players_online' => 'No hay jugadores conectados.',
+    'and_x_more' => 'y {x} más', // Don't replace {x}
+
+	/*
+	 *  Cookie notice
+	 */
+	'cookie_notice' => 'Este sitio web utiliza Cookies para mejorar tu experiencia. Si sigues navegando, estás aceptando nuestros términos y condiciones.',
+	'cookie_agree' => 'Acepto',
+
+    /*
+     *  Widgets
+     */
+    'statistics' => 'Estadísticas',
+    'users_registered' => 'Usuarios Registrados',
+    'latest_member' => 'Último Miembro',
+    'forum_statistics' => 'Estadísticas del Foro',
+    'total_threads' => 'Hilos totales',
+    'total_posts' => 'Publicaciones totales',
+    'online_staff' => 'Staff Conectado',
+    'no_online_staff' => 'No hay miembros del Staff conectados.',
+	'total_online_staff' => 'Miembros del Staff conectados: {x}',
+    'online_users' => 'Usuarios conectados',
+    'no_online_users' => 'No hay usuarios conectados.',
+	'total_online_users' => 'Total de usuarios conectados: {x}',
+    'discord_widget_disabled' => 'El widget está deshabilitado para el servidor de Discord especificado.. Vaya a la pestaña Widget en la configuración del servidor de Discord y asegúrese de que el widget de Discord esté habilitado y que la ID sea correcta..',
+
+    /*
+     *  Privacy policy
+     */
+    'privacy_policy' => 'Política de privacidad'
 );
