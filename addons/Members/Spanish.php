43dd839342d36b6057a66716c030faf1f28e5ec1
Joseph Gonzalo <imj0s3ph.gcs@gmail.com>
2019-06-08 23:07:11 -0600

    Create Spanish.php

---
+++ addons/Members/Spanish.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+ *	Hecho por Partydragen
+ *  http://partydragen.com/
+ *
+ *  Modificado por JosGCS
+ *  https://twitter.com/iJosGCS
+ *
+ */
+// Archivo de idioma para el complemento de "miembros".
+$members_language = array(
+	'members' => 'Miembros',
+	'members_icon' => '', // Ícono a mostrar en la barra de navegación.
+	'username' => 'Usuario',
+	'group' => 'Grupo',
+	'created' => 'Fecha de Registro',
+);
