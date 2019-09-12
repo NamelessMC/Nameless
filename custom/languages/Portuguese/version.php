53368f8aa59f5c378dcd8ab99b7dad76dbfc2b53
dgateles <dga.teles@gmail.com>
2019-09-12 09:01:05 -0300

    Update language ver

--- custom/languages/Portuguese/version.php
+++ custom/languages/Portuguese/version.php
@@ -2,14 +2,14 @@
 /*
  *	Made by Samerton
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-pr2
+ *  NamelessMC version 2.0.0-pr6
  *
  *  License: MIT
  *
  *  Portuguese Language - Language version
- *  Translation By Douglas Teles & dasilvaj4
- *  Last Update: 11/04/2018
+ *  Translation By Douglas Teles
+ *  Last Update: 12/09/2019
  */
 // Which version of NamelessMC is this language file updated to?
-$language_version = '2.0.0-pr2';
+$language_version = '2.0.0-pr6';
 $language_html = 'pt-br';
