26664c6d1ad318f859a152488c74eae810036692
Marck200 <45741056+Marckos200@users.noreply.github.com>
2020-01-12 20:57:37 +0100

    Create general.php
    es-ES translation

---
+++ custom/languages/SpanishES/general.php
@@ -0,0 +1,152 @@
+<?php
+/*
+ *  Translated by ManiaNetwork (Marck200 & Osiris)
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr7
+ *
+ *  License: MIT
+ *
+ *  Spanish/Spain Language - Language version
+ */
+
+$language = array(
+	/*
+	 *  General terms
+	 */
+	'submit' => 'Enviar',
+	'register' => 'Registro',
+	'already_registered' => '¿Ya está registrado?',
+	'not_registered_yet' => '¿No se ha registrado aún?',
+	'sign_in' => 'Iniciar sesión',
+	'log_out' => 'Cerrar sesión',
+	'profile' => 'Perfil',
+	'invalid_token' => 'Símbolo no válido, inténtelo de nuevo.',
+	'close' => 'Cerrar',
+	'back' => 'Volver',
+	'warning' => 'Advertencia',
+	'date' => 'Fecha',
+	'search' => 'Buscar',
+	'edit' => 'Editar',
+	'next' => 'Siguiente',
+	'previous' => 'Anterior',
+	'confirm_cancel' => '¿Está seguro de que desea cancelarlo?',
+	'confirm_delete' => 'Confirmar la eliminación',
+	'confirm_deletion' => '¿Está seguro de que desea eliminarlo?',
+	'cancel' => 'Cancelar',
+	'delete' => 'Borrar',
+	'none' => 'Ninguna',
+	'view' => 'Ver',
+	'change' => 'Cambiar',
+	'remove' => 'Eliminar',
+	'info' => 'Información',
+	'confirm' => 'Confirmar',
+	'actions' => 'Acciones',
+	'alerts' => 'Notificaciones',
+	'view_alerts' => 'Ver notificacionees',
+	'no_alerts' => 'Sin notificaciones',
+	'x_new_alerts' => 'Tiene {x} nuevas notificaciones.', // Don't replace {x}
+	'messages' => 'Mensajes',
+	'view_messages' => 'Ver mensajes',
+	'no_messages' => 'No tiene mensajes',
+	'x_new_messages' => 'Tiene {x} nuevos mensajes.', // Don't replace {x}
+	'loading' => 'Cargando...',
+	'report' => 'Reportar',
+	'page_loaded_in' => 'Página cargada en {x} segundos', // Don't replace {x}, time unit is seconds
+	'page_x' => 'Pagina {x}',
+	'browse' => 'Buscar',
+	'bypass' => 'Saltarse',
+	'yes' => 'Sí',
+	'no' => 'No',
+	'content' => 'Contenido',
+	'full_content' => 'Ver todo el contenido',
+	'url' => 'Enlace',
+	'success' => 'Exito',
+	'error' => 'Error',
+	'are_you_sure' => '¿Está seguro?',
+	'spoiler' => 'Spoiler',
+
+	/*
+	 *  Contact form
+	 */
+	'contact' => 'Contacto',
+	'message' => 'Mensaje',
+	'email_address' => 'Correo electrónico',
+	'contact_email_subject' => 'Nuevo formulario de contacto',
+	'contact_message_failed' => 'No se pudo enviar el mensaje. Asegúrese de haber ingresado un mensaje de entre 10 y 5000 caracteres.',
+	'contact_message_email' => 'Ese correo electrónico es inválido.',
+	'contact_message_sent' => 'Mensaje enviado con éxito.',
+	'contact_message_limit' => 'Sólo puede enviar un mensaje por hora. Podrá enviar otro mensaje en {x} minutos.', // Don't replace {x}
+
+	/*
+	 *  Navbar
+	 */
+	'home' => 'Inicio',
+	'more' => 'Más',
+	'status' => 'Estado',
+
+	/*
+	 *  Homepage
+	 */
+	'social' => 'Social',
+
+
+	/*
+	 *  Help dialogues
+	 */
+	'markdown_help' => 'Puedes redactar tu publicación utilizando Markdown. Para más información, pinche <a href=\'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet\' target=\'_blank\'>aquí</a>.',
+
+	/*
+	 *  Server query
+	 */
+	'online' => 'En línea',
+	'offline' => 'Desconectado',
+	'player_count' => '{x}/{y}', // Don't replace {x} or {y}
+	'ping' => 'Conexión: {x}', // Don't replace {x}
+	'version' => 'Versión: {x}', // Don't replace {x}
+	'currently_1_player_online' => 'Hay 1 jugador conectado.',
+	'currently_x_players_online' => 'Hay actualmente <strong>{x}</strong> jugadores conectados.', // Don't replace {x}
+	'server_offline' => 'El servidor se encuentra actualmente sin conexión.',
+	'connect_with_ip_x' => 'Conéctate ahora {x}', // Don't replace {x}
+    'click_to_copy_tooltip' => 'Haga clic para copiar',
+    'ip' => 'IP',
+    'copied' => 'Dirección copiada',
+    'no_servers' => 'No hay servidores para mostrar.',
+    'bungee_instance' => 'Este servidor es una instancia de BungeeCord.',
+    'no_players_online' => 'No hay jugadores conectados.',
+    'and_x_more' => 'y {x} más', // Don't replace {x}
+
+	/*
+	 *  Cookie notice
+	 */
+	'cookie_notice' => 'Este sitio web utiliza Cookies para mejorar su experiencia. Si sigues navegando, estás aceptando nuestros términos y condiciones.',
+	'cookie_agree' => 'Acepto',
+
+    /*
+     *  Widgets
+     */
+    'statistics' => 'Estadísticas',
+    'users_registered' => 'Usuarios registrados',
+    'latest_member' => 'Último miembro',
+    'forum_statistics' => 'Estadísticas del foro',
+    'total_threads' => 'Hilos totales',
+    'total_posts' => 'Publicaciones totales',
+    'online_staff' => 'Staff conectado',
+    'no_online_staff' => 'No hay miembros del Staff conectados.',
+    'total_online_staff' => 'Miembros del Staff conectados: {x}',
+    'online_users' => 'Usuarios conectados',
+    'no_online_users' => 'No hay usuarios conectados.',
+    'total_online_users' => 'Total de usuarios conectados: {x}',
+    'discord_widget_disabled' => 'El widget está deshabilitado para el servidor de Discord especificado. Vaya a la pestaña Widget en la configuración del servidor y asegúrese de que el widget de Discord esté habilitado y que la ID sea correcta.',
+    'online_guests' => 'Visitantes en línea',
+    'total_online' => 'Personas en línea', // this is Online Users + Online Guests
+
+    /*
+     *  Privacy policy
+     */
+    'privacy_policy' => 'Política de privacidad',
+
+	/*
+	 *  Custom page redirect
+	 */
+	'redirecting_message' => 'Redirigiendo...<br />Si no es redirigido automáticamente, haga click <a href="{x}" rel="nofollow noopener">aquí</a>.' // Don't replace {x} which is the redirect URL
+);
