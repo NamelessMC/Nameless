6614ef7a0ab0195bec9d3a668b39e4a3780e60f1
Marck200 <45741056+Marckos200@users.noreply.github.com>
2020-01-12 20:54:11 +0100

    Create time.php
    es-ES translation

---
+++ custom/languages/SpanishES/time.php
@@ -0,0 +1,29 @@
+<?php 
+/*
+ *  Translated by ManiaNetwork (Marck200 & Osiris)
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr7
+ *
+ *  License: MIT
+ *
+ *  Spanish/Spain Language - Language version
+ */
+
+$language = array(
+	/*
+	 *  Time
+	 */
+	'time' => 'tiempo', // Placeholder, no need to translate
+	'seconds_short' => 'segundos', // Shortened "seconds", eg "s"
+	'less_than_a_minute' => 'Hace menos de 1 minuto',
+	'1_minute' => 'Hace 1 minuto',
+	'_minutes' => 'Hace {x} minutos',
+	'about_1_hour' => 'Hace aproximadamente 1 hora',
+	'_hours' => 'Hace {x} horas',
+	'1_day' => 'Hace 1 día',
+	'_days' => 'Hace {x} días',
+	'about_1_month' => 'Hace aproximadamente 1 mes',
+	'_months' => 'Hace {x} mes',
+	'about_1_year' => 'Hace aproximadamente 1 año',
+	'over_x_years' => 'Hace {x} años'
+);
