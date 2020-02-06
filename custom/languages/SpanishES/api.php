509bdc9717561bb5b20db4d8344fabbccdcf2bca
Marck200 <45741056+Marckos200@users.noreply.github.com>
2020-02-06 21:57:19 +0100

    Create api.php

---
+++ custom/languages/SpanishES/api.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ *	Translated by ManiaNetwork (Marck200 & Osiris)
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr7
+ *
+ *  License: MIT
+ *
+ *  Spanish_ES Language - API
+ */
+
+$language = array(
+    // Errors
+    'unknown_error' => 'Error desconocido.',
+    'invalid_api_key' => 'Llave de API inválida.',
+    'invalid_language_file' => 'Archivo de lenguaje inválido.',
+    'invalid_api_method' => 'Método de API inválido.',
+    'no_unique_site_id' => 'No hay una identificación de sitio única disponible.',
+    'unable_to_check_for_updates' => 'No se ha podido realizar la búsqueda de actualizaciones.',
+    'invalid_post_contents' => 'Contenido POST no válido.',
+    'invalid_get_contents' => 'Contenido GET no válido.',
+    'invalid_email_address' => 'Dirección de correo electrónico no válida.',
+    'invalid_username' => 'Nombre de usuario no válido.',
+    'invalid_uuid' => 'UUID inválido',
+    'email_already_exists' => 'Esa dirección de correo electrónico ya está en uso.',
+    'username_already_exists' => 'Ese nombre de usuario ya está en uso.',
+    'uuid_already_exists' => 'Esa UUID ya está en uso.',
+    'unable_to_create_account' => 'La cuenta no ha podido ser creada.',
+    'unable_to_send_registration_email' => 'Su cuenta no ha podido ser activada. Contacte con un miembro del equipo administrativo para resolver este problema.',
+    'unable_to_find_user' => 'No se ha podido encontrar al usuario.',
+    'unable_to_find_group' => 'No se ha podido encontrar ese grupo.',
+    'unable_to_update_group' => 'No se ha podido actualizar el grupo de ese usuario.',
+    'report_content_too_long' => 'El contenido del reporte debe tener 255 carácteres o menos.',
+    'you_must_register_to_report' => 'Debe registrarse para poder reportar a un usuario.',
+    'you_have_been_banned_from_website' => 'Ha sido baneado del foro.',
+    'you_have_open_report_already' => 'Ya tiene un informe abierto con respecto a este usuario.',
+    'unable_to_create_report' => 'No se ha podido crear el reporte.',
+    'unable_to_update_username' => 'No se ha podido actualizar el nombre de usuario.',
+    'unable_to_update_server_info' => 'No se ha podido actualizar la información del servidor.',
+    'invalid_server_id' => 'La ID del servidor no es válida.',
+    'invalid_code' => 'El código proporcionado es inválido.',
+
+    // Success messages
+    'finish_registration_link' => 'Haga clic en el siguiente enlace para confirmar su registro:',
+    'finish_registration_email' => 'Revise su correo electrónico para confirmar el registro.',
+    'group_updated' => 'Grupo actualizado correctamente.',
+    'report_created' => 'Reporte creado correctamente.',
+    'new_private_message_from' => 'Nuevo mensaje privado de {x}', // Don't replace {x}
+    'username_updated' => 'Nombre de usuario actualizado correctamente.',
+    'server_info_updated' => 'Información del servidor actualizada correctamnete.',
+    'account_validated' => 'Cuenta verificada correctamente.'
+);
