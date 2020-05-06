8950f2de95d28e845581c70d4fc0b56eaed9b7ce
Fjuro <40403606+Fjuro@users.noreply.github.com>
2020-05-06 09:54:42 +0200

    Create Czech.php

---
+++ addons/Infractions/Czech.php
@@ -0,0 +1,38 @@
+ 
+<?php 
+/*
+ *	Made by Samerton
+ *  http://worldscapemc.co.uk
+ *
+ *  License: MIT
+ *  Copyright (c) 2016 Samerton
+ */
+
+// Language file for "Infractions" addon
+
+$infractions_language = array(
+	'infractions' => 'Přestupky',
+	'infractions_icon' => '', // Icon to display before "Infractions" in navbar
+	'view' => 'Zobrazit &raquo;',
+	'staff_member' => 'Pracovník',
+	'action' => 'Akce',
+	'actions' => 'Akce',
+	'ban' => 'Ban',
+	'temp_ban' => 'Dočasný ban',
+	'warning' => 'Varování',
+	'mute' => 'Ztlumení',
+	'kick' => 'Vykopnutí',
+	'created' => 'Vytvořeno',
+	'reason' => 'Důvod',
+	'expires_x' => 'Vyprší: {x}', // Don't replace {x}
+	'expired' => 'Vypršelo',
+	'expired_x' => 'Vypršelo: {x}', // Don't replace {x}
+	'active' => 'Aktivní',
+	'permanent' => 'Trvalé',
+	'viewing_infraction' => 'Prohlížení přestupku',
+	'user' => 'Uživatel:',
+	'status' => 'Stav:',
+	'no_reason' => 'Nebyl určen důvod.',
+	'revoked' => 'Odvoláno',
+	'revoked_by' => 'Odvolal {x}' // Don't replace {x}
+);
