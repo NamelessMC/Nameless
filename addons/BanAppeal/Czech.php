7e48e09a324cf6006e9d7901b06d5a7de7e9480d
Fjuro <40403606+Fjuro@users.noreply.github.com>
2020-05-06 10:09:41 +0200

    Create Czech.php

---
+++ addons/BanAppeal/Czech.php
@@ -0,0 +1,59 @@
+<?php 
+/*
+ *	Made by Partydragen
+ *  http://partydragen.com/
+ *
+ */
+// Language file for "banappeal" addon
+$banappeal_language = array(
+	'ban_appeal' => 'Žádost o odvolání banu',
+	'ban_appeal_icon' => '', // Icon to display before the text in the navbar
+	'viewing_open_ban_appeal' => 'Prohlížení <span class="label label-info">otevřených</span> žádostí o odvolání. Změnit na <a href="/mod/banappeal/?view=accepted"><span class="label label-success">přijmuté</span></a> nebo <a href="/mod/banappeal/?view=declined"><span class="label label-danger">zamítnuté</span></a>.',
+	'viewing_accepted_ban_appeal' => 'Prohlížení <span class="label label-success">přijmutých</span> žádostí o odvolání. Změnit na <a href="/mod/banappeal/"><span class="label label-info">otevřené</span></a> nebo <a href="/mod/banappeal/?view=declined"><span class="label label-danger">zamítnuté</span></a>.',
+	'viewing_declined_ban_appeal' => 'Prohlížení <span class="label label-danger">zamítnutých</span> žádostí o odvolání. Změnit na <a href="/mod/banappeal/"><span class="label label-info">otevřené</span></a> nebo <a href="/mod/banappeal/?view=accepted"><span class="label label-success">přijmuté</span></a>.',
+	'time_applied' => 'Aplikovaný čas',
+	'no_ban_appeal' => 'V této kategorii nejsou žádné žádosti o odvolání',
+	'viewing_ban_appeal_from' => 'Prohlížení žádosti o odvolání od hráče {x}', // Don't replace "{x}"
+	'open' => 'Otevřeno',
+	'accepted' => 'Přijmuto',
+	'declined' => 'Zamítnuto',
+	'accept' => 'Přijmout',
+	'decline' => 'Zamítnout',
+	'new_ban_appeal_submitted_alert' => 'Nová žádost o odvolání od hráče {x}', // Don't replace "{x}"
+	'new_comment' => 'Nový komentář',
+	'comment_error' => 'Ujistěte se, že váš komentář je 2 až 2048 znaků dlouhý.',
+	'comments' => 'Komentáře',
+	'comment_added' => 'Komentář přidán.',
+	'submit' => 'Odeslat',
+	'ban_appeal_submitted' => 'Žádost o odvolání banu byla úspěšně odeslána.',
+	'ban_appeal_already_submitted' => 'Již jste odeslali žádost o odvolání. Před odesláním další žádosti počkejte, než bude momentální žádost dokončena.',
+	'ban_appeal_accepted' => 'Vaše žádost o odvolání byla přijmuta.',
+	'ban_appeal_rejected' => 'Vaše žádost o odvolání byla zamítnuta.',
+	'permissions' => 'Oprávnění:',
+	'view_ban_appeal' => 'Zobrazit žádosti o odvolání banu?',
+	'accept_reject_ban_appeal' => 'Přijmout/odmítnout žádosti o odvolání banu?',
+	'questions' => 'Otázky:',
+	'question' => 'Otázka',
+	'type' => 'Typ',
+	'options' => 'Možnosti',
+	'options_help' => 'Každá možnost na nový řádek; může být prázdná (pouze rozbalovací nabídky)',
+	'no_questions' => 'Nebyla přidána žádná otázka.',
+	'new_question' => 'Nová otázka',
+	'editing_question' => 'Upravování otázky',
+	'delete_question' => 'Smazat otázku',
+	'dropdown' => 'Rozbalovací nabídka',
+	'text' => 'Text',
+	'textarea' => 'Oblast textu',
+	'question_deleted' => 'Otázka smazána',
+	'group' => 'Skupina',
+	'groups' => 'Skupiny',
+	'name' => 'Název',
+	'confirm_cancellation' => 'Jste si jistí?',
+// Error messages
+	'name_required' => 'Je vyžadován název.',
+	'question_required' => 'Je vyžadována otázka.',
+	'name_minimum' => 'Název musí obsahovat minimálně 2 znaky.',
+	'question_minimum' => 'Otázka musí obsahovat minimálně 2 znaky.',
+	'name_maximum' => 'Název může obsahovat maximálně 16 znaků.',
+	'question_maximum' => 'Otázka může obsahovat maximálně 16 znaků.',
+);
