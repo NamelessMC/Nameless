98407bed5d60617376e21a94a49b3cc02adf708e
Fjuro <40403606+Fjuro@users.noreply.github.com>
2020-05-06 09:48:05 +0200

    Create Czech.php

---
+++ addons/Donate/Czech.php
@@ -0,0 +1,21 @@
+<?php 
+/*
+ *	Made by Samerton
+ *  http://worldscapemc.co.uk
+ *
+ *  License: MIT
+ * Copyright (c) 2016 Samerton
+ */
+
+// Language file for "Donate" addon
+
+$donate_language = array(
+	'donate' => 'Přispět',
+	'donate_icon' => '', // Icon to display before "Donate" in navbar
+	'latest_donors' => 'Nejnovější přispěvatelé',
+	'agree_with_terms' => 'Přispěním souhlasíte s pravidly a podmínkami',
+	'link' => '(Odkaz)',
+	'agree' => 'Souhlasím &raquo;',
+	'cancel' => 'Zrušit',
+	'select' => 'Vybrat'
+);
