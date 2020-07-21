9e7505ea0cec3ac862cc2f44bda99e699a3aaf82
Tadhg <tadhgsmboyle@gmail.com>
2020-07-21 14:04:00 -0700

    Apply role change on user edit page - still some things to flush out though

--- modules/Core/pages/panel/users_edit.php
+++ modules/Core/pages/panel/users_edit.php
@@ -191,7 +191,29 @@ if(Input::exists()){
 			$validation = $validate->check($_POST, $to_validation);
 
 			if($validation->passed()){
+
 				try {
+					// The new group is not the same as their old group
+					if ($user_query->group_id != $group) {
+						// Discord integration is enabled
+						if ($queries->getWhere('settings', array('name', '=', 'discord_integration'))[0]->value == '1') {
+							// They have a valid discord Id
+							if ($user_query->discord_id != null && $user_query->discord_id != 010) {
+								$group_discord_id = $queries->getWhere('groups', array('id', '=', $group))[0]->discord_role_id;
+								$old_group_discord_id = $queries->getWhere('groups', array('id', '=', $user_query->group_id))[0]->discord_role_id;
+								// The role has a valid discord id OR the old group has valid id
+								// Then the bot will receive the new or old group and add/remove as needed
+								if ($group_discord_id != null || $old_group_discord_id != null) {
+									$bot_url = 'http://localhost:8001';
+									$api_key = $queries->getWhere('settings', array('name', '=', 'mc_api_key'))[0]->value;
+									$api_url = rtrim(Util::getSelfURL(), '/') . rtrim(URL::build('/api/v2/' . Output::getClean($api_key), '', 'non-friendly'), '/');
+									$full_url = $bot_url . '/roleChange?id=' . $user_query->discord_id . '&guild_id=' . $queries->getWhere('settings', array('name', '=', 'discord'))[0]->value . '&role=' . $group_discord_id . '&oldRole=' . $old_group_discord_id;
+									file_get_contents($full_url);
+								}
+							}
+						}
+					}
+
 					// Signature from Markdown -> HTML if needed
 					$cache->setCache('post_formatting');
 					$formatting = $cache->retrieve('formatting');
