38cbfc985388685923aa9acea4293425f427fc6b
Michele Carta <68933611+michelecursed@users.noreply.github.com>
2020-08-05 20:34:22 +0200

    Create Italian.php (#1792)
    + translated file

---
+++ addons/BanAppeal/Italian.php
@@ -0,0 +1,59 @@
+<?php 
+/*
+ *	Made by michelecursed
+ *  https://github.com/michelecursed
+ *
+ */
+// Language file for "banappeal" addon
+$banappeal_language = array(
+	'ban_appeal' => 'Appello ban',
+	'ban_appeal_icon' => '', // Icon to display before the text in the navbar
+	'viewing_open_ban_appeal' => 'Visualizzando gli appelli ban <span class="label label-info">aperti</span>. Cambia a <a href="/mod/banappeal/?view=accepted"><span class="label label-success">accettato</span></a> o <a href="/mod/banappeal/?view=declined"><span class="label label-danger">rifiutato</span></a>.',
+	'viewing_accepted_ban_appeal' => 'Visualizzando gli appelli ban <span class="label label-success">accettati</span>. Cambia a <a href="/mod/banappeal/"><span class="label label-info">aperto</span></a> o <a href="/mod/banappeal/?view=declined"><span class="label label-danger">rifiutato</span></a>.',
+	'viewing_declined_ban_appeal' => 'Visualizzando gli appelli ban <span class="label label-danger">rifiutati</span>. Cambia a <a href="/mod/banappeal/"><span class="label label-info">aperto</span></a> o <a href="/mod/banappeal/?view=accepted"><span class="label label-success">accettato</span></a>.',
+	'time_applied' => 'Tempo applicato',
+	'no_ban_appeal' => 'Nessun appello ban in questa categoria',
+	'viewing_ban_appeal_from' => 'Visualizzando appelli ban da {x}', // Don't replace "{x}"
+	'open' => 'Aperto',
+	'accepted' => 'Accettato',
+	'declined' => 'Rifiutato',
+	'accept' => 'Accetta',
+	'decline' => 'Rifiuta',
+	'new_ban_appeal_submitted_alert' => 'Nuovo appello ban inviato da {x}', // Don't replace "{x}"
+	'new_comment' => 'Nuovo commento',
+	'comment_error' => 'Per favore, assicurati che il tuo commento sia tra i 2 e i 2048 caratteri.',
+	'comments' => 'Commenti',
+	'comment_added' => 'Commento aggiunto.',
+	'submit' => 'Invia',
+	'ban_appeal_submitted' => 'Appello ban inviato con successo.',
+	'ban_appeal_already_submitted' => 'Hai gi&agrave; inviato un appello ban. Per favore, aspetta che venga visionato prima di inviarne un altro.',
+	'ban_appeal_accepted' => 'Il tuo appello ban &egrave; stato accettato.',
+	'ban_appeal_rejected' => 'Il tuo appello ban &egrave; stato rifiutato.',
+	'permissions' => 'Permessi:',
+	'view_ban_appeal' => 'Visualizzare appelli ban?',
+	'accept_reject_ban_appeal' => 'Accetta/Rifiuta appelli ban?',
+	'questions' => 'Domande:',
+	'question' => 'Domanda',
+	'type' => 'Tipo',
+	'options' => 'Opzioni',
+	'options_help' => 'Ogni opzione in una nuova riga; pu&ograve; essere lasciato vuoto (dropdowns only)',
+	'no_questions' => 'Non c\'&egrave; nessuna domanda.',
+	'new_question' => 'Nuova domanda',
+	'editing_question' => 'Modificando domanda',
+	'delete_question' => 'Elimina domanda',
+	'dropdown' => 'Dropdown',
+	'text' => 'Testo',
+	'textarea' => 'Area testo',
+	'question_deleted' => 'Domanda rimossa',
+	'group' => 'Gruppo',
+	'groups' => 'Gruppi',
+	'name' => 'Nome',
+	'confirm_cancellation' => 'Sei sicuro di voler cancellare?',
+// Error messages
+	'name_required' => 'Il nome &egrave; obbligatorio.',
+	'question_required' => 'La domanda &egrave; obbligatoria.',
+	'name_minimum' => 'Il nome deve essere di almeno 2 caratteri.',
+	'question_minimum' => 'La domanda deve essere di almeno 2 caratteri.',
+	'name_maximum' => 'Il nome deve contenere massimo 16 caratteri.',
+	'question_maximum' => 'La domanda deve contenere massimo 16 caratteri.',
+);
