d055d329ca28b01b907f9f28ba07ad67912d8880
maiu <25614237+Maiu15@users.noreply.github.com>
2020-08-08 22:49:55 +0200

    Update api.php

--- custom/languages/Norwegian/api.php
+++ custom/languages/Norwegian/api.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Translations by Maiu#7777
+ *  Translations by Maiu
  *  https://github.com/NamelessMC/Nameless/
  *  NamelessMC versjon 2.0.0-pr6
  *
@@ -12,44 +12,44 @@
 $language = array(
     // Errors
     'unknown_error' => 'Ukjent feil',
-    'invalid_api_key' => 'Ugyldig API nøkkel',
+    'invalid_api_key' => 'Ugyldig API-nøkkel',
     'invalid_language_file' => 'Ugyldig språkfil',
-    'invalid_api_method' => 'Ugyldig API method',
-    'no_unique_site_id' => 'Ingen unik nettside ID tilgjengelig',
+    'invalid_api_method' => 'Ugyldig API-metode',
+    'no_unique_site_id' => 'Ingen unik nettside-ID tilgjengelig',
     'unable_to_check_for_updates' => 'Kunne ikke søke etter oppdateringer',
-    'invalid_post_contents' => 'Ugyldig POST innhold',
-    'invalid_get_contents' => 'Ugyldig GET innhold',
-    'invalid_email_address' => 'Ugyldig email adresse',
+    'invalid_post_contents' => 'Ugyldig POST-innhold',
+    'invalid_get_contents' => 'Ugyldig GET-innhold',
+    'invalid_email_address' => 'Ugyldig e-postadresse',
     'invalid_username' => 'Ugyldig brukernavn',
     'invalid_uuid' => 'Ugyldig UUID',
-    'email_already_exists' => 'Email eksisterer allerede',
-    'username_already_exists' => 'Brukernavn eksisterer allerede',
+    'email_already_exists' => 'E-postadressen eksisterer allerede',
+    'username_already_exists' => 'Brukernavnet eksisterer allerede',
     'uuid_already_exists' => 'UUID eksisterer allerede',
     'unable_to_create_account' => 'Vi kunne ikke registrere kontoen',
-    'unable_to_send_registration_email' => 'Vi kunne ikke sende en registreringsmail, vennligst kontakt en administrator for å aktivere kontoen',
+    'unable_to_send_registration_email' => 'Vi kunne ikke sende en registerings-e-post, vennligst kontakt en administrator for å aktivere kontoen',
     'unable_to_find_user' => 'Kunne ikke finne bruker',
     'unable_to_find_group' => 'Kunne ikke finne gruppe',
     'unable_to_update_group' => 'Kunne ikke oppdatere brukerens gruppe',
-    'report_content_too_long' => 'Rapporteringen må inneholde mindre enn 255 tegn',
+    'report_content_too_long' => 'Rapporten må inneholde mindre enn 255 tegn',
     'you_must_register_to_report' => 'Du må registrere deg for å kunne rapportere',
     'you_have_been_banned_from_website' => 'Du har blitt bannlyst fra nettsiden',
     'you_have_open_report_already' => 'Du har allerede en åpen rapportering angående denne spilleren',
     'unable_to_create_report' => 'Kunne ikke opprette rapportering',
     'unable_to_update_username' => 'Kunne ikke oppdatere brukernavn',
     'unable_to_update_server_info' => 'Kunne ikke oppdatere serverinfo',
-    'invalid_server_id' => 'Ugyldig server ID',
+    'invalid_server_id' => 'Ugyldig server-ID',
     'invalid_code' => 'Ugyldig kode oppgitt',
-    'discord_integration_disabled' => 'Discord integration is disabled.',
-    'unable_to_set_discord_id' => 'Unable to set Discord ID.',
+    'discord_integration_disabled' => 'Discord-integrering er deaktivert.',
+    'unable_to_set_discord_id' => 'Kunne ikke oppdatere Discord-IDen.',
 
     // Success messages
     'finish_registration_link' => 'Vennligst klikk på følgende lenke for å fullføre registeringen:',
-    'finish_registration_email' => 'Vennligst sjekk emailen din for å fullføre registeringen.',
-    'group_updated' => 'Gruppe suksessfullt oppdatert',
-    'report_created' => 'Rapportering suksessfullt opprettet',
+    'finish_registration_email' => 'Vennligst sjekk inboksen din for å fullføre registeringen.',
+    'group_updated' => 'Gruppen har blitt oppdatert',
+    'report_created' => 'Rapporten har blitt opprettet',
     'new_private_message_from' => 'Ny privatmelding fra {x}', // Don't replace {x}
-    'username_updated' => 'Brukernavn suksessfullt oppdatert',
-    'server_info_updated' => 'Serverinfo suksessfullt oppdatert',
-	'account_validated' => 'Brukeren har blitt validert',
-    'discord_id_set' => 'Discord ID set successfully'
+    'username_updated' => 'Brukernavnet har blitt oppdatert.',
+    'server_info_updated' => 'Serverinfoen har blitt oppdatert',
+    'account_validated' => 'Brukeren har blitt validert',
+    'discord_id_set' => 'Discord-IDen har blitt endret.'
 );
