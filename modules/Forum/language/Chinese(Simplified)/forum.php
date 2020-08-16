27cd610261ff1fc745c846e02bc7508fc7b24db3
ahdg <36436808+ahdg6@users.noreply.github.com>
2020-08-16 22:16:47 +0800

    Update forum.php

--- modules/Forum/language/Chinese(Simplified)/forum.php
+++ modules/Forum/language/Chinese(Simplified)/forum.php
@@ -1,8 +1,8 @@
 <?php 
 /*
  *	Made by Samerton
- *  https://github。com/NamelessMC/Nameless/
- *  NamelessMC version 2。0。0-pr5
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr5
  *
  *  License: MIT
  *
