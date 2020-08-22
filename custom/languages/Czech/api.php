53f9a41e18d93a7c1fa8031984d179193e808218
Fjuro <40403606+Fjuro@users.noreply.github.com>
2020-08-23 01:09:16 +0200

    Updated Translations
    Translations are now consistent

--- custom/languages/Czech/api.php
+++ custom/languages/Czech/api.php
@@ -11,8 +11,8 @@
 $language = array(
     // Errors
     'unknown_error' => 'Neznámá chyba',
-    'invalid_api_key' => 'Neplatný API Key',
-    'invalid_language_file' => 'Neplatný soubor jazyk',
+    'invalid_api_key' => 'Neplatný klíč API',
+    'invalid_language_file' => 'Neplatný jazykový soubor',
     'invalid_api_method' => 'Neplatná API metoda',
     'no_unique_site_id' => 'Není dostupné žádné ID stránky',
     'unable_to_check_for_updates' => 'Nelze zjistit aktualizace',
@@ -21,34 +21,34 @@ $language = array(
     'invalid_email_address' => 'Neplatný e-mail',
     'invalid_username' => 'Neplatné jméno',
     'invalid_uuid' => 'Neplatné UUID',
-    'email_already_exists' => 'Email již existuje',
+    'email_already_exists' => 'E-mail již existuje',
     'username_already_exists' => 'Jméno již existuje',
     'uuid_already_exists' => 'UUID již existuje',
     'unable_to_create_account' => 'Nelze vytvořit účet',
-    'unable_to_send_registration_email' => 'Nelze poslat registrační mail, kontaktuj administrátora',
+    'unable_to_send_registration_email' => 'Nelze poslat registrační mail, kontaktujte administrátora',
     'unable_to_find_user' => 'Nelze najít uživatele',
     'unable_to_find_group' => 'Nelze najít skupinu',
     'unable_to_update_group' => 'Nelze aktualizovat hráčovu skupinu',
-    'report_content_too_long' => 'Nahlášení nesmí být delší jak 255 znaků',
-    'you_must_register_to_report' => 'Musíte být registrován a ověřen, aby jsi mohl napsat nahlášení',
-    'you_have_been_banned_from_website' => 'Byl jste zabanován z této stránky',
-    'you_have_open_report_already' => 'Již jste otevřel nahlášení na tohoto hráče',
+    'report_content_too_long' => 'Nahlášení může být maximláně 255 znaků dlouhé',
+    'you_must_register_to_report' => 'Musíte být registrováni a ověřeni pro poslání nahlášení',
+    'you_have_been_banned_from_website' => 'Byli jste zabanován z tohoto webu',
+    'you_have_open_report_already' => 'Již jste otevřeli nahlášení na tohoto hráče',
     'unable_to_create_report' => 'Nelze vytvořit nahlášení',
     'unable_to_update_username' => 'Nelze aktualizovat jméno',
     'unable_to_update_server_info' => 'Nelze aktualizovat informace o serveru',
     'invalid_server_id' => 'Neznámé ID serveru',
-    'invalid_code' => 'Byl vložen neznámý kód',
-    'discord_integration_disabled' => 'Discord integration is disabled.',
-    'unable_to_set_discord_id' => 'Unable to set Discord ID.',
+    'invalid_code' => 'Byl zadán neznámý kód',
+    'discord_integration_disabled' => 'Integrace Discordu je zakázána.',
+    'unable_to_set_discord_id' => 'Nepodařilo se nastavit Discord ID.',
     
     // Success messages
-    'finish_registration_link' => 'Prosím klikněte na tento odkaz pro dokončení registrace:',
+    'finish_registration_link' => 'Klikněte na tento odkaz pro dokončení registrace:',
     'finish_registration_email' => 'Zkontrolujte email pro dokončení registrace.',
     'group_updated' => 'Skupina úspěšně aktualizována',
     'report_created' => 'Nahlášení úspěšně vytvořeno',
-    'new_private_message_from' => 'Nová soukromá zpráva od {x}', // Don't replace {x}
+    'new_private_message_from' => 'Nová soukromá zpráva od uživatele {x}', // Don't replace {x}
     'username_updated' => 'Jméno úspěšně aktualizováno',
     'server_info_updated' => 'Informace o serveru aktualizovány',
 	'account_validated' => 'Účet úspěšně ověřen.',
-    'discord_id_set' => 'Discord ID set successfully'
+    'discord_id_set' => 'Discord ID úspěšně nastaveno'
 );
