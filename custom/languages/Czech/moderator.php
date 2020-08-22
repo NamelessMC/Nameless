b92e191f10533501421d025a582722e79eddcbe2
Fjuro <40403606+Fjuro@users.noreply.github.com>
2020-08-23 01:33:00 +0200

    Update Translations
    Translations are now consistent

--- custom/languages/Czech/moderator.php
+++ custom/languages/Czech/moderator.php
@@ -10,14 +10,14 @@
  */
 
 $language = array(
-	'mod_cp' => 'Ovládací panel moderátora',
-	'staff_cp' => 'AdminPanel',
+	'mod_cp' => 'Moderátorský panel',
+	'staff_cp' => 'Ovládací panel',
 	'overview' => 'Přehled',
 	
 	// Spam
 	'spam' => 'Spam',
 	'mark_as_spam' => 'Označit jako spam',
-	'confirm_spam' => '<p>Opravdu chcete označit tohohle uživatele za spam?</p><p>Uživatel bude zabanován a veškerý jeho obsah odebrán..</p>',
+	'confirm_spam' => '<p>Opravdu chcete označen tohoto uživatele za spam?</p><p>Uživatel bude zabanován a veškerý jeho obsah odebrán.</p>',
 	'user_marked_as_spam' => 'Uživatel byl úpěšně označen za spam.',
 	'cant_ban_admin' => 'Nemůžete zabanovat administrátora!',
 	
@@ -27,9 +27,9 @@ $language = array(
 	'no_ips_with_username' => 'Pro daného uživatele nebyly nalezeny žádné IP adresy.',
 	'no_accounts_with_that_ip' => 'Pro danou IP adresu nebyl nalezen žádný uživatel.',
 	'1_account_with_ip' => 'Nalezen 1 účet s IP {y}', // Don't replace "{y}"
-	'count_accounts_with_ip' => 'Nalezl jsem {x} účtů pro IP {y}', // Don't replace "{x}" or "{y}"
+	'count_accounts_with_ip' => 'Nalezeno {x} účtů s IP {y}', // Don't replace "{x}" or "{y}"
 	'1_ip_with_name' => 'Nalezena 1 IP adresa pro uživatele {y}', // Don't replace "{y}"
-	'count_ips_with_name' => 'Nalezl jsem {x} IP adres  pro uživatele {y}', // Don't replace "{x}" or "{y}"
+	'count_ips_with_name' => 'Nalezeno {x} IP adres pro uživatele {y}', // Don't replace "{x}" or "{y}"
 	'no_users_or_ips_found' => 'Žádný uživatel nebo IP adresa nenalezena.',
 	
 	// Reports
@@ -45,16 +45,16 @@ $language = array(
 	'view_content' => 'Zobrazit nahlášený příspěvek',
 	'no_comments' => 'Žádné komentáře',
 	'new_comment' => 'Nový komentář',
-	'report_comment_invalid' => 'Nezadal jste komentář. Zajistěte, aby jste zadal komentář s minimálně 5 znaky.',
+	'report_comment_invalid' => 'Nezadali jste komentář. Musíte zadat komentář s minimálně 5 znaky.',
 	'close_report' => 'Zavřít nahlášení',
 	'reopen_report' => 'Znovu otevřít náhlášení',
 	'1_open_report' => 'V současné době je otevřeno <strong> 1 </strong> nahlášení.',
-	'open_reports' => 'V současné době je <strong>{x}</strong> otevřených nahlášení.', // Don't replace {x}
-	'no_open_reports' => 'V současné době nejsou otevřené žádné nahlášení.',
+	'open_reports' => 'V současné době je otevřeno <strong>{x}</strong> nahlášení.', // Don't replace {x}
+	'no_open_reports' => 'V současné době nejsou otevřena žádná nahlášení.',
 	'no_closed_reports' => 'V současné době není uzavřeno žádné nahlášení.',
-	'recent_reports' => 'Nedávná Nahlášení',
-	'reported_by' => 'Nahlášen od:',
-	'website' => 'Webová stránka',
+	'recent_reports' => 'Nedávná nahlášení',
+	'reported_by' => 'Nahlášen uživatelem:',
+	'website' => 'Web',
 	'ingame' => 'Ve hře',
 	'x_closed_report' => '{x} uzavřel toto nahlášení.', // Don't replace {x} (username)
 	'x_reopened_report' => '{x} toto nahlášení znovu otevřel.', // Don't replace {x} (username)
@@ -85,24 +85,24 @@ $language = array(
 	'revoke' => 'Odvolat trest',
 	'revoked' => 'Trest odvolán',
 	'acknowledged' => 'Uznáno.',
-	'confirm_revoke_warning' => 'Jste si jist, že chcete odvolat toto varování?',
-	'confirm_revoke_ban' => 'Jste si jist, že chcete odvolat zákaz přístupu ke stránce? Přístup ke stránce bude uživateli povolen, i když mají jiný zákaz k přístupu!',
+	'confirm_revoke_warning' => 'Jste si jisti, že chcete odvolat toto varování?',
+	'confirm_revoke_ban' => 'Jste si jisti, že chcete odvolat zákaz přístupu ke stránce? Přístup ke stránce bude uživateli povolen, i když mají jiný zákaz k přístupu!',
 	'punishment_revoked' => 'Trest odvolán.',
-	'punishment_revoked_alert' => 'Člen AT {x} odvolal trest uživateli {y}', // Don't replace {x} (staff member) or {y} (user with revoked punishment)
+	'punishment_revoked_alert' => 'Pracovník {x} odvolal trest uživateli {y}', // Don't replace {x} (staff member) or {y} (user with revoked punishment)
 	'cant_punish_admin' => 'Nemůžete potrestat administrátora!',
     'viewing_all_punishments' => 'Prohlížení všech trestů',
     'no_punishments_found' => 'Žádné tresty nenalezeny.',
     'view_user' => 'Prohlédnout hráče',
     'when' => 'Kdy',
-    'staff' => 'Člen AT',
+    'staff' => 'Pracovník',
     'type' => 'Typ',
     'recent_punishments' => 'Nedávné tresty',
     'created' => 'Vytvořeno:',
-    'staff:' => 'Admin:',
+    'staff:' => 'Pracovník:',
     'reason:' => 'Důvod:',
 	
     // Users
     'recent_registrations' => 'Nedávné registrace',
-	'reset_profile_banner' => 'Resetovat Banner profilu'
+	'reset_profile_banner' => 'Resetovat banner profilu'
 
 );
