74b265db1d683ef5621f6f688a350151139c4862
Fjuro <40403606+Fjuro@users.noreply.github.com>
2020-08-23 01:11:05 +0200

    Update Translations
    Translations are now consistent

--- custom/languages/Czech/emails.php
+++ custom/languages/Czech/emails.php
@@ -13,17 +13,17 @@ $language = array(
 
     // General Options
     'greeting' => 'Zdravím,',
-    'thanks' => 'Thanks,',
+    'thanks' => 'S pozdravem',
 
     // Registration Email
-    'register_subject' => 'Ověřit účet',
+    'register_subject' => 'Ověření účtu',
     'register_message' => 'Děkujeme za registraci na našem webu! Pro dokončení registrace klikněte na odkaz níže:',
 
     // Change Password
     'change_password_subject' => 'Přejeme příjemné hraní,',
-    'change_password_message' => 'Pro obnovení Vašeho zapomenutého hesla klikněte prosím na následující odkaz. Pokud jste o obnovení zapomenutého hesla nepožádal sám, můžete e-mail bezpečně smazat.',
+    'change_password_message' => 'Pro obnovení vašeho zapomenutého hesla klikněte na následující odkaz. Pokud jste o obnovení zapomenutého hesla nepožádali, můžete e-mail ignorovat.',
 
     // Forum Topic Replies
-    'forum_topic_reply_subject' => '{x} has replied to {y}', // Don't remove {x} or {y}
-    'forum_topic_reply_message' => '{x} has replied to a topic you follow. Content: {z}' // Don't remove {x} or {z}
+    'forum_topic_reply_subject' => '{x} odpověděl na {y}', // Don't remove {x} or {y}
+    'forum_topic_reply_message' => '{x} odpověděl na téma, které sledujete. Obsah: {z}' // Don't remove {x} or {z}
 );
