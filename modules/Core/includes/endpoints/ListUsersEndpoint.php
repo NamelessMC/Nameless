7d6176abdd99915cc04acf40426a03c26278612f
Tadhg <tadhgsmboyle@gmail.com>
2020-09-26 11:12:41 -0700

    Add listUsers filters

--- modules/Core/includes/endpoints/ListUsersEndpoint.php
+++ modules/Core/includes/endpoints/ListUsersEndpoint.php
@@ -15,6 +15,15 @@ class ListUsersEndpoint extends EndpointBase {
 
     public function execute(Nameless2API $api) {
         if ($api->isValidated()) {
+
+            if (isset($_GET['banned'])) {
+                $banned = ($_GET['banned'] == 'false') ? false : true;
+            } else $banned = true;
+            
+            if (isset($_GET['active'])) {
+                $active = ($_GET['active'] == 'false') ? false : true;
+            } else $active = true;
+
             $users = $api->getDb()->query('SELECT id, username, uuid, isbanned AS banned, active FROM nl2_users')->results();
 
             $users_array = array();
@@ -22,8 +31,8 @@ class ListUsersEndpoint extends EndpointBase {
                 $users_array[$user->id]['id'] = intval($user->id);
                 $users_array[$user->id]['username'] = $user->username;
                 $users_array[$user->id]['uuid'] = $user->uuid;
-                $users_array[$user->id]['banned'] = ($user->banned) ? true : false;
-                $users_array[$user->id]['active'] = ($user->active) ? true : false;
+                if ($banned) $users_array[$user->id]['banned'] = ($user->banned) ? true : false;
+                if ($active) $users_array[$user->id]['active'] = ($user->active) ? true : false;
             }
 
             $api->returnArray(array('users' => $users_array));
