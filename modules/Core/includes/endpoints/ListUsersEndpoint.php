3cd07250914d34c97419c2193d716981bf7de245
Tadhg <tadhgsmboyle@gmail.com>
2020-09-27 16:32:53 -0700

    Rework #1839

--- modules/Core/includes/endpoints/ListUsersEndpoint.php
+++ modules/Core/includes/endpoints/ListUsersEndpoint.php
@@ -16,23 +16,37 @@ class ListUsersEndpoint extends EndpointBase {
     public function execute(Nameless2API $api) {
         if ($api->isValidated()) {
 
+            $sortBans = false;
+            $sortActive = false;
+
             if (isset($_GET['banned'])) {
                 $banned = ($_GET['banned'] == 'false') ? false : true;
-            } else $banned = true;
+                $sortBans = true;
+            }
             
             if (isset($_GET['active'])) {
                 $active = ($_GET['active'] == 'false') ? false : true;
-            } else $active = true;
+                $sortActive = true;
+            }
 
             $users = $api->getDb()->query('SELECT id, username, uuid, isbanned AS banned, active FROM nl2_users')->results();
 
             $users_array = array();
             foreach ($users as $user) {
+                if ($sortBans) {
+                    if (!$banned && $user->banned) continue;
+                    if ($banned && !$user->banned) continue;
+                }
+                if ($sortActive) {
+                    if (!$active && $user->active) continue;
+                    if ($active && !$user->active) continue;
+                }
                 $users_array[$user->id]['id'] = intval($user->id);
                 $users_array[$user->id]['username'] = $user->username;
                 $users_array[$user->id]['uuid'] = $user->uuid;
-                if ($banned) $users_array[$user->id]['banned'] = ($user->banned) ? true : false;
-                if ($active) $users_array[$user->id]['active'] = ($user->active) ? true : false;
+                $users_array[$user->id]['banned'] = ($user->banned) ? true : false;
+                $users_array[$user->id]['active'] = ($user->active) ? true : false;
+                
             }
 
             $api->returnArray(array('users' => $users_array));
