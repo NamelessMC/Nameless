52966ef8b4195363d5cb2ca62db4198bad154bb2
Oniel <51058739+0niel@users.noreply.github.com>
2020-10-10 12:50:06 +0300

    var name fix

--- modules/Forum/classes/Forum.php
+++ modules/Forum/classes/Forum.php
@@ -606,21 +606,21 @@ class Forum {
 	}
 	
 	// Converting Cyrillic to Latin letters (https://en.wikipedia.org/wiki/ISO_9)
-	public function russianToLatin($string)
+	public function cyrillicToLatin($string)
 	{
-		$curillic = [
+		$cyrillic = [
             'а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п',
             'р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я',
             'А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П',
             'Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я'
-        ];
+		];
         $latin = [
             'a','b','v','g','d','e','io','zh','z','i','y','k','l','m','n','o','p',
             'r','s','t','u','f','h','ts','ch','sh','sht','a','i','y','e','yu','ya',
             'A','B','V','G','D','E','Io','Zh','Z','I','Y','K','L','M','N','O','P',
             'R','S','T','U','F','H','Ts','Ch','Sh','Sht','A','I','Y','e','Yu','Ya'
         ];
-        return str_replace($curillic, $latin, $string);
+        return str_replace($cyrillic, $latin, $string);
 	}
 	
 	// Transform a topic title to URL-ify it
