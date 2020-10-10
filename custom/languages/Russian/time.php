aa82dbd119266cff8d53892485d2385c4b674599
Oniel <51058739+0niel@users.noreply.github.com>
2020-10-10 10:23:51 +0300

    Russian language (#1861)
    Most of the strings visible to users have been translated

---
+++ custom/languages/Russian/time.php
@@ -0,0 +1,29 @@
+<?php 
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-dev
+ *
+ *  License: MIT
+ *
+ *  Russian Language - Time
+ */
+
+$language = array(
+	/*
+	 *  Time
+	 */
+	'time' => 'time', // Placeholder, no need to translate
+	'seconds_short' => 'сек', // Shortened "seconds", eg "s"
+	'less_than_a_minute' => 'меньше минуты назад',
+	'1_minute' => '1 минуту назад',
+	'_minutes' => '{x} минут назад',
+	'about_1_hour' => 'около 1 часа назад',
+	'_hours' => '{x} часов назад',
+	'1_day' => '1 день назад',
+	'_days' => '{x} дней назад',
+	'about_1_month' => 'около 1 месяца назад',
+	'_months' => '{x} месяцев назад',
+	'about_1_year' => 'около 1 года назад',
+	'over_x_years' => 'более {x} лет назад'
+);
