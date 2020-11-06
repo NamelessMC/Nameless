a735ac789aec66b8147689f450f5e3d3e1577043
Dreta <yangmain3@gmail.com>
2020-11-06 19:14:44 +0800

    Update api.php

--- custom/languages/Chinese(Simplified)/api.php
+++ custom/languages/Chinese(Simplified)/api.php
@@ -7,7 +7,7 @@
  *  License: MIT
  *
  *  Chinese Simplified Language - API
- *  Translation(Chinese Simplified) by ahdg,lian20,Dreta (yangyang200)
+ *  Translation(Chinese Simplified) by ahdg,lian20,Dreta
  *  Translation progress(v2-pr8) : 100%
  */
 
@@ -28,15 +28,15 @@ $language = array(
     'username_already_exists' => '用户名已存在',
     'uuid_already_exists' => 'UUID 已存在',
     'unable_to_create_account' => '未能创建账号',
-    'unable_to_send_registration_email' => '未能发送注册邮件,请联系管理员激活您的账号',
+    'unable_to_send_registration_email' => '未能发送注册邮件, 请联系管理员激活您的账号',
     'unable_to_find_user' => '未能找到用户',
     'unable_to_find_group' => '未能找到权限组',
     'unable_to_update_group' => '未能找到用户的权限组',
     'report_content_too_long' => '报告内容必须少于 255 个字符',
-    'you_must_register_to_report' => '你必须在我们的网站上注册才能反馈',
-    'you_have_been_banned_from_website' => '你已被我们的网站封禁',
-    'cannot_report_yourself' => 'You cannot report yourself.',
-    'you_have_open_report_already' => '你已经有一篇关于该玩家的公开反馈',
+    'you_must_register_to_report' => '您必须在我们的网站上注册才能反馈',
+    'you_have_been_banned_from_website' => '您已被我们的网站封禁',
+    'cannot_report_yourself' => '您不能举报您自己。',
+    'you_have_open_report_already' => '您已经有一篇关于该玩家的公开反馈',
     'unable_to_create_report' => '未能反馈',
     'unable_to_update_username' => '未能更新用户名',
     'unable_to_update_server_info' => '未能更新服务器信息',
@@ -45,7 +45,7 @@ $language = array(
     'user_already_active' => '您的帐号已经被激活过了。',
     'discord_integration_disabled' => '已关闭 Discord 集成。',
     'unable_to_set_discord_id' => '无法设置 Discord ID。',
-    'unable_to_set_discord_bot_url' => 'Unable to set Discord bot URL',
+    'unable_to_set_discord_bot_url' => '无法设置 Discord 机器人 URL',
 
     // Success messages
     'finish_registration_link' => '请点击该链接以完成注册:',
@@ -57,5 +57,5 @@ $language = array(
     'server_info_updated' => '服务器信息更新成功',
     'account_validated' => '账号校验成功',
     'discord_id_set' => 'Discord ID 更新成功',
-    'discord_bot_url_updated' => 'Discord bot URL updated successfully'
+    'discord_bot_url_updated' => 'Discord 机器人 URL 更新成功'
 );
