f665d970e8156f318b51876789d60250860e857a
Fjuro <40403606+Fjuro@users.noreply.github.com>
2020-12-26 19:27:15 +0100

    Update log.php

--- custom/languages/Czech/log.php
+++ custom/languages/Czech/log.php
@@ -1,6 +1,6 @@
 <?php
 $language = array(
     //forums
-    'info_forums_lock' => 'Locked',
-    'info_forums_unlock' => 'Unlocked',
+    'info_forums_lock' => 'Zamčeno',
+    'info_forums_unlock' => 'Odemčeno',
 );
