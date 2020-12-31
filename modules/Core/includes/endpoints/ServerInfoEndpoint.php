97cec06cf3474e54a2ceda2ad1a0b682f3468af7
Tadhg <tadhgsmboyle@gmail.com>
2020-12-30 19:36:59 -0800

    nevermind, actually fix endpoint now

--- modules/Core/includes/endpoints/ServerInfoEndpoint.php
+++ modules/Core/includes/endpoints/ServerInfoEndpoint.php
@@ -9,7 +9,10 @@ class ServerInfoEndpoint extends EndpointBase {
     }
 
     public function execute(Nameless2API $api) {
-        $api->validateParams($_POST, ['server-id', 'max-memory', 'free-memory', 'allocated-memory', 'tps', 'groups', 'players']);
+        $api->validateParams($_POST, ['server-id', 'max-memory', 'free-memory', 'allocated-memory', 'tps', 'groups']);
+        if (!isset($_POST['players'])) {
+            $api->throwError(6, $this->_language->get('api', 'invalid_post_contents'), 'players');
+        }
 
         $serverId = $_POST['server-id'];
         // Ensure server exists
