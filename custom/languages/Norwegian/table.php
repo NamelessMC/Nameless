3eabb20563d681b2ecf3112c7a8a1ceafc4ea916
maiu <25614237+Maiu15@users.noreply.github.com>
2021-01-01 19:00:21 +0100

    Update table.php

--- custom/languages/Norwegian/table.php
+++ custom/languages/Norwegian/table.php
@@ -13,7 +13,7 @@ $language = array(
     /*
      *  Tables
      */
-    'display_records_per_page' => 'Vis _MENU_-rekorder siden', // Don't replace "_MENU_"
+    'display_records_per_page' => 'Vis _MENU_ rekorder', // Don't replace "_MENU_"
     'nothing_found' => 'Ingen resultater funnet',
     'page_x_of_y' => 'Viser side _PAGE_ av _PAGES_', // Don't replace "_PAGE_" or "_PAGES_"
     'no_records' => 'Ingen rekorder er tilgjengelige',
