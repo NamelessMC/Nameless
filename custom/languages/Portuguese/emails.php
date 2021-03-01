43058e39fc98ad684eedcbe193f5e5ce5435c9af
Mansffer <57368089+Mansffer@users.noreply.github.com>
2021-03-01 05:54:40 -0300

    update emails.php

--- custom/languages/Portuguese/emails.php
+++ custom/languages/Portuguese/emails.php
@@ -7,6 +7,8 @@
  *  License: MIT
  *
  *  Portuguese Language - Emails
+*  Translation by ?? & Mansffer
+ *  Last Update: 01/03/2021
  */
 
 $language = array(
@@ -16,14 +18,14 @@ $language = array(
     'thanks' => 'Obrigado,',
 
     // Registration Email
-    'register_subject' => 'Validar Conta',
+    'register_subject' => 'Verificar Conta',
     'register_message' => 'Obrigado por se registrar! Para completar o seu cadastro, clique no link a seguir:',
 
     // Change Password
-    'change_password_subject' => 'Esqueceu a senha?',
-    'change_password_message' => 'Para resetar sua senha, clique no seguinte link. Se você desconhece essa requisição, você pode excluir com segurança este e-mail.',
+    'change_password_subject' => 'Esqueceu sua senha?',
+    'change_password_message' => 'Para resetar sua senha, clique no seguinte link. Se você desconhece essa requisição, você pode excluir com segurança este email.',
 
     // Forum Topic Replies
-    'forum_topic_reply_subject' => '{x} has replied to {y}', // Don't remove {x} or {y}
-    'forum_topic_reply_message' => '{x} has replied to a topic you follow. Content: {z}' // Don't remove {x} or {z}
+    'forum_topic_reply_subject' => '{x} respondeu a {y}', // Don't remove {x} or {y}
+    'forum_topic_reply_message' => '{x} respondeu a um tópico que você segue. Conteúdo: {z}' // Don't remove {x} or {z}
 );
