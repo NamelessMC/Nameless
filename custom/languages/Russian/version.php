d32eabb93358db32ec92938fef17ea8eaa5b3f7b
Tadhg <tadhgsmboyle@gmail.com>
2021-03-01 10:09:30 -0800

    fix #2038

--- custom/languages/Russian/version.php
+++ custom/languages/Russian/version.php
@@ -10,11 +10,17 @@
  *  Russian Language - Language version
  */
 
+/*
+ *  Russian translation by Oniel
+ *  https://github.com/0niel
+ */
 // Which version of NamelessMC is this language file updated to?
 $language_version = '2.0.0-pr9';
 $language_html = 'ru';
 
 // Plural function
-function pluralForm($n, $forms) {
-    return $n % 10 == 1 && $n % 100 != 11 ? $forms[0] : ($n % 10 >= 2 && $n % 10 <= 4 && ($n % 100 < 10 || $n % 100 >= 20) ? $forms[1] : $forms[2]);
+if (!function_exists('pluralForm')) {
+    function pluralForm($n, $forms) {
+        return $n % 10 == 1 && $n % 100 != 11 ? $forms[0] : ($n % 10 >= 2 && $n % 10 <= 4 && ($n % 100 < 10 || $n % 100 >= 20) ? $forms[1] : $forms[2]);
+    }
 }
