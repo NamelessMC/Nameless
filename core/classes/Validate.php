04ffa9fa4ae47a62b429d6d5f6d77feaf6211d25
Tadhg <tadhgsmboyle@gmail.com>
2021-03-15 23:11:01 -0700

    initial work (not working or tested yet)

--- core/classes/Validate.php
+++ core/classes/Validate.php
@@ -13,9 +13,59 @@
 
 class Validate {
 
-    private $_passed = false,
-        $_errors = array(),
-        $_db = null;
+    private $_messages = array();
+    private $_passed = false;
+    private $_errors = array();
+    private $_db = null;
+
+    /**
+     * Ensure this field is not empty
+     */
+    const REQUIRED = 'required';
+    /**
+     * Define minimum characters
+     */
+    const MIN = 'min';
+    /**
+     * Define max characters
+     */
+    const MAX = 'max';
+    /**
+     * Ensure provided value matches another
+     */
+    const MATCHES = 'matches';
+    /**
+     * Check the user has agreed to the terms and conditions
+     */
+    const AGREE = 'agree';
+    /**
+     * Check the value has not already been inputted in the database
+     */
+    const UNIQUE = 'unique';
+    /**
+     * Check if email is valid
+     */
+    const EMAIL = 'email';
+    /**
+     * Check that timezone is valid
+     */
+    const TIMEZONE = 'timezone';
+    /**
+     * Check that the specified user account is set as active (ie validated)
+     */
+    const IS_ACTIVE = 'isactive';
+    /**
+     * Check that the specified user account is not banned
+     */
+    const IS_BANNED = 'isbanned';
+    /**
+     * Check that the value is alphanumeric
+     */
+    const ALPHANUMERIC = 'alphanumeric';
+    /**
+     * Check that the value is numeric
+     */
+    const NUMERIC = 'numeric';
 
     // Construct Validate class
     // No parameters
@@ -49,50 +99,41 @@ class Validate {
                 $item = Output::getClean($item);
 
                 // Required rule
-                if ($rule === 'required' && empty($value)) {
+                if ($rule === Validate::REQUIRED && empty($value)) {
                     // The post array does not include this value, return an error
-                    $this->addError("{$item} is required");
+                    $this->addError($item, "{$item} is required");
+
                 } else if (!empty($value)) {
                     // The post array does include this value, continue validating
                     switch ($rule) {
-                            // Minimum of $rule_value characters
-                        case 'min';
+                        case Validate::MIN;
                             if (mb_strlen($value) < $rule_value) {
-                                // Not a minumum of $rule_value characters, return an error
-                                $this->addError("{$item} must be a minimum of {$rule_value} characters.");
+                                $this->addError($this->getError($item, "{$item} must be a minimum of {$rule_value} characters."));
                             }
                             break;
 
-                            // Maximum of $rule_value characters
-                        case 'max';
+                        case Validate::MAX;
                             if (mb_strlen($value) > $rule_value) {
-                                // Above the maximum of $rule_value characters, return an error
-                                $this->addError("{$item} must be a maximum of {$rule_value} characters.");
+                                $this->addError($this->getError($item, "{$item} must be a maximum of {$rule_value} characters."));
                             }
                             break;
 
-                            // Check value matches another value
-                        case 'matches';
+                        case Validate::MATCHES;
                             if ($value != $source[$rule_value]) {
-                                // Value does not match, return an error
-                                $this->addError("{$rule_value} must match {$item}.");
+                                $this->addError($this->getError($item, "{$rule_value} must match {$item}."));
                             }
                             break;
 
-                            // Check the user has agreed to the terms and conditions
-                        case 'agree';
+                        case Validate::AGREE;
                             if ($value != 1) {
-                                // The user has not agreed, return an error
-                                $this->addError("You must agree to our terms and conditions in order to register.");
+                                $this->addError($this->getError($item, "You must agree to our terms and conditions in order to register."));
                             }
                             break;
 
-                            // Check the value has not already been inputted in the database
-                        case 'unique';
+                        case Validate::UNIQUE;
                             $check = $this->_db->get($rule_value, array($item, '=', $value));
                             if ($check->count()) {
-                                // The value has already been inputted, return an error
-                                $this->addError("The username/email {$item} already exists!");
+                                $this->addError($this->getError($item, "The username/email {$item} already exists!"));
                             }
                             break;
 
@@ -108,59 +149,51 @@ class Validate {
                         break;
                         */
 
-                            // Check if email is valid
-                        case 'email';
+                        case Validate::EMAIL;
                             if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
-                                // Value is not a valid email
-                                $this->addError("{$value} is not a valid email.");
+                                $this->addError($this->getError($item, "{$value} is not a valid email."));
                             }
                             break;
 
-                        case 'timezone':
+                        case Validate::TIMEZONE:
                             if (!in_array($value, DateTimeZone::listIdentifiers(DateTimeZone::ALL))) {
-                                $this->addError("The timezone {$item} is invalid.");
+                                $this->addError($this->getError($item, "The timezone {$item} is invalid."));
                             }
                             break;
 
-                            // Check that the specified user account is set as active (ie validated)
-                        case 'isactive';
+                        case Validate::IS_ACTIVE;
                             $check = $this->_db->get('users', array($item, '=', $value));
                             if ($check->count()) {
                                 $isuseractive = $check->first()->active;
                                 if ($isuseractive == 0) {
-                                    // Not active, return an error
-                                    $this->addError("That username is inactive. Have you validated your account or requested a password reset?");
+                                    $this->addError($this->getError($item, "That username is inactive. Have you validated your account or requested a password reset?"));
                                 }
                             }
                             break;
 
-                            // Check that the specified user account is not banned
-                        case 'isbanned';
+                        case Validate::IS_BANNED;
                             $check = $this->_db->get('users', array($item, '=', $value));
                             if ($check->count()) {
                                 $isuserbanned = $check->first()->isbanned;
                                 if ($isuserbanned == 1) {
-                                    // The user is banned, return an error
-                                    $this->addError("The username {$item} is banned.");
+                                    $this->addError($this->getError($item, "The username {$item} is banned."));
                                 }
                             }
                             break;
 
-                        case 'isbannedip';
-                            // Todo: Check if IP is banned
-                            break;
+                        // case 'isbannedip';
+                        //     TODO: Check if IP is banned
+                        //     break;
 
-                        case 'alphanumeric':
+                        case Validate::ALPHANUMERIC:
                             if (!ctype_alnum($value)) {
-                                // $value is not alphanumeric
-                                $this->addError("{$item} must be alphanumeric.");
+                                $this->addError($this->getError($item, "{$item} must be alphanumeric."));
                             }
                             break;
 
-                        case 'numeric':
+                        case Validate::NUMERIC:
                             if (!is_numeric($value)) {
-                                // $value is not numeric
-                                $this->addError("{$item} must be numeric.");
+                                $this->addError($this->getError($item, "{$item} must be numeric."));
                             }
                             break;
                     }
@@ -176,20 +209,44 @@ class Validate {
         return $this;
     }
 
-    // Add an error to the error array
-    // No parameters
+    /**
+     * Add custom messages to this Validator
+     * @param array $messages array of input names and strings or arrays to use as messages
+     */
+    public function messages($messages) {
+        $this->_messages = $messages;
+        return $this;
+    }
+
+    /**
+     * Add an error to the error array
+     * @param string $error message to add to error array
+     */
     private function addError($error) {
         $this->_errors[] = $error;
     }
 
-    // Return the array of errors
-    // No parameters
+    /**
+     * Get message for provided field
+     * @param string $field name of field to search for 
+     * @param string $fallback fallback default message if custom message is not supplied
+     */
+    private function getError($field, $fallback) {
+
+    }
+
+    /**
+     * Get current errors, if any
+     * @return array Any and all errors for this Validator
+     */
     public function errors() {
         return $this->_errors;
     }
 
-    // Return whether the validation passed (true or false)
-    // No parameters
+    /**
+     * Get if this Validator passed
+     * @return bool whether this Validator passed or not
+     */
     public function passed() {
         return $this->_passed;
     }
