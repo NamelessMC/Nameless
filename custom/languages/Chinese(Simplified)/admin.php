c78ca87f05bd27d85989c730a5b8f65a64dec23c
Gabriel "Dreta" Leen <hello@dreta.dev>
2021-05-10 18:03:21 +0800

    Update versions

--- custom/languages/Chinese(Simplified)/admin.php
+++ custom/languages/Chinese(Simplified)/admin.php
@@ -2,13 +2,13 @@
 /*
  *  Made by Samerton
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-pr8
+ *  NamelessMC version 2.0.0-pr9
  *
  *  License: MIT
  *
  *  Chinese Simplified Language - Admin
  *  Translation(Chinese Simplified) by ahdg,lian20,LingDong,NEWLY_1129514,Dreta
- *  Translation progress(v2-pr8) : 100%
+ *  Translation progress(v2-pr9) : 100%
  */
 
 $language = array(
