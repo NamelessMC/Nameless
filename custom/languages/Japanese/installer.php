9e8b88d38092bc867aa8bd8802825090c21a809b
snake <admin@viasnake.com>
2021-05-10 15:08:41 +0900

    Update installer.php

--- custom/languages/Japanese/installer.php
+++ custom/languages/Japanese/installer.php
@@ -2,7 +2,7 @@
 /*
  *  Made by Samerton
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-pr8
+ *  NamelessMC version 2.0.0-pr9
  *
  *  Translator
  *  - SimplyRin( @SimplyRin_, https://www.simplyrin.net )
