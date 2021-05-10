1c7389345e15fbac61c507a2f6608d91f918104c
snake <admin@viasnake.com>
2021-05-10 15:08:45 +0900

    Update log.php

--- custom/languages/Japanese/log.php
+++ custom/languages/Japanese/log.php
@@ -1,6 +1,19 @@
 <?php
+/*
+ *  Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr9
+ *
+ *  Translator
+ *  - snake( @ViaSnake, https://github.com/ViaSnake )
+ *
+ *  License: MIT
+ *
+ *  Japanese Language - Log
+ */
+
 $language = array(
     //forums
-    'info_forums_lock' => 'Locked',
-    'info_forums_unlock' => 'Unlocked',
+    'info_forums_lock' => 'ロック',
+    'info_forums_unlock' => 'ロック解除',
 );
