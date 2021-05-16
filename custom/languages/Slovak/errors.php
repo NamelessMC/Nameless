755cea11b95eed7e947b8e84227910a0d3345dae
Róbert Kelčák <RobiNN1@users.noreply.github.com>
2021-05-17 00:06:48 +0200

    Update Slovak translation (#2277)

--- custom/languages/Slovak/errors.php
+++ custom/languages/Slovak/errors.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Made by JozefSorokac
+ *  Made by RobiNN
  *  https://github.com/NamelessMC/Nameless/
  *  NamelessMC version 2.0.0-pr8
  *
@@ -14,21 +14,21 @@ $language = array(
      *  Errors
      */
     '404_title' => 'Ospravedlňujeme sa!',
-    '404_content' => 'Ale túto stránku nemôžeme nájsť :(',
+    '404_content' => 'Túto stránku sme nenašli.',
     '404_back' => 'Vrátiť späť',
     '404_home' => 'Domov',
-    '404_error' => 'Pokiaľ sa domnievate, že je to chyba, {x}kontaktujte prosím administrátora{y}', // Don't replace {x} or {y}
+    '404_error' => 'Ak si myslíte, že ide o chybu, {x} kontaktujte správcu {y}', // Don't replace {x} or {y}
     '403_title' => 'Ospravedlňujeme sa!',
-    '403_content' => 'You do not have permission to view this page.',
-    '403_login' => 'Please log in to continue.',
+    '403_content' => 'Na prezeranie tejto stránky nemáte oprávnenia.',
+    '403_login' => 'Prosím prihláste sa pre pokračovanie.',
     '403_back' => 'Vrátiť späť',
     '403_home' => 'Domov',
-    'maintenance_title' => 'Údržba.',
+    'maintenance_title' => 'Režim údržby.',
     'maintenance_retry' => 'Opakovať',
-    'fatal_error' => 'Fatal Error',
+    'fatal_error' => 'Fatálna chyba',
     'fatal_error_title' => 'Ospravedlňujeme sa!',
-    'fatal_error_message_user' => 'Sorry, but something went wrong while loading the page. Please contact an administrator.',
-    'fatal_error_message_admin' => 'Sorry, but something went wrong while loading the page. Error details:',
-    'in_file' => 'in file <strong>{x}</strong>', // Don't replace {x}
-    'on_line' => 'on line <strong>{x}</strong>' // Don't replace {x}
+    'fatal_error_message_user' => 'Je nám ľúto, ale pri načítaní stránky sa niečo pokazilo. Prosím kontaktujte správcu.',
+    'fatal_error_message_admin' => 'Je nám ľúto, ale pri načítaní stránky sa niečo pokazilo. Detaily chyby:',
+    'in_file' => 'v súbore <strong>{x}</strong>', // Don't replace {x}
+    'on_line' => 'na riadku <strong>{x}</strong>' // Don't replace {x}
 );
--- custom/languages/Slovak/table.php
+++ custom/languages/Slovak/table.php
@@ -1,21 +1,21 @@
 <?php
 /*
- *  Made by Samerton
+ *  Made by RobiNN
  *  https://github.com/NamelessMC/Nameless/
  *  NamelessMC version 2.0.0-dev
  *
  *  License: MIT
  *
- *  EnglishUK Language - Table
+ *  Slovak Language - Table
  */
 
 $language = array(
     /*
      *  Tables
      */
-    'display_records_per_page' => 'Display _MENU_ records per page', // Don't replace "_MENU_"
-    'nothing_found' => 'No results found',
-    'page_x_of_y' => 'Showing page _PAGE_ of _PAGES_', // Don't replace "_PAGE_" or "_PAGES_"
-    'no_records' => 'No records available',
-    'filtered' => '(filtered from _MAX_ total records)' // Don't replace "_MAX_"
+    'display_records_per_page' => 'Zobraziť _MENU_ záznamov na stránku', // Don't replace "_MENU_"
+    'nothing_found' => 'Neboli nájdené žiadne výsledky',
+    'page_x_of_y' => 'Zobrazuje sa stránka _PAGE_ z _PAGES_', // Don't replace "_PAGE_" or "_PAGES_"
+    'no_records' => 'Nie sú k dispozícii žiadne záznamy',
+    'filtered' => '(filtrované z _MAX_ celkových záznamov)' // Don't replace "_MAX_"
 );
