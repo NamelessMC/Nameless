ec76f39c49260fafb5e82e51dfb16c376f83d95a
IkiaeM <64362443+IkiaeM@users.noreply.github.com>
2021-06-28 13:24:52 +0200

    French translation of api.php

---
+++ personnalisé /langues /français /api.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ *  Made by White.
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr10
+ *
+ *  License: MIT
+ *
+ *  French Language - Users
+ */
+
+$language = array(
+
+    // Errors
+    'api_disabled' => 'L\'API est désactivée',
+    'unknown_error' => 'Erreur inconnue',
+    'invalid_api_key' => 'Clé API non valide',
+    'invalid_language_file' => 'Fichier de langue non valide',
+    'invalid_api_method' => 'Méthode API non valide',
+    'no_unique_site_id' => 'Pas d\'ID de site unique disponible',
+    'unable_to_check_for_updates' => 'Impossible de vérifier les mises à jour',
+    'invalid_post_contents' => 'Contenu POST invalide',
+    'invalid_get_contents' => 'Contenu GET invalide',
+    'invalid_email_address' => 'Adresse électronique invalide',
+    'invalid_username' => 'Nom d\'utilisateur invalide',
+    'invalid_uuid' => 'UUID invalide',
+    'email_already_exists' => 'L\'e-mail existe déjà',
+    'username_already_exists' => 'Le nom d\'utilisateur existe déjà',
+    'uuid_already_exists' => 'L\'UUID existe déjà',
+    'unable_to_create_account' => 'Impossible de créer un compte',
+    'unable_to_send_registration_email' => 'Impossible d\'envoyer l\'email d\'inscription, veuillez contacter un administrateur pour activer votre compte.',
+    'unable_to_find_user' => 'Impossible de trouver l\'utilisateur',
+    'unable_to_find_group' => 'Impossible de trouver le groupe',
+    'unable_to_update_group' => 'Impossible de mettre à jour le groupe de l\'utilisateur',
+    'report_content_too_long' => 'Le contenu du rapport ne doit pas dépasser 255 caractères',
+    'you_have_been_banned_from_website' => 'Vous avez été banni du site',
+    'you_have_open_report_already' => 'Vous avez déjà un rapport ouvert concernant ce joueur',
+    'unable_to_create_report' => 'Impossible de créer un rapport',
+    'unable_to_update_username' => 'Impossible de mettre à jour le nom d\'utilisateur',
+    'unable_to_update_server_info' => 'Impossible de mettre à jour les informations sur le serveur',
+    'invalid_server_id' => 'ID de serveur invalide',
+    'invalid_code' => 'Code invalide fourni',
+    'provide_one_discord_settings' => 'Veuillez fournir au moins un des éléments suivants : "url", "guild_id".',
+    'no_pending_verification_for_token' => 'Il n\'y a pas de vérifications en cours sous le jeton fourni.',
+    'unable_to_update_discord_username' => 'Impossible de mettre à jour le nom d\'utilisateur Discord.',
+    'unable_to_update_discord_roles' => 'Impossible de mettre à jour la liste des rôles Discord.',
+    'unable_to_update_discord_bot_username' => 'Impossible de mettre à jour le nom d\'utilisateur du robot Discord.',
+
+    // Success messages
+    'finish_registration_link' => 'Veuillez cliquer sur le lien suivant pour compléter l\'inscription :',
+    'finish_registration_email' => 'Veuillez vérifier vos emails pour compléter l\'inscription.',
+    'group_updated' => 'Groupe mis à jour avec succès',
+    'report_created' => 'Rapport créé avec succès',
+    'new_private_message_from' => 'Nouveau message privé de {x}', // Don't replace {x}
+    'username_updated' => 'Le nom d\'utilisateur a été mis à jour avec succès',
+    'server_info_updated' => 'Les informations sur le serveur ont été mises à jour avec succès',
+    'discord_settings_updated' => 'Les paramètres de Discord ont été mis à jour avec succès',
+    'discord_usernames_updated' => 'Les noms d\'utilisateur Discord ont été mis à jour avec succès'
+);
