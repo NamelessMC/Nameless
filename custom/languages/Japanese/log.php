f67db58d42525c8bd12696b11445827991247438
snake <admin@viasnake.com>
2021-06-30 12:49:37 +0900

    Update log.php

--- custom/languages/Japanese/log.php
+++ custom/languages/Japanese/log.php
@@ -2,7 +2,7 @@
 /*
  *  Made by Samerton
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-pr9
+ *  NamelessMC version 2.0.0-pr10
  *
  *  Translator
  *  - snake( @ViaSnake, https://github.com/ViaSnake )
