8e2b810004c97adae8dd3b9b41f2868c682c9c39
snake <admin@viasnake.com>
2021-06-30 12:19:43 +0900

    Update api.php

--- custom/languages/Japanese/api.php
+++ custom/languages/Japanese/api.php
@@ -2,7 +2,7 @@
 /*
  *  Made by Samerton
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-pr9
+ *  NamelessMC version 2.0.0-pr10
  *
  *  Translator
  *  - SimplyRin( @SimplyRin_, https://www.simplyrin.net )
@@ -19,16 +19,16 @@ $language = array(
     // Errors
     'api_disabled' => 'APIが無効',
     'unknown_error' => '不明なエラー',
-    'invalid_api_key' => '無効な API キー',
+    'invalid_api_key' => '無効なAPIキー',
     'invalid_language_file' => '言語ファイルが無効です。',
-    'invalid_api_method' => 'API メソッドが無効です。',
-    'no_unique_site_id' => 'サイト ID はありません。',
+    'invalid_api_method' => 'APIメソッドが無効です。',
+    'no_unique_site_id' => 'サイトIDはありません。',
     'unable_to_check_for_updates' => '更新を確認できません。',
-    'invalid_post_contents' => '無効な POST コンテンツ',
-    'invalid_get_contents' => '無効な GET コンテンツ',
-    'invalid_email_address' => '無効な メールアドレス',
-    'invalid_username' => '無効な ユーザー名',
-    'invalid_uuid' => '無効な UUID',
+    'invalid_post_contents' => '無効なPOSTコンテンツ',
+    'invalid_get_contents' => '無効なGETコンテンツ',
+    'invalid_email_address' => '無効なメールアドレス',
+    'invalid_username' => '無効なユーザー名',
+    'invalid_uuid' => '無効なUUID',
     'email_already_exists' => '指定されたメールアドレスは既に登録されています。',
     'username_already_exists' => '指定されたユーザー名は既に登録されています。',
     'uuid_already_exists' => '指定されたUUIDは既に登録されています。',
@@ -37,7 +37,7 @@ $language = array(
     'unable_to_find_user' => '指定されたユーザを見つけることができませんでした。',
     'unable_to_find_group' => '指定されたグループを見つけることができませんでした。',
     'unable_to_update_group' => 'ユーザーのグループを更新できませんでした。',
-    'report_content_too_long' => 'レポートの内容は <strong>255文字</strong>　以内にする必要があります。',
+    'report_content_too_long' => 'レポートの内容は<strong>255文字</strong>以内にする必要があります。',
     'cannot_report_yourself' => '自分自身を報告することはできません。',
     'you_have_been_banned_from_website' => 'あなたはウェブサイトにてアカウントがロックされています。',
     'you_have_open_report_already' => '既に指定されたプレイヤーのレポートが存在します。',
@@ -61,7 +61,7 @@ $language = array(
     'finish_registration_email' => '登録を完了するにはメールを確認して処理を完了してください。',
     'group_updated' => 'グループを正常に更新しました。',
     'report_created' => 'レポートが正常に送信されました。',
-    'new_private_message_from' => '{x} から新しいプライベートメッセージが届いています。', // Don't replace {x}
+    'new_private_message_from' => '{x}から新しいプライベートメッセージが届いています。', // Don't replace {x}
     'username_updated' => 'ユーザー名が更新されました。',
     'server_info_updated' => 'サーバー情報が更新されました。',
     'account_validated' => 'アカウントの検証が完了しました。',
