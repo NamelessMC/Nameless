beb137ac6410697dce0846f559c6586b4fb26e41
ItsLynix <71310187+ItsLynix@users.noreply.github.com>
2021-07-09 02:42:43 +0200

    Update version.php

--- custom/languages/SwedishSE/version.php
+++ custom/languages/SwedishSE/version.php
@@ -6,10 +6,10 @@
  *
  *  License: MIT
  *
- *  Translated by IsS127
+ *  Translated by IsS127, ItsLynix
  *  SwedishSE Language - Errors
  */
 
 // Which version of NamelessMC is this language file updated to?
-$language_version = '2.0.0-pr3';
+$language_version = '2.0.0-pr8';
 $language_html = 'sv';
