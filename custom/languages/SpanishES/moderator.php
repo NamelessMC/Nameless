07d86d01b686ac0e6a9e7a209fc0e7e0d4681c70
Marcos <45741056+Marcos03BR@users.noreply.github.com>
2021-07-12 17:53:42 +0200

    Update moderator.php
    Full translation from "EnglishUK". Made from scratch

--- custom/languages/SpanishES/moderator.php
+++ custom/languages/SpanishES/moderator.php
@@ -1,108 +1,108 @@
-<?php
+<?php 
 /*
- *  Made by TheSuperSkills (Edited by Ariuw and iMaykolRD_)
+ *  Translated by ManiaNetwork
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-pr8
+ *  NamelessMC version 2.0.0-pr10
  *
  *  License: MIT
  *
- *  Spanish Language - Moderator terms
+ *  Spanish/Spain Language - Moderator
  */
 
 $language = array(
-    'mod_cp' => 'Panel de Moderación',
-    'staff_cp' => 'Panel Administrativo',
-    'overview' => 'Visión General',
-
+    'staff_cp' => 'Panel de administración',
+    'overview' => 'Resumen',
+    
     // Spam
     'spam' => 'Spam',
-    'mark_as_spam' => 'Marcar como correo no deseado',
-    'confirm_spam' => '<p>¿Estás seguro de que quieres marcar a este usuario como spam?</p><p>Se prohibirá la IP del usuario y todo su contenido se eliminará.</p>',
-    'user_marked_as_spam' => 'Usuario marcado como spam correctamente.',
-    'cant_ban_admin' => '¡No se puede prohibir a un administrador!',
-
+    'mark_as_spam' => 'Marcar como spam',
+    'confirm_spam' => '<p>¿Estás seguro de que quieres marcar a este usuario como spam?</p> <p>El usuario será baneado por IP, y todo su contenido será eliminado.</p>',
+    'user_marked_as_spam' => 'Usuario marcado como spam con éxito.',
+    'cant_ban_admin' => '¡No se puede expulsar a un administrador!',
+    
     // IP Lookup
-    'ip_lookup' => 'Buscar IP',
-    'search_for_ip' => 'Buscar un nombre de usuario o una dirección IP:',
-    'no_ips_with_username' => 'No hay direcciones IP para ese usuario.',
-    'no_accounts_with_that_ip' => 'No existen cuentas con esa dirección IP.',
-    '1_account_with_ip' => 'Found 1 account with the IP {y}', // Don't replace "{y}"
-    'count_accounts_with_ip' => 'Encontró {x} cuentas con la IP {y}', // Don't replace "{x}" or "{y}"
-    '1_ip_with_name' => 'Se encontró una dirección IP para el usuario {y}', // Don't replace "{y}"
-    'count_ips_with_name' => 'Encontró {x} Dirección(es) IP para el usuario {y}', // Don't replace "{x}" or "{y}"
+    'ip_lookup' => 'Búsqueda de IP',
+    'search_for_ip' => 'Buscar un nombre de usuario o una dirección IP',
+    'no_ips_with_username' => 'No se han encontrado direcciones IP para ese usuario.',
+    'no_accounts_with_that_ip' => 'No se han encontrado cuentas para esa dirección IP.',
+    '1_account_with_ip' => 'Se ha encontrado 1 cuenta con la IP {y}', // Don't replace "{y}"
+    'count_accounts_with_ip' => 'Encontradas {x} cuentas con la IP {y}', // Don't replace "{x}" or "{y}"
+    '1_ip_with_name' => 'Se ha encontrado 1 dirección IP para el usuario {y}', // Don't replace "{y}"
+    'count_ips_with_name' => 'Se han encontrado {x} direcciones IP para el usuario {y}', // Don't replace "{x}" or "{y}"
     'no_users_or_ips_found' => 'No se han encontrado usuarios ni direcciones IP.',
-
+    
     // Reports
-    'reports' => 'Reportes',
+    'reports' => 'Informes',
     'report_alert' => 'Nuevo informe presentado',
-    'user_reported' => 'Usuario notificado',
+    'user_reported' => 'Usuario denunciado',
     'comments' => 'Comentarios',
     'updated_by' => 'Actualizado por',
-    'actions' => 'Comportamiento',
-    'view_closed' => 'Ver cerrado',
-    'view_open' => 'Ver abierto',
-    'viewing_report' => 'Visualización de reportes',
-    'view_content' => 'Ver contenido reportado',
-    'no_comments' => 'Sin comentarios',
-    'new_comment' => 'Nuevos comentarios',
-    'report_comment_invalid' => 'Contenido de comentario no válido. Asegúrate de haber ingresado un comentario entre 1 y 10000 caracteres.',
-    'close_report' => 'Cerrar reporte',
-    'reopen_report' => 'Reabrir reporte',
+    'actions' => 'Acciones',
+    'view_closed' => 'Ver informes cerrados',
+    'view_open' => 'Ver informes abiertos',
+    'viewing_report' => 'Ver informe',
+    'view_content' => 'Ver el contenido denunciado',
+    'no_comments' => 'No hay comentarios',
+    'new_comment' => 'Nuevo comentario',
+    'report_comment_invalid' => 'El contenido del comentario no es válido. Por favor, asegúrate de que has introducido un comentario de entre 1 y 10000 caracteres.',
+    'close_report' => 'Cerrar el informe',
+    'reopen_report' => 'Reabrir el informe',
     '1_open_report' => 'Actualmente hay <strong>1</strong> informe abierto.',
-    'open_reports' => 'Hay actualmente <strong>{x}</strong> reportes abiertos.', // Don't replace {x}
-    'no_open_reports' => 'No hay ningún reporte abierto.',
-    'no_closed_reports' => 'No hay ningún reporte cerrado.',
-    'recent_reports' => 'Reportes Recientes',
-    'reported_by' => 'Reportado por:',
-    'website' => 'Sitio Web',
-    'ingame' => 'En Juego',
-    'x_closed_report' => '{x} cerró este reporte.', // Don't replace {x} (username)
-    'x_reopened_report' => '{x} reabrió este reporte.', // Don't replace {x} (username)
-    'report_reopened' => 'Reporte reabrierto correctamente.',
-    'report_closed' => 'Reporte cerrado correctamente.',
-    'comment_created' => 'Comentario publicado.',
-
+    'open_reports' => 'Actualmente hay <strong>{x}</strong> informes abiertos.', // Don't replace {x}
+    'no_open_reports' => 'Actualmente no hay informes abiertos.',
+    'no_closed_reports' => 'Actualmente no hay informes cerrados.',
+    'recent_reports' => 'Informes recientes',
+    'reported_by' => 'Denunciado por:',
+    'website' => 'Sitio web',
+    'ingame' => 'Dentro del juego',
+    'x_closed_report' => '{x} cerró este informe.', // Don't replace {x} (username)
+    'x_reopened_report' => '{x} reabrió este informe.', // Don't replace {x} (username)
+    'report_reopened' => 'Informe reabierto con éxito.',
+    'report_closed' => 'Informe cerrado con éxito.',
+    'comment_created' => 'Comentario creado con éxito.',
+    
     // Punishments
-    'punishments' => 'Castigos',
-    'view_punishments' => 'Mirar sanciones',
-    'banned' => 'Baneados',
+    'punishments' => 'Sanciones',
+    'view_punishments' => 'Ver las sanciones',
+    'banned' => 'Expulsado',
     'groups' => 'Grupos',
-    'punish' => 'Castigar',
-    'ban' => 'Banear',
+    'punish' => 'Sancionar',
+    'ban' => 'Expulsar',
     'warn' => 'Advertir',
-    'ban_ip' => 'Banear de IP',
-    'viewing_user_x' => 'Observando al usuario {x}', // Don't replace {x}
+    'ban_ip' => 'Prohibir la IP',
+    'viewing_user_x' => 'Viendo el usuario {x}', // Don't replace {x}
     'previous_punishments' => 'Sanciones anteriores',
-    'no_previous_punishments' => 'No hay sanciones anteriores.',
+    'no_previous_punishments' => 'No hay sanciones previas',
+    'reset_avatar' => 'Restablecer el avatar',
     'warning' => 'Advertencia',
-    'ip_ban' => 'Baneo de IP',
+    'ip_ban' => 'Prohibición de la IP',
     'reason' => 'Razón',
-    'warn_user' => 'Advertir usuario',
-    'ban_user' => 'Banear usuario',
-    'enter_valid_punishment_reason' => 'Por favor ingrese una razón válida, entre 5 y 5000 carácteres.',
-    'user_punished' => 'Usuario sancionado..',
+    'warn_user' => 'Advertir al usuario',
+    'ban_user' => 'Expulsar al usuario',
+    'enter_valid_punishment_reason' => 'Por favor, introduzca una razón válida para su sanción, entre 5 y 5000 caracteres.',
+    'user_punished' => 'Usuario sancionado.',
     'user_punished_alert' => '{x} ha sancionado al usuario {y}', // Don't replace {x} (staff member) or {y} (user punished)
     'revoke' => 'Revocar',
     'revoked' => 'Revocado',
-    'acknowledged' => 'Reconocer',
-    'confirm_revoke_warning' => '¿Estás seguro de que quieres revocar esa advertencia?',
-    'confirm_revoke_ban' => '¿Estás seguro de que quieres revocar ese baneo? ¡El usuario será desbaneado, incluso si tiene un castigo reciente!',
-    'punishment_revoked' => 'Castigo revocado..',
-    'punishment_revoked_alert' => '{x} ha revocado el castigo de {y}', // Don't replace {x} (staff member) or {y} (user with revoked punishment)
-    'cant_punish_admin' => 'No puedes castigar a un administrador.',
-    'viewing_all_punishments' => 'Viendo todos los castigos',
-    'no_punishments_found' => 'No se han encontrado castigos.',
+    'acknowledged' => 'Condenado',
+    'confirm_revoke_warning' => '¿Está seguro de que quiere revocar esta advertencia?',
+    'confirm_revoke_ban' => '¿Estás seguro de que quieres revocar esta expulsión? Se levantará la expulsión del usuario, ¡incluso si tiene otras expulsiones más recientes!',
+    'punishment_revoked' => 'Sanción revocada.',
+    'punishment_revoked_alert' => '{x} ha revocado una sanción al usuario {y}', // Don't replace {x} (staff member) or {y} (user with revoked punishment)
+    'cant_punish_admin' => '¡No se puede sancionar a un administrador!',
+    'viewing_all_punishments' => 'Ver todas las sanciones',
+    'no_punishments_found' => 'No se han encontrado sanciones.',
     'view_user' => 'Ver usuario',
     'when' => 'Cuando',
-    'staff' => 'Staff',
+    'staff' => 'Personal administrativo',
     'type' => 'Tipo',
-    'recent_punishments' => 'Sanciones Recientes',
-    'created' => 'Creada:',
-    'staff:' => 'Staff:',
+    'recent_punishments' => 'Sanciones recientes',
+    'created' => 'Creado:',
+    'staff:' => 'Personal administrativo:',
     'reason:' => 'Razón:',
 
     // Users
-    'recent_registrations' => 'Registros Recientes',
-    'reset_profile_banner' => 'Reiniciar Banner del Perfil'
+    'recent_registrations' => 'Registros recientes',
+    'reset_profile_banner' => 'Restablecer el banner del perfil'
 
 );
