deae25687ab06bf9d52262cdf7e17028af951b61
partydragen <adrian_kili@outlook.com>
2021-08-25 09:17:24 +0200

    Update upgrade_perform.php

--- core/installation/views/includes/upgrade_perform.php
+++ core/installation/views/includes/upgrade_perform.php
@@ -848,12 +848,12 @@ switch ($s) {
         if (count($version)) {
             $queries->update('settings', $version[0]->id, array(
                 'name' => 'nameless_version',
-                'value' => '2.0.0-pr10'
+                'value' => '2.0.0-pr11'
             ));
         } else {
             $queries->create('settings', array(
                 'name' => 'nameless_version',
-                'value' => '2.0.0-pr10'
+                'value' => '2.0.0-pr11'
             ));
         }
 
