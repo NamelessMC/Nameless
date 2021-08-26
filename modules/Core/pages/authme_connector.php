bb35f26431548b5ae9e8a7f99256c5a05b680308
Partydragen <admin@partydragen.com>
2021-08-26 18:59:54 +0200

    Fix another array error alert

--- modules/Core/pages/authme_connector.php
+++ modules/Core/pages/authme_connector.php
@@ -2,7 +2,7 @@
 /*
  *	Made by Samerton
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-pr10
+ *  NamelessMC version 2.0.0-pr12
  *
  *  License: MIT
  *
@@ -330,7 +330,7 @@ if(Input::exists()){
 
             } else {
                 // Invalid recaotcha
-                $errors[] = array($language->get('user', 'invalid_recaptcha'));
+                $errors[] = $language->get('user', 'invalid_recaptcha');
             }
         }
     } else {
