defb43d7ee43e26c12ac03efe3a80d457a3cb71a
PadowYT2 <me@padow.ru>
2021-10-11 13:24:04 +0300

    Update version.php

--- custom/languages/EnglishUK/version.php
+++ custom/languages/EnglishUK/version.php
@@ -2,7 +2,7 @@
 /*
  *  Made by Samerton
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-pr8
+ *  NamelessMC version 2.0.0-pr12
  *
  *  License: MIT
  *
@@ -10,5 +10,5 @@
  */
 
 // Which version of NamelessMC is this language file updated to?
-$language_version = '2.0.0-pr10';
+$language_version = '2.0.0-pr12';
 $language_html = 'en';
