fa2f839635ab951a27ae79dd955b7919d9e16219
PadowYT2 <me@padow.ru>
2021-10-11 16:00:02 +0300

    Update version.php

--- custom/languages/Russian/version.php
+++ custom/languages/Russian/version.php
@@ -2,14 +2,15 @@
 <?php 
 /*
  *  Made by Samerton
+ *  Translated by Oniel, Я научу тебя шить XIMI
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-pr8
+ *  NamelessMC version 2.0.0-pr12
  *
  *  License: MIT
  *
- *  EnglishUK Language - Language version
+ *  Russian Language - Language version
  */
 
 // Which version of NamelessMC is this language file updated to?
 $language_version = '2.0.0-pr12';
-$language_html = 'en';
+$language_html = 'ru';
