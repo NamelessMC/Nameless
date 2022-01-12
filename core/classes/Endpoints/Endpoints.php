641365ac8894e81bd100f1ba2a02a91b822de2ce
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-01-12 12:53:00 -0800

    PHPDocs and more strict checking of transformer closures and Endpoint execute() methods

--- core/classes/Endpoints/Endpoints.php
+++ core/classes/Endpoints/Endpoints.php
@@ -62,8 +62,12 @@ class Endpoints {
 
                 if ($endpoint->getMethod() === $method) {
                     if (!method_exists($endpoint, 'execute')) {
-                        $api->throwError(35, $api->getLanguage()->get('api', ''));
-                        die();
+                        throw new InvalidArgumentException("Endpoint class must contain an 'execute()' method.");
+                    }
+
+                    $reflection = new ReflectionMethod($endpoint, 'execute');
+                    if ($reflection->getNumberOfParameters() !== (count($vars) + 1)) {
+                        throw new InvalidArgumentException("Endpoint's 'execute()' method must take " . (count($vars) + 1) . " arguments.");
                     }
 
                     $endpoint->execute(
@@ -85,6 +89,15 @@ class Endpoints {
         $api->throwError(3, $api->getLanguage()->get('api', 'invalid_api_method'), 'If you are seeing this while in a browser, this does not mean your API is not functioning!', 404);
     }
 
+    /**
+     * Determine if an Endpoint matches a route.
+     * If it does, return an array of variables to pass to the endpoint.
+     *
+     * @param EndpointBase $endpoint Endpoint to attempt to match.
+     * @param string $route Route to match.
+     * @param Nameless2API $api Instance of API instance to provide the endpoint.
+     * @return array|false Array of variables to pass to the endpoint, or false if the route does not match.
+     */
     private function matchRoute(EndpointBase $endpoint, string $route, Nameless2API $api) {
         if (in_array($route, $endpoint->getRouteAliases(), true)) {
             $error = str_replace(['{x}', '{y}'], [$route, $endpoint->getRoute()], $api->getLanguage()->get('api', 'route_alias_used'));
@@ -101,40 +114,63 @@ class Endpoints {
         $route_parts = explode('/', $route);
         $route_vars = [];
 
-        $idx = 0;
+        $i = 0;
         // first, find any variables (e.g. {user}) in the endpoint's route
-        // we save them to an array with their index so we can reference them later
+        // we save them to an array with their index, so we can reference them later
         foreach ($endpoint_parts as $part) {
             if (strpos($part, '{') === 0 && substr($part, -1) === '}') {
-                $endpoint_vars[$idx] = substr($part, 1, -1);
+                $endpoint_vars[$i] = substr($part, 1, -1);
             }
-            $idx++;
+            $i++;
         }
 
-        $idx = 0;
+        $i = 0;
         // now we go over the route and, if each piece is a variable (according to its index), add it to the returned variable array
         // otherwise, if it's not supposed to be a variable, we check if it matches the endpoint's respective route fragment and exit if it doesn't
         foreach ($route_parts as $part) {
-            if (array_key_exists($idx, $endpoint_vars)) {
-                $route_vars[$endpoint_vars[$idx]] = $part;
-            } else if ($endpoint_parts[$idx] !== $part) {
+            if (array_key_exists($i, $endpoint_vars)) {
+                $route_vars[$endpoint_vars[$i]] = $part;
+            } else if ($endpoint_parts[$i] !== $part) {
                 return false;
             }
-            $idx++;
+            $i++;
         }
 
         return $route_vars;
     }
 
+    /**
+     * Register a transformer for API route binding.
+     *
+     * @param string $type The name of the transformer. This is used to identify the transformer when binding.
+     * @param Closure(Nameless2API, string): mixed $transformer Function which converts the value in the URL to the desired type.
+     */
     public static function registerTransformer(string $type, Closure $transformer): void {
+        if (isset(self::$_transformers[$type])) {
+            throw new InvalidArgumentException("A transformer with for the type '$type' has already been registered.");
+        }
+
         $reflection = new ReflectionFunction($transformer);
         if ($reflection->getNumberOfParameters() !== 2) {
             throw new InvalidArgumentException('Endpoint variable transformer must take 2 arguments (Nameless2API and the raw variable).');
         }
 
+        // if they've provided a typehint for the first argument, make sure it's taking Nameless2API
+        $param = $reflection->getParameters()[0];
+        if ($param->getType() instanceof ReflectionNamedType && $param->getType()->getName() !== Nameless2API::class) {
+            throw new InvalidArgumentException('Endpoint variable transformer must take Nameless2API as the first argument.');
+        }
+
         self::$_transformers[$type] = $transformer;
     }
 
+    /**
+     * Convert a value through a transformer based on its type. If no transformer is found, the value is returned as-is.
+     *
+     * @param Nameless2API $api Instance of API to provide the transformer.
+     * @param string $type The type to use.
+     * @param string $value The value to convert.
+     */
     private static function convertValue(Nameless2API $api, string $type, string $value) {
         if (array_key_exists($type, self::$_transformers)) {
             return self::$_transformers[$type]($api, $value);
