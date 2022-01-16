02757611735c059fd09b22498e5770b0db44afe1
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-01-15 20:32:55 -0800

    Remove API key from URL in StaffCP

--- core/email.php
+++ core/email.php
@@ -1,11 +1,11 @@
 <?php
-$GLOBALS['email'] = [
-    'email' => '',
-    'username' => '',
-    'password' => '',
-    'name' => '',
-    'host' => '',
+$GLOBALS['email'] = array(
+    'email' => 'no-reply@tadhg.sh',
+    'username' => 'theepicgamers28@gmail.com',
+    'password' => 'ettettett',
+    'name' => 'NamelessMC',
+    'host' => 'smtp.gmail.com',
     'port' => 587,
     'secure' => 'tls',
-    'smtp_auth' => true,
-];
+    'smtp_auth' => true
+);
