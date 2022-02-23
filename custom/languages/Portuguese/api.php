9a796f59decc4a577e416abf3872e981957ad35e
DeusDrizzyy <66096216+DeusDrizzyy@users.noreply.github.com>
2022-02-22 23:46:24 -0300

    Update Portuguese Language (#2609)

--- custom/languages/Portuguese/api.php
+++ custom/languages/Portuguese/api.php
@@ -2,55 +2,55 @@
 /*
  *  Made by Samerton
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-pr8
+ *  NamelessMC version 2.0.0-pr12
  *
  *  License: MIT
  *
  *  Portuguese Language - API
- *  Translation by Douglas Teles & Mansffer
- *  Last Update: 01/03/2021
+ *  Translation by Douglas Teles & Mansffer.
+ *  Last Update: 22/02/2022
  */
 
 $language = [
 
     // Errors
-    'api_disabled' => 'A API está desativada',
+    'api_disabled' => 'A API está desativada.',
     'unknown_error' => 'Erro desconhecido',
-    'invalid_api_key' => 'A chave de API fornecida é inválida',
-    'invalid_language_file' => 'O arquivo de idioma fornecido é inválido',
-    'invalid_api_method' => 'Método de API inválido',
-    'no_unique_site_id' => 'Nenhum ID de site exclusivo disponível',
-    'unable_to_check_for_updates' => 'Não foi possível verificar atualizações',
-    'invalid_post_contents' => 'Conteúdo de POST inválido',
-    'invalid_get_contents' => 'Conteúdo de GET inválido',
-    'invalid_email_address' => 'Endereço de email inválido',
-    'invalid_username' => 'Nome de usuário inválido',
-    'invalid_uuid' => 'UUID inválido',
-    'email_already_exists' => 'O email já existe',
-    'username_already_exists' => 'O usuário já existe',
-    'uuid_already_exists' => 'O UUID já existe',
-    'unable_to_create_account' => 'Não foi possível criar a conta',
-    'unable_to_send_registration_email' => 'Não foi possível enviar um email de registro, entre em contato com um administrador para ativar sua conta',
-    'unable_to_find_user' => 'Não foi possível localizar o usuário',
-    'unable_to_find_group' => 'Não foi possível localizar o grupo',
-    'unable_to_update_group' => 'Não foi possível atualizar o grupo do usuário',
-    'report_content_too_long' => 'O conteúdo da denúncia deve ter 255 caracteres ou menos',
-    'cannot_report_yourself' => 'Você não pode denúnciar a si mesmo.',
-    'you_have_been_banned_from_website' => 'Você foi banido do site',
-    'you_have_open_report_already' => 'Você já tem uma denúncia aberta sobre este jogador',
-    'unable_to_create_report' => 'Não foi possível criar uma denúncia',
-    'unable_to_update_username' => 'Não foi possível atualizar o nome de usuário',
-    'unable_to_update_server_info' => 'Não foi possível atualizar informações do servidor',
-    'invalid_server_id' => 'O ID do servidor é inválido',
-    'invalid_code' => 'O código fornecido é inválido',
+    'invalid_api_key' => 'A chave de API fornecida é inválida.',
+    'invalid_language_file' => 'O arquivo de idioma fornecido é inválido.',
+    'invalid_api_method' => 'Método de API inválido.',
+    'no_unique_site_id' => 'Nenhum ID de site exclusivo disponível.',
+    'unable_to_check_for_updates' => 'Não foi possível verificar atualizações.',
+    'invalid_post_contents' => 'O conteúdo do POST é inválido.',
+    'invalid_get_contents' => 'O conteúdo do GET é inválido.',
+    'invalid_email_address' => 'Endereço de email inválido.',
+    'invalid_username' => 'Nome de usuário inválido.',
+    'invalid_uuid' => 'UUID inválido.',
+    'email_already_exists' => 'O email já existe.',
+    'username_already_exists' => 'O usuário já existe.',
+    'uuid_already_exists' => 'O UUID já existe.',
+    'unable_to_create_account' => 'Não foi possível criar a conta.',
+    'unable_to_send_registration_email' => 'Não foi possível enviar um email de registro, entre em contato com um administrador para ativar a sua conta.',
+    'unable_to_find_user' => 'Não foi possível localizar o usuário.',
+    'unable_to_find_group' => 'Não foi possível localizar o grupo.',
+    'unable_to_update_group' => 'Não foi possível atualizar o grupo do usuário.',
+    'report_content_too_long' => 'O conteúdo da denúncia deve ter 255 caracteres ou menos.',
+    'cannot_report_yourself' => 'Você não pode se denunciar.',
+    'you_have_been_banned_from_website' => 'Você foi banido do site.',
+    'you_have_open_report_already' => 'Você já tem uma denúncia aberta sobre este jogador.',
+    'unable_to_create_report' => 'Não foi possível criar uma denúncia.',
+    'unable_to_update_username' => 'Não foi possível atualizar o nome de usuário.',
+    'unable_to_update_server_info' => 'Não foi possível atualizar informações do servidor.',
+    'invalid_server_id' => 'O ID do servidor é inválido.',
+    'invalid_code' => 'O código fornecido é inválido.',
     'user_already_active' => 'A sua conta já foi ativada.',
 
     // Success messages
     'finish_registration_link' => 'Por favor, clique no seguinte link para concluir o registro:',
     'finish_registration_email' => 'Por favor, verifique seus emails para completar o registro.',
-    'group_updated' => 'Grupo atualizado com êxito',
-    'report_created' => 'Relatório criado com êxito',
-    'username_updated' => 'Nome de usuário atualizado com sucesso',
-    'server_info_updated' => 'Informações do servidor atualizadas com sucesso',
-    'account_validated' => 'A conta foi validada com sucesso',
+    'group_updated' => 'Grupo atualizado com êxito.',
+    'report_created' => 'Relatório criado com êxito.',
+    'username_updated' => 'Nome de usuário atualizado com sucesso.',
+    'server_info_updated' => 'Informações do servidor atualizadas com sucesso.',
+    'account_validated' => 'A conta foi validada com sucesso.',
 ];
