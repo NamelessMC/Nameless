9724ea0622a481108f7098887a1498f5ffb0abb7
Partydragen <admin@partydragen.com>
2022-02-27 16:50:16 +0100

    Fixes verify banner issue https://github.com/NamelessMC/Nameless/issues/2052
    Fixes https://github.com/NamelessMC/Nameless/issues/2052

--- core/templates/frontend_init.php
+++ core/templates/frontend_init.php
@@ -84,9 +84,12 @@ if ($user->isLoggedIn()) {
     }
 
     $api_verification = $configuration->get('Core', 'api_verification');
-    if ($api_verification == 1 && in_array($default_group, $user->getAllGroupIds()) && ($user->data()->reset_code)) {
-        // User needs to validate account
-        $smarty->assign('MUST_VALIDATE_ACCOUNT', str_replace('{x}', Output::getClean($user->data()->reset_code), $language->get('user', 'validate_account_command')));
+    if ($api_verification == 1) {
+        $integrationUser = $user->getIntegration('Minecraft');
+        if ($integrationUser != null && !$integrationUser->isVerified()) {
+            // User needs to validate account
+            $smarty->assign('MUST_VALIDATE_ACCOUNT', str_replace('{x}', Output::getClean($integrationUser->data()->code), $language->get('user', 'validate_account_command')));
+        }
     }
 }
 
