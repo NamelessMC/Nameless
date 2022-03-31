8cf3e386f50706a00ed72eafacddc514ff4c6579
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-03-31 11:14:01 -0700

    Add message when no OAuth providers are configured

--- custom/languages/EnglishUK/user.php
+++ custom/languages/EnglishUK/user.php
@@ -245,4 +245,5 @@ $language = [
     'oauth_login_success' => 'You have logged in with your {x} account.', // Don't replace {x}
     'oauth_link_confirm' => 'You will be taken to the {x} website to link your account.', // Don't replace {x}
     'oauth_unlink_confirm' => 'Are you sure you want to unlink your account from {x}.<br>You will not be able to login with this account anymore.', // Don't replace {x}
+    'no_providers' => 'No OAuth providers are setup.'
 ];
