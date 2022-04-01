937c518d94a8df3ac4df8d9928392d4562abd872
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-04-01 14:20:20 -0700

    Use 1 Font Awesome file

--- custom/panel_templates/Default/template.php
+++ custom/panel_templates/Default/template.php
@@ -30,7 +30,7 @@ if (!class_exists('Default_Panel_Template')) {
             );
 
             $this->addCSSFiles([
-                (defined('CONFIG_PATH') ? CONFIG_PATH : '') . '/custom/panel_templates/Default/assets/plugins/fontawesome/css/all.min.css' => [],
+                (defined('CONFIG_PATH') ? CONFIG_PATH : '') . '/core/assets/css/font-awesome.min.css' => [],
                 (defined('CONFIG_PATH') ? CONFIG_PATH : '') . '/custom/panel_templates/Default/assets/css/sb-admin-2.min.css' => [],
                 'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i' => [],
                 (defined('CONFIG_PATH') ? CONFIG_PATH : '') . '/custom/panel_templates/Default/assets/css/custom.css?v=2' => [],
