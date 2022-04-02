8b23ee1c2a0e65e7d0e384fdb5b64f02f52b7478
Robin <robinslot@hotmail.nl>
2022-04-03 01:16:09 +0200

    Add script to find unused terms

---
+++ scripts/find-unused-language-terms.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+if [ ! -d 'custom' ]
+then
+    echo "You need to run this script from the Nameless repo root (scripts/find-unused-language-terms.sh)"
+    exit 1
+fi
+
+KEYS=$(jq -M -r 'keys | .[]' custom/languages/en_UK.json)
+for KEY in $KEYS
+do
+    BEFORE_SLASH="${KEY%%/*}"
+    AFTER_SLASH="${KEY#*/}"
+
+    if ! grep -r --exclude-dir .git "get('$BEFORE_SLASH', '$AFTER_SLASH')" > /dev/null
+    then
+        echo "$KEY"
+    fi
+done
