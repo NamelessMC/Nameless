a9de07471c5a4a8966cfbaf1a125371d2d688d92
Partydragen <admin@partydragen.com>
2022-04-14 19:10:46 +0200

    Fix Synchronisation text

--- custom/languages/EnglishUK/admin.php
+++ custom/languages/EnglishUK/admin.php
@@ -2,7 +2,7 @@
 /*
  *  Made by Samerton
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-pr12
+ *  NamelessMC version 2.0.0-pr13
  *
  *  License: MIT
  *
@@ -776,5 +776,5 @@ $language = [
     'integration_identifier_required' => '{{integration}} identifier is required.',
     'integration_identifier_invalid' => '{{integration}} identifier is invalid.',
     'sync_integration' => 'Sync Integration',
-    'integration_sync_not_supported' => 'Synchronization is not supported for this integration!',
+    'integration_sync_not_supported' => 'Synchronisation is not supported for this integration!',
 ];
