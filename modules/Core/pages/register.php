d9f8717863097625190213397be44d28038da23f
Robin <robinslot@hotmail.nl>
2022-04-26 19:42:01 +0200

    replace $user->getIP usage

--- modules/Core/pages/register.php
+++ modules/Core/pages/register.php
@@ -359,7 +359,7 @@ if (Input::exists()) {
                             }
                         }
 
-                        Log::getInstance()->log(Log::Action('user/register'), '', $user_id, $user->getIP());
+                        Log::getInstance()->log(Log::Action('user/register'), '', $user_id, Util::getRemoteAddress());
 
                         EventHandler::executeEvent('registerUser', [
                             'user_id' => $user_id,
