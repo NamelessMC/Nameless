5f3577aca044bd4e30b6c7a517a4799f74d216ae
Robin <robin+git@rkslot.nl>
2022-05-11 12:14:37 +0200

    Merge DB and DB_Custom to one class

--- core/classes/Database/DB.php
+++ core/classes/Database/DB.php
@@ -8,29 +8,411 @@
  * @version 2.0.0-pr13
  * @license MIT
  */
-class DB extends Instanceable {
+class DB {
 
-    use InteractsWithDatabase;
+    private static ?DB $_instance = null;
 
-    public function __construct() {
+    protected PDO $_pdo;
+    private PDOStatement $_statement;
+    private bool $_error = false;
+    private array $_results;
+    private string $_prefix = 'nl2_';
+    private int $_count = 0;
+    protected QueryRecorder $_query_recorder;
+
+    private function __construct(string $host, string $database, string $username, string $password, int $port = 3306, ?string $force_charset, string $prefix) {
         try {
-            $charset = '';
-            if (Config::get('mysql/initialise_charset')) {
-                $charset = 'charset=' . (Config::get('mysql/charset') ?: 'utf8mb4');
+            $connection_string = 'mysql:host=' . $host . ';port=' . $port . ';dbname=' . $database;
+            if ($force_charset) {
+                $connection_string .= ';charset=' . $force_charset;
             }
-
             $this->_pdo = new PDO(
-                'mysql:host=' . Config::get('mysql/host') . ';port=' . Config::get('mysql/port') . ';dbname=' . Config::get('mysql/db') . ';' . $charset,
-                Config::get('mysql/username'),
-                Config::get('mysql/password'),
+                $connection_string,
+                $username,
+                $password,
                 [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-                ],
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"
+                ]
             );
+            $this->_prefix = $prefix;
         } catch (PDOException $e) {
             die("<strong>Error:<br /></strong><div class=\"alert alert-danger\">" . $e->getMessage() . '</div>Please check your database connection settings.');
         }
 
         $this->_query_recorder = QueryRecorder::getInstance();
     }
+
+    /**
+     * Get the underlying PDO instance.
+     *
+     * @return PDO The PDO instance.
+     */
+    public function getPDO(): PDO {
+        return $this->_pdo;
+    }
+
+    /**
+     * Get the first row of the result.
+     *
+     * @return object|null The result object, or null if no result was returned.
+     */
+    public function first(): ?object {
+        return $this->results()[0] ?? null;
+    }
+
+    /**
+     * Get all the results of the query.
+     *
+     * @return array The results of the query.
+     */
+    public function results(): array {
+        return $this->_results;
+    }
+
+    /**
+     * Get the number of rows affected by the last query.
+     *
+     * @return int The number of rows.
+     */
+    public function count(): int {
+        return $this->_count;
+    }
+
+    /**
+     * Get the last inserted ID
+     *
+     * @return string|false ID of the last inserted row or false on failure
+     */
+    public function lastId() {
+        return $this->_pdo->lastInsertId();
+    }
+
+    /**
+     * Whether there was an error during the last query.
+     *
+     * @return bool Whether there was an error.
+     */
+    public function error(): bool {
+        return $this->_error;
+    }
+
+    /**
+     * Perform a SELECT query on the database.
+     *
+     * @param string $table The table to select from.
+     * @param array $where The where clause.
+     * @return static|false This instance if successful, false otherwise.
+     */
+    public function get(string $table, array $where = []) {
+        return $this->action('SELECT *', $table, $where);
+    }
+
+    /**
+     * Perform a DELTE query on the database.
+     *
+     * @param string $table The table to delete from.
+     * @param array $where The where clause.
+     * @return static|false This instance if successful, false otherwise.
+     */
+    public function delete(string $table, array $where) {
+        return $this->action('DELETE', $table, $where);
+    }
+
+    /**
+     * Perform a raw SQL query on the database.
+     *
+     * @param string $sql The SQL query string to execute.
+     * @param array $params The parameters to bind to the query.
+     * @param int $fetch_method The PDO fetch method to use.
+     * @return static This DB instance.
+     */
+    public function query(string $sql, array $params = [], int $fetch_method = PDO::FETCH_OBJ) {
+        $this->_error = false;
+        if ($this->_statement = $this->_pdo->prepare($sql)) {
+            $x = 1;
+            foreach ($params as $param) {
+                // Convert "true" and "false" to 1 and 0 so that query params can be more fluent
+                if (is_bool($param)) {
+                    $param = $param ? 1 : 0;
+                }
+                $this->_statement->bindValue($x, $param, is_int($param)
+                    ? PDO::PARAM_INT
+                    : PDO::PARAM_STR
+                );
+                $x++;
+            }
+
+            $this->_query_recorder->pushQuery($sql, $params);
+
+            if ($this->_statement->execute()) {
+                // Only fetch the results if this is a SELECT query.
+                if (str_starts_with(strtoupper($sql), 'SELECT')) {
+                    $this->_results = $this->_statement->fetchAll($fetch_method);
+                }
+                $this->_count = $this->_statement->rowCount();
+            } else {
+                print_r($this->_pdo->errorInfo());
+                $this->_error = true;
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @deprecated Use query() instead
+     * @return static
+     */
+    public function selectQuery(string $sql, array $params = [], int $fetch_method = PDO::FETCH_OBJ) {
+        return $this->query($sql, $params, $fetch_method);
+    }
+
+    /**
+     * @deprecated Use query() instead
+     * @return static
+     */
+    public function createQuery(string $sql, array $params = []) {
+        return $this->query($sql, $params);
+    }
+
+    /**
+     * Execute some SQL action (which uses a where clause) on the database.
+     *
+     * @param string $action The action to perform (SELECT, DELETE).
+     * @param string $table The table to perform the action on.
+     * @param array $where The where clause.
+     * @return static|false This instance if successful, false otherwise.
+     */
+    private function action(string $action, string $table, array $where = []) {
+        [$field, $operator, $value] = $this->parseWhereClause($where);
+
+        $table = $this->_prefix . $table;
+        $sql = "{$action} FROM {$table} WHERE {$field} {$operator} ?";
+
+        if (!$this->query($sql, [$value])->error()) {
+            return $this;
+        }
+
+        return false;
+    }
+
+    /**
+     * Insert a new row into a table within the database.
+     *
+     * @param string $table The table to insert into.
+     * @param array $fields Array of data in "column => value" format to insert.
+     * @return bool Whether an error occurred or not.
+     */
+    public function insert(string $table, array $fields = []): bool {
+        $keys = array_keys($fields);
+        $fieldCount = count($fields);
+        $values = '';
+        $x = 1;
+
+        for ($i = 0; $i < $fieldCount; $i++) {
+            $values .= '?';
+            if ($x < $fieldCount) {
+                $values .= ', ';
+            }
+            $x++;
+        }
+
+        $table = $this->_prefix . $table;
+        $sql = "INSERT INTO {$table} (`" . implode('`,`', $keys) . "`) VALUES ({$values})";
+
+        return !$this->query($sql, $fields)->error();
+    }
+
+    /**
+     * Perform an UPDATE query on a table.
+     *
+     * @param string $table The table to update.
+     * @param mixed $where The where clause. If not an array, it will be used for "id" column lookup.
+     * @param array $fields Array of data in "column => value" format to update.
+     * @return bool Whether an error occurred or not.
+     */
+    public function update(string $table, $where, array $fields): bool {
+        $set = '';
+        $x = 1;
+
+        foreach (array_keys($fields) as $column) {
+            $set .= "`{$column}` = ?";
+            if ($x < count($fields)) {
+                $set .= ', ';
+            }
+            $x++;
+        }
+
+        if (!is_array($where)) {
+            $field = 'id';
+            $operator = '=';
+            $value = $where;
+        } else {
+            [$field, $operator, $value] = $this->parseWhereClause($where);
+        }
+
+        $table = $this->_prefix . $table;
+
+        if (is_string($value)) {
+            $value = "'{$value}'";
+        }
+
+        $sql = "UPDATE {$table} SET {$set} WHERE `{$field}` {$operator} {$value}";
+
+        return !$this->query($sql, $fields)->error();
+    }
+
+    /**
+     * Increment a numeric column value by 1.
+     *
+     * @param string $table The table to use.
+     * @param int $id The id of the row to increment a column in.
+     * @param string $field The field to increment.
+     * @return bool Whether an error occurred or not.
+     */
+    public function increment(string $table, int $id, string $field): bool {
+        $table = $this->_prefix . $table;
+
+        return !$this->query("UPDATE {$table} SET {$field} = {$field} + 1 WHERE id = ?", [$id])->error();
+    }
+
+    /**
+     * Select rows from the database, ordering by a specific column and sort type.
+     *
+     * @param string $table The table to use.
+     * @param string $order The column to order by.
+     * @param string $sort ASC or DESC
+     * @return static|false This instance if successful, false otherwise.
+     */
+    public function orderAll(string $table, string $order, string $sort) {
+        $table = $this->_prefix . $table;
+        $sql = "SELECT * FROM {$table} ORDER BY {$order} {$sort}";
+
+        if (!$this->query($sql)->error()) {
+            return $this;
+        }
+
+        return false;
+    }
+
+    /**
+     * Select rows from the database with a where clause, ordering by a specific column and sort type.
+     *
+     * @param string $table The table to use.
+     * @param string $order The column to order by.
+     * @param string $sort ASC or DESC
+     * @return static|false This instance if successful, false otherwise.
+     */
+    public function orderWhere(string $table, string $where, string $order, string $sort) {
+        $table = $this->_prefix . $table;
+        $sql = "SELECT * FROM {$table} WHERE {$where} ORDER BY {$order} {$sort}";
+
+        if (!$this->query($sql)->error()) {
+            return $this;
+        }
+
+        return false;
+    }
+
+    /**
+     * Create a new table in the database.
+     *
+     * @param string $name The name of the table.
+     * @param string $table_schema The table SQL schema.
+     * @param string $other Other SQL table options.
+     * @return bool Whether an error occurred or not.
+     */
+    public function createTable(string $name, string $table_schema, string $other): bool {
+        $name = $this->_prefix . $name;
+        $sql = "CREATE TABLE `{$name}` ({$table_schema}) {$other}";
+
+        return !$this->query($sql)->error();
+    }
+
+    /**
+     * Perform a SHOW TABLES LIKE query.
+     *
+     * @param string $table Name of table to try and lookup.
+     * @return int|false The number of rows affected, or false on failure.
+     */
+    public function showTables(string $table) {
+        $table = $this->_prefix . $table;
+        $sql = "SHOW TABLES LIKE '{$table}'";
+
+        if (!$this->query($sql)->error()) {
+            return $this->_statement->rowCount();
+        }
+
+        return false;
+    }
+
+    /**
+     * Add a new column to a table.
+     *
+     * @param string $table Name of table to alter.
+     * @param string $column The column to add.
+     * @param string $attributes The attributes of the column.
+     * @return bool Whether an error occurred or not.
+     */
+    public function addColumn(string $table, string $column, string $attributes): bool {
+        $table = $this->_prefix . $table;
+        $sql = "ALTER TABLE {$table} ADD {$column} {$attributes}";
+
+        return !$this->query($sql)->error();
+    }
+
+    /**
+     * Attempt to parse a where clause in array format.
+     *
+     * @internal This method is used by the class and should not be called directly.
+     * @param array $clause The where clause.
+     * @return array The parsed where clause as [field, operator, value] form.
+     */
+    private function parseWhereClause(array $clause): array {
+        $operators = ['=', '>', '<', '>=', '<=', '<>'];
+        if (count($clause) === 3) {
+            [$field, $operator, $value] = $clause;
+        } else if (count($clause) === 2) {
+            [$field, $value] = $clause;
+            $operator = '=';
+        } else {
+            throw new InvalidArgumentException("Invalid SQL where clause");
+        }
+
+        if (!in_array($operator, $operators)) {
+            throw new InvalidArgumentException("Invalid SQL operator: '{$operator}'");
+        }
+
+        return [$field, $operator, $value];
+    }
+
+    public static function getCustomInstance(string $host, string $database, string $username,
+            string $password, int $port = 3306, ?string $force_charset = null, string $prefix = 'nl2_'): DB {
+        return new DB($host, $database, $username, $password, $port, $force_charset, $prefix);
+    }
+
+    public static function getInstance(): DB {
+        if (self::$_instance) {
+            return self::$_instance;
+        }
+
+        if (Config::get('mysql/initialise_charset')) {
+            $force_charset = Config::get('mysql/charset') ?: 'utf8mb4';
+        } else {
+            $force_charset = null;
+        }
+
+        self::$_instance = self::getCustomInstance(
+            Config::get('mysql/host'),
+            Config::get('mysql/db'),
+            Config::get('mysql/username'),
+            Config::get('mysql/password'),
+            Config::get('mysql/port'),
+            $force_charset=$force_charset
+        );
+
+        return self::$_instance;
+    }
+
 }
