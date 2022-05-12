13aaae781a3928a2f150edd12db0f517d3efdde5
PadowYT2 <71085027+PadowYT2@users.noreply.github.com>
2022-05-12 10:48:17 +0300

    resolve conversation

--- custom/templates/DefaultRevamp/template_settings/settings.php
+++ custom/templates/DefaultRevamp/template_settings/settings.php
@@ -45,67 +45,67 @@ if ($cache->isCached('navbarColour')) {
 }
 
 $nav_colours = [
-    0 => [
+    [
         'value' => 'white',
         'name' => $language->get('general', 'default'),
         'selected' => ($navbarColour == 'white')
     ],
-    1 => [
+    [
         'value' => 'red',
         'name' => $language->get('general', 'red'),
         'selected' => ($navbarColour == 'red')
     ],
-    2 => [
+    [
         'value' => 'orange',
         'name' => $language->get('general', 'orange'),
         'selected' => ($navbarColour == 'orange')
     ],
-    3 => [
+    [
         'value' => 'yellow',
         'name' => $language->get('general', 'yellow'),
         'selected' => ($navbarColour == 'yellow')
     ],
-    4 => [
+    [
         'value' => 'olive',
         'name' => $language->get('general', 'olive'),
         'selected' => ($navbarColour == 'olive')
     ],
-    5 => [
+    [
         'value' => 'green',
         'name' => $language->get('general', 'green'),
         'selected' => ($navbarColour == 'green')
     ],
-    6 => [
+    [
         'value' => 'teal',
         'name' => $language->get('general', 'teal'),
         'selected' => ($navbarColour == 'teal')
     ],
-    7 => [
+    [
         'value' => 'blue',
         'name' => $language->get('general', 'blue'),
         'selected' => ($navbarColour == 'blue')
     ],
-    8 => [
+    [
         'value' => 'violet',
         'name' => $language->get('general', 'violet'),
         'selected' => ($navbarColour == 'violet')
     ],
-    9 => [
+    [   
         'value' => 'purple',
         'name' => $language->get('general', 'purple'),
         'selected' => ($navbarColour == 'purple')
     ],
-    10 => [
+    [
         'value' => 'pink',
         'name' => $language->get('general', 'pink'),
         'selected' => ($navbarColour == 'pink')
     ],
-    11 => [
+    [
         'value' => 'brown',
         'name' => $language->get('general', 'brown'),
         'selected' => ($navbarColour == 'brown')
     ],
-    12 => [
+    [
         'value' => 'grey',
         'name' => $language->get('general', 'grey'),
         'selected' => ($navbarColour == 'grey')
