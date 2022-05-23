6f7b88cd35b88ed2fd6176476810049fa67745f6
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-05-22 23:20:46 -0600

    Create email.php

---
+++ core/email.php
@@ -0,0 +1,11 @@
+<?php
+$GLOBALS['email'] = [
+    'email' => '',
+    'username' => '',
+    'password' => '',
+    'name' => ' ',
+    'host' => '',
+    'port' => 587,
+    'secure' => 'tls',
+    'smtp_auth' => true,
+];
