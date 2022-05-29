f81d3f077dfbb10e73b40c238b67de952298bcfc
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-05-29 16:55:48 -0600

    Fix mention_users avatars

--- modules/Core/queries/mention_users.php
+++ modules/Core/queries/mention_users.php
@@ -7,8 +7,8 @@ if (!$user->isLoggedIn()) {
 }
 
 $users = DB::getInstance()->query(
-    'SELECT u.id, u.nickname, u.gravatar, u.email, u.has_avatar, u.avatar_updated, IFNULL(nl2_users_integrations.identifier, \'none\') as uuid FROM nl2_users u LEFT JOIN nl2_users_integrations ON user_id=u.id AND integration_id=1 WHERE u.nickname LIKE ?',
-    ["{$_GET['nickname']}%"]
+    'SELECT u.id, u.username, u.nickname, u.gravatar, u.email, u.has_avatar, u.avatar_updated, IFNULL(nl2_users_integrations.identifier, \'none\') as uuid FROM nl2_users u LEFT JOIN nl2_users_integrations ON user_id=u.id AND integration_id=1 WHERE u.nickname LIKE ? OR u.username LIKE ?',
+    ["{$_GET['nickname']}%", "{$_GET['nickname']}%"]
 )->results();
 
 $users_json = [];
