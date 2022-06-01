d887622e3b875eb0efdf5381613570197a2cf39b
Robin <robinslot@hotmail.nl>
2022-06-01 12:02:44 +0200

    Handle missing cookieconsent

--- modules/Cookie Consent/assets/js/template.js
+++ modules/Cookie Consent/assets/js/template.js
@@ -1,6 +1,9 @@
-window.cookieconsent.initialise({
-  onStatusChange: function() {
-    window.location.reload();
-  },
-  //"{x}"
-});
+if (window.cookieconsent === undefined) {
+  console.warn('Failed to initialise cookie consent, it may be blocked by your browser or a browser extension');
+} else {
+  window.cookieconsent.initialise({
+    onStatusChange: function() {
+      window.location.reload();
+    },
+  });
+}
