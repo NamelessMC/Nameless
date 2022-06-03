cba96c7cba6446f740a27de5bb6be1bd015e416b
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-06-02 21:55:14 -0600

    Fix database connection for emoji support

--- core/classes/Database/DB.php
+++ core/classes/Database/DB.php
@@ -36,7 +36,6 @@ class DB {
                 $password,
                 [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"
                 ]
             );
 
