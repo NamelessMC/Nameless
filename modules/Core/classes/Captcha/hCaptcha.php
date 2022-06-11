a0a8df5612dcbdacbde95af591708194fec23655
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2022-06-11 18:30:42 +0200

    Update hCaptcha.php (#2849)

--- modules/Core/classes/Captcha/hCaptcha.php
+++ modules/Core/classes/Captcha/hCaptcha.php
@@ -33,12 +33,7 @@ class hCaptcha extends CaptchaBase {
     }
 
     public function validateKey(string $key) : bool {
-        $url = 'https://api.hcaptcha.com/checksiteconfig?sitekey=' . $key;
-        $client = HttpClient::get($url);
-        if ($client->hasError()) {
-            return false;
-        }
-        return $client->json(true)['pass'] == true;
+        return true; // hCaptcha changed their validation so this is a temporary fix
     }
 
     public function getHtml(): string {
