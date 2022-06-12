a01120d1fe1919e9f90ff5401460905d46b0b192
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-06-11 22:16:59 -0600

    Use migration to drop group_html_lg column

--- core/includes/updates/200-pr13.php
+++ core/includes/updates/200-pr13.php
@@ -3,16 +3,6 @@
 class Nameless200 extends UpgradeScript {
 
     public function run(): void {
-        $db = DB::getInstance();
-
-        try {
-            $db->query('ALTER TABLE nl2_groups DROP COLUMN group_html_lg;');
-        } catch (PDOException $e) {
-            if (!str_contains($e->getMessage(), 'check that column/key exists')) {
-                $this->log($e->getMessage());
-            }
-        }
-
         $this->setVersion('2.0.0');
     }
 }
