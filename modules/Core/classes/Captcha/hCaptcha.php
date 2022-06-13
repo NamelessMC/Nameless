bf23040247e2580aba69dac698d4a807a7a758c5
PadowYT2 <me@padow.ru>
2022-06-13 10:15:20 +0800

    Dark/light mode for Captcha (#2852)
    Co-authored-by: Tadhg Boyle <tadhgsmboyle@gmail.com>

--- modules/Core/classes/Captcha/hCaptcha.php
+++ modules/Core/classes/Captcha/hCaptcha.php
@@ -37,7 +37,11 @@ class hCaptcha extends CaptchaBase {
     }
 
     public function getHtml(): string {
-        return '<div class="h-captcha" data-sitekey="' . $this->getPublicKey() . '"></div>';
+        if (defined('DARK_MODE') && DARK_MODE == 1) {
+            return '<div class="h-captcha" data-sitekey="' . $this->getPublicKey() . '" data-theme="dark"></div>';
+        } else {
+            return '<div class="h-captcha" data-sitekey="' . $this->getPublicKey() . '"></div>';
+        }
     }
 
     public function getJavascriptSource(): string {
