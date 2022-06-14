2cf6f139be409e6e2dfafa7078751a37d0368ede
PadowYT2 <71085027+PadowYT2@users.noreply.github.com>
2022-06-14 18:28:28 +0300

    no more tabs

--- core/assets/js/mcassoc_client.js
+++ core/assets/js/mcassoc_client.js
@@ -1,39 +1,39 @@
-var MCAssoc = (function() {
-	var MCAssoc = {
-		baseurl: 'https://mcassoc.lukegb.com/'
-	};
+var MCAssoc = (function () {
+    var MCAssoc = {
+        baseurl: 'https://mcassoc.lukegb.com/'
+    };
 
-	var iframe;
+    var iframe;
 
-	MCAssoc.init = function(siteid, key, postback, mcusername) {
-		iframe = document.getElementById('mcassoc');
+    MCAssoc.init = function (siteid, key, postback, mcusername) {
+        iframe = document.getElementById('mcassoc');
 
-		// mcusername is optional
-		mcusername = mcusername || null;
+        // mcusername is optional
+        mcusername = mcusername || null;
 
-		// formulate the URL:
-		this.url = this.baseurl + 'perform?';
+        // formulate the URL:
+        this.url = this.baseurl + 'perform?';
 
-		var pieces = {
-			siteid: siteid,
-			key: key,
-			postback: postback,
-			mcusername: mcusername
-		};
-		var qs = '';
-		for (var piecen in pieces) {
-			if (!pieces.hasOwnProperty(piecen)) continue;
-			var piecev = pieces[piecen];
-			if (piecev) {
-				if (qs != '') qs += '&';
-				qs += encodeURIComponent(piecen) + '=' + encodeURIComponent(piecev);
-			}
-		}
+        var pieces = {
+            siteid: siteid,
+            key: key,
+            postback: postback,
+            mcusername: mcusername
+        };
+        var qs = '';
+        for (var piecen in pieces) {
+            if (!pieces.hasOwnProperty(piecen)) continue;
+            var piecev = pieces[piecen];
+            if (piecev) {
+                if (qs != '') qs += '&';
+                qs += encodeURIComponent(piecen) + '=' + encodeURIComponent(piecev);
+            }
+        }
 
-		this.url += qs;
+        this.url += qs;
 
-		iframe.src = this.url;
-	};
+        iframe.src = this.url;
+    };
 
-	return MCAssoc;
+    return MCAssoc;
 })();
