0c0c29e48be7b42d9cf4da223b291474549394ee
samerton <samerton@users.noreply.github.com>
2022-06-23 20:30:39 +0100

    Default Language::get $term to null

--- core/classes/Core/Language.php
+++ core/classes/Core/Language.php
@@ -203,7 +203,7 @@ class Language {
      * @param array $variables Any variables to pass through to the translation.
      * @return string Translated phrase.
      */
-    public function get(string $section, ?string $term, array $variables = []): string {
+    public function get(string $section, ?string $term = null, array $variables = []): string {
         if ($term) {
             $section .= '/' . $term;
         }
