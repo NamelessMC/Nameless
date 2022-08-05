5b9fc90432583c03bac5c0f29edc930ba247193a
samerton <samerton@users.noreply.github.com>
2022-08-05 22:36:27 +0100

    Fix panel upgrade page

--- modules/Core/pages/panel/upgrade.php
+++ modules/Core/pages/panel/upgrade.php
@@ -2,7 +2,7 @@
 /*
  *  Made by Samerton
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-pr8
+ *  NamelessMC version 2.0.0
  *
  *  License: MIT
  *
@@ -10,23 +10,27 @@
  */
 
 // Ensure an update is needed
-$update_needed = Util::getSetting('version_update');
+$update_needed = DB::getInstance()->query('SELECT `value` FROM nl2_settings WHERE `name` = \'version_update\'')->first();
 
-if ($update_needed !== 'true' && $update_needed !== 'urgent') {
+if (!$update_needed || $update_needed->value !== 'true' && $update_needed->value !== 'urgent') {
     Redirect::to(URL::build('/panel/update'));
 }
 
 $cache = new Cache(['name' => 'nameless', 'extension' => '.cache', 'path' => ROOT_PATH . '/cache/']);
 
-// Perform the update
-$upgradeScript = UpgradeScript::get(Util::getSetting('nameless_version'));
-if ($upgradeScript instanceof UpgradeScript) {
-    $upgradeScript->run();
-}
+$version = DB::getInstance()->query('SELECT `value` FROM nl2_settings WHERE `name` = \'nameless_version\'')->first();
 
-$cache->setCache('update_check');
-if ($cache->isCached('update_check')) {
-    $cache->erase('update_check');
+if ($version) {
+    // Perform the update
+    $upgradeScript = UpgradeScript::get($version->value);
+    if ($upgradeScript instanceof UpgradeScript) {
+        $upgradeScript->run();
+    }
+
+    $cache->setCache('update_check');
+    if ($cache->isCached('update_check')) {
+        $cache->erase('update_check');
+    }
 }
 
 Redirect::to(URL::build('/panel/update'));
