a0ffdbda83347c19f8ce44a4d0ac4295f0943808
samerton <samerton@users.noreply.github.com>
2022-08-05 17:16:34 +0100

    Fix forum post avatars not matching rest of site

--- modules/Forum/pages/forum/view_topic.php
+++ modules/Forum/pages/forum/view_topic.php
@@ -308,7 +308,7 @@ if (Input::exists()) {
                     'author' => $user->getDisplayname(),
                 ]),
                 'content_full' => strip_tags(str_ireplace(['<br />', '<br>', '<br/>'], "\r\n", $content)),
-                'avatar_url' => $user->getAvatar(128, true),
+                'avatar_url' => $user->getAvatar(null, true),
                 'title' => $topic->topic_title,
                 'url' => URL::getSelfURL() . ltrim(URL::build('/forum/topic/' . urlencode($topic->id) . '-' . $forum->titleToURL($topic->topic_title)), '/'),
                 'topic_author_user_id' => $topic_user->data()->id,
@@ -687,7 +687,7 @@ foreach ($results->data as $n => $nValue) {
                     'username' => $reaction_user->getDisplayname(true),
                     'nickname' => $reaction_user->getDisplayname(),
                     'style' => $reaction_user->getGroupStyle(),
-                    'avatar' => $reaction_user->getAvatar(500),
+                    'avatar' => $reaction_user->getAvatar(),
                     'profile' => $reaction_user->getProfileURL()
                 ];
             }
@@ -711,7 +711,7 @@ foreach ($results->data as $n => $nValue) {
         'heading' => $heading,
         'id' => $nValue->id,
         'user_id' => $post_creator->data()->id,
-        'avatar' => $post_creator->getAvatar(500),
+        'avatar' => $post_creator->getAvatar(),
         'integrations' => $user_integrations,
         'username' => $post_creator->getDisplayname(),
         'mcname' => $post_creator->getDisplayname(true),
