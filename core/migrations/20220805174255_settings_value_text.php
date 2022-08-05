c35ac6c3992541a4c52f0f4fc700ff7229b4ad00
samerton <samerton@users.noreply.github.com>
2022-08-05 18:45:43 +0100

    Change settings table value column from varchar to text Closes #2880

---
+++ core/migrations/20220805174255_settings_value_text.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class SettingsValueText extends AbstractMigration
+{
+    public function change(): void
+    {
+        $table = $this->table('nl2_settings');
+        $table->changeColumn('value', 'text');
+        $table->update();
+    }
+}
