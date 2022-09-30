fba983255062e5b5766656444981cc1d9473a0b5
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-09-29 18:47:26 -0600

    Cleanup some frontend JS

--- custom/templates/DefaultRevamp/js/core/core.js
+++ custom/templates/DefaultRevamp/js/core/core.js
@@ -7,24 +7,6 @@ function redirect(url) {
     window.location.href = url;
 }
 
-function copy(element) {
-    const temp = $('<input>');
-    $('body').append(temp);
-    temp.val($(element).text()).select();
-    document.execCommand('copy');
-    temp.remove();
-    $('body').toast({
-        showIcon: 'checkmark',
-        message: copied,
-        class: 'success',
-        progressUp: true,
-        displayTime: 6000,
-        showProgress: 'bottom',
-        pauseOnHover: false,
-        position: 'bottom left',
-    });
-}
-
 $(document).ready(function () {
     $('[data-action="logout"]').click(function () {
         const url = $(this).data('link');
@@ -74,13 +56,19 @@ $(function () {
 
     $('*[data-poload]').mouseenter(function () {
         const elem = this;
-        $.get($(elem).data('poload'),
-            function (d) {
-                (debugging ? console.log(d) : '');
-                const data = JSON.parse(d);
+        $.get(
+            $(elem).data('poload'),
+            function (data) {
+                if (debugging) {
+                    console.log(data);
+                }
+
+                data = JSON.parse(data);
                 cachedUsers[$(elem).data('poload')] = data;
+
                 const tmp = document.createElement('div');
                 tmp.innerHTML = data.html;
+
                 const img = tmp.getElementsByTagName('img')[0];
                 const image = new Image();
                 image.src = img.src;
@@ -96,14 +84,13 @@ $(function () {
     });
 
     const timezone = document.getElementById('timezone');
-
     if (timezone) {
-        const timezoneValue = Intl.DateTimeFormat().resolvedOptions().timeZone;
+        const dateFormatter = Intl.DateTimeFormat();
+        const timezoneValue = dateFormatter.resolvedOptions().timeZone;
         if (timezoneValue) {
             timezone.value = timezoneValue;
         }
     }
-
 });
 
 const announcements = document.querySelectorAll('[id^="announcement"]');
