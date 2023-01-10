159317581dd86e61d9f568a2e4f45c176f2e14a4
Robin <robin+git@rkslot.nl>
2023-01-10 19:12:40 +0000

    Fix sorting with thousands separators in value (#3162)

--- core/classes/Misc/Placeholders.php
+++ core/classes/Misc/Placeholders.php
@@ -124,7 +124,7 @@ class Placeholders extends Instanceable {
         $sort = $this->getPlaceholder($server_id, sha1($placeholder_name))->leaderboard_sort;
 
         // We have to add 0 to value so mysql converts from the TEXT field to an integer value
-        $leaderboard_data = $this->_db->query("SELECT * FROM nl2_users_placeholders WHERE name = ? AND server_id = ? ORDER BY value + 0 {$sort} LIMIT 50", [$placeholder_name, $server_id]);
+        $leaderboard_data = $this->_db->query("SELECT * FROM nl2_users_placeholders WHERE name = ? AND server_id = ? ORDER BY REPLACE(value, ',', '') + 0 {$sort} LIMIT 50", [$placeholder_name, $server_id]);
 
         if (!$leaderboard_data->count()) {
             return [];
