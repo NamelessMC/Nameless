6cd9b134e1a9642a274e4de796baa521b23be891
Tadhg Boyle <tadhgsmboyle@gmail.com>
2023-01-18 10:21:41 -0800

    Fix LatestPostsWidget bugs (#3204)
    * Fix MySQL `IN` clause string interpolation in Forum `getLatestDiscussions`
    
    * Fix $limit being applied
    
    * Remove comments

--- modules/Forum/widgets/LatestPostsWidget.php
+++ modules/Forum/widgets/LatestPostsWidget.php
@@ -54,21 +54,14 @@ class LatestPostsWidget extends WidgetBase {
             $template_array = $this->_cache->retrieve('discussions');
 
         } else {
+            $limit = (int) Util::getSetting('latest_posts_limit', 5, 'Forum');
             // Generate latest posts
-            $discussions = $forum->getLatestDiscussions($user_groups, ($this->_user->isLoggedIn() ? $this->_user->data()->id : 0), 5);
-
-            $n = 0;
-            // Calculate the number of discussions to display
-            $limit = Util::getSetting('latest_posts_limit', 5, 'Forum');
-            if (count($discussions) <= $limit) {
-                $limit = count($discussions);
-            }
+            $discussions = $forum->getLatestDiscussions($user_groups, ($this->_user->isLoggedIn() ? $this->_user->data()->id : 0), $limit);
 
             $template_array = [];
 
             // Generate an array to pass to template
-            while ($n < $limit) {
-                $discussion = $discussions[$n];
+            foreach ($discussions as $discussion) {
                 // Get the name of the forum from the ID
                 $forum_name = $db->get('forums', ['id', $discussion->forum_id])->results();
                 $forum_name = Output::getPurified($forum_name[0]->forum_title);
@@ -128,8 +121,6 @@ class LatestPostsWidget extends WidgetBase {
                     'last_reply_profile_link' => $last_reply_user->getProfileURL(),
                     'last_reply_link' => URL::build('/forum/topic/' . $discussion->id . '-' . $forum->titleToURL($discussion->topic_title), 'pid=' . $discussion->last_post_id)
                 ];
-
-                $n++;
             }
 
             $this->_cache->store('discussions', $template_array, 60);
