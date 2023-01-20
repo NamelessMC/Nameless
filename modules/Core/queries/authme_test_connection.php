5811314ed3a98aa511698b2901d5dbb52e4f1147
Tadhg Boyle <tadhgsmboyle@gmail.com>
2023-01-19 22:14:54 -0800

    wip

--- modules/Core/queries/authme_test_connection.php
+++ modules/Core/queries/authme_test_connection.php
@@ -19,6 +19,7 @@ if (!isset($_POST['db_address'], $_POST['db_name'], $_POST['db_username'], $_POS
 try {
     $connection = DB::getCustomInstance($_POST['db_address'], $_POST['db_name'], $_POST['db_username'], $_POST['db_password'], $_POST['db_port']);
     $connection->query('SELECT * FROM ' . $_POST['db_table'] . ' LIMIT 1');
+    // TODO: add check for hashing algorithm, will need a nice way to check if a password hash is of a certain hash algo
 } catch (Exception $e) {
     die('Invalid connection');
 }
