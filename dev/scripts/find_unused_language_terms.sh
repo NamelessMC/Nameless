8ce00fcebbcc210b90cc5ab7d6b99af3b7b22bd2
Tadhg Boyle <tadhgsmboyle@gmail.com>
2023-02-07 19:18:29 -0800

    wip

--- dev/scripts/find_unused_language_terms.sh
+++ dev/scripts/find_unused_language_terms.sh
@@ -6,32 +6,42 @@ then
 fi
 
 UNUSED_TERMS_FOUND=false
-KEYS=$(jq -M -r 'keys | .[]' custom/languages/en_UK.json)
+FILES=(
+  "custom/languages/en_UK.json"
+  "modules/Forum/language/en_UK.json"
+  "modules/Cookie Consent/language/en_UK.json"
+  "modules/Discord Integration/language/en_UK.json"
+)
 
-for KEY in $KEYS
+for FILE in "${FILES[@]}"
 do
-    BEFORE_SLASH="${KEY%%/*}"
-    if [ "$BEFORE_SLASH" = "time" ]
-    then
-        continue
-    fi
+  echo "Checking $FILE for unused terms..."
+  KEYS=$(jq -M -r 'keys | .[]' "$FILE")
+  for KEY in $KEYS
+  do
+      BEFORE_SLASH="${KEY%%/*}"
+      AFTER_SLASH="${KEY#*/}"
 
-    AFTER_SLASH="${KEY#*/}"
-
-    if ! grep -r --exclude-dir=.git --exclude-dir=vendor --exclude-dir=cache --exclude-dir=node_modules \
-            -e "get('$BEFORE_SLASH', '$AFTER_SLASH'" \
-            -e "get(\"$BEFORE_SLASH\", '$AFTER_SLASH'" \
-            -e "get('$BEFORE_SLASH', \"$AFTER_SLASH\"" \
-            -e "get(\"$BEFORE_SLASH\", \"$AFTER_SLASH\"" \
-            -e "'$AFTER_SLASH'," \
-            -e "= '$AFTER_SLASH'" \
-            -e "\['$AFTER_SLASH'\]" \
-            -e "\['$BEFORE_SLASH', '$AFTER_SLASH'\]" \
-            > /dev/null
-    then
-        UNUSED_TERMS_FOUND=true
-        echo "$KEY"
-    fi
+      # If running on macOS, install GNU Grep via `brew install grep`, and change `grep` to `ggrep` below
+      if ! grep -r --exclude-dir=.git --exclude-dir=vendor --exclude-dir=cache --exclude-dir=node_modules \
+              -e "get('$BEFORE_SLASH', '$AFTER_SLASH'" \
+              -e "get(\"$BEFORE_SLASH\", '$AFTER_SLASH'" \
+              -e "get('$BEFORE_SLASH', \"$AFTER_SLASH\"" \
+              -e "get(\"$BEFORE_SLASH\", \"$AFTER_SLASH\"" \
+              -e "'$AFTER_SLASH'," \
+              -e "= '$AFTER_SLASH'" \
+              -e "=> '$AFTER_SLASH']" \
+              -e "\['$AFTER_SLASH'\]" \
+              -e "\['$BEFORE_SLASH', '$AFTER_SLASH'\]" \
+              -e "? '$AFTER_SLASH'" \
+              -e ": '$AFTER_SLASH'" \
+              -e "getLanguageTerm('$AFTER_SLASH')" \
+              > /dev/null
+      then
+          UNUSED_TERMS_FOUND=true
+          echo "$FILE: $KEY"
+      fi
+  done
 done
 
 if $UNUSED_TERMS_FOUND
