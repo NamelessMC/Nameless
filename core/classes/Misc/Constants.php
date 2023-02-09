992da708971b51c3938931fe2148ea504f110ddb
Tadhg Boyle <tadhgsmboyle@gmail.com>
2023-02-09 12:57:45 -0800

    Add header comment to Constants class

--- core/classes/Misc/Constants.php
+++ core/classes/Misc/Constants.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Constants utility class.
+ *
+ * @package NamelessMC\Misc
+ * @author Derkades
+ * @version 2.1.0
+ * @license MIT
+ */
 class Constants {
 
     const ROOT_PATH = __DIR__ . '/../../..';
