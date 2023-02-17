37776521b27b30a28dafe4ffbe48cdc0c816afd5
Knuvex <46167826+Knuvex@users.noreply.github.com>
2023-02-17 08:27:09 +0100

    Codestyle revision

--- modules/Core/pages/user/messaging.php
+++ modules/Core/pages/user/messaging.php
@@ -405,13 +405,14 @@ if (!isset($_GET['action'])) {
 
                     // Display success message
                     $smarty->assign('MESSAGE_SENT', $language->get('user', 'message_sent_successfully'));
-                    unset($_POST['content']);if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-                    // Redirect to same page to avoid resubmitting form on refresh
-                    header('Location: ' . $_SERVER['REQUEST_URI']);
-                    exit;
-                }
-
+                    unset($_POST['content']);
+                    
+                    // Use strict comparison operator to compare request method
+                    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                       // Redirect to same page to avoid resubmitting form on refresh
+                       header('Location: ' . $_SERVER['REQUEST_URI']);
+                       exit;
+                    }
 
                 } else {
                     // Errors
