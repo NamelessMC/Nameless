845bb18fc15a9301671dc89f5501dd666b4e57c2
Tadhg Boyle <tadhgsmboyle@gmail.com>
2023-03-02 20:06:29 -0800

    Replace`external_query` with `query_type` in upgrade script, cc @supercrafter100

--- core/includes/updates/203.php
+++ core/includes/updates/203.php
@@ -1,23 +1,27 @@
 <?php
 return new class extends UpgradeScript {
 
-public function run(): void {
-    $this->runMigrations();
+    public function run(): void {
+        $this->runMigrations();
 
-    // Move query interval from cache to settings table
-    $cache = new Cache(['name' => 'nameless', 'extension' => '.cache', 'path' => ROOT_PATH . '/cache/']);
-    $cache->setCache('server_query_cache');
-    if ($cache->isCached('query_interval')) {
-        $query_interval = $cache->retrieve('query_interval');
-        if (is_numeric($query_interval) && $query_interval <= 60 && $query_interval >= 5) {
-            // Interval ok
-        } else {
-            // Default to 10
-            $query_interval = 10;
+        // Move query interval from cache to settings table
+        $cache = new Cache(['name' => 'nameless', 'extension' => '.cache', 'path' => ROOT_PATH . '/cache/']);
+        $cache->setCache('server_query_cache');
+        if ($cache->isCached('query_interval')) {
+            $query_interval = $cache->retrieve('query_interval');
+            if (is_numeric($query_interval) && $query_interval <= 60 && $query_interval >= 5) {
+                // Interval ok
+            } else {
+                // Default to 10
+                $query_interval = 10;
+            }
+            Util::setSetting('minecraft_query_interval', $query_interval);
         }
-        Util::setSetting('minecraft_query_interval', $query_interval);
-    }
 
-    $this->setVersion('2.1.0');
-}
+        // Replace`external_query` with `query_type`
+        Util::setSetting('query_type', Util::getSetting('external_query') == 1 ? 'external' : 'internal');
+        Util::setSetting('external_query', null);
+
+        $this->setVersion('2.1.0');
+    }
 };
