862a0a12aeb8087ad54b9017d4081945ee93cedb
Tadhg Boyle <tadhgsmboyle@gmail.com>
2023-03-11 06:43:33 -0800

    Purify

--- core/templates/frontend_init.php
+++ core/templates/frontend_init.php
@@ -110,8 +110,8 @@ if (!defined('PAGE_DESCRIPTION')) {
     if ($page_metadata->count()) {
         $page_metadata = $page_metadata->first();
         $smarty->assign([
-            'PAGE_DESCRIPTION' => str_replace('{site}', Output::getClean(SITE_NAME), $page_metadata->description),
-            'PAGE_KEYWORDS' => $page_metadata->tags
+            'PAGE_DESCRIPTION' => str_replace('{site}', Output::getClean(SITE_NAME), Output::getPurified($page_metadata->description)),
+            'PAGE_KEYWORDS' => Output::getPurified($page_metadata->tags),
         ]);
 
         $og_image = $page_metadata->image;
@@ -121,8 +121,8 @@ if (!defined('PAGE_DESCRIPTION')) {
     }
 } else {
     $smarty->assign([
-        'PAGE_DESCRIPTION' => str_replace('{site}', Output::getClean(SITE_NAME), PAGE_DESCRIPTION),
-        'PAGE_KEYWORDS' => (defined('PAGE_KEYWORDS') ? PAGE_KEYWORDS : '')
+        'PAGE_DESCRIPTION' => str_replace('{site}', Output::getClean(SITE_NAME), Output::getPurified(PAGE_DESCRIPTION)),
+        'PAGE_KEYWORDS' => (defined('PAGE_KEYWORDS') ? Output::getPurified(PAGE_KEYWORDS) : '')
     ]);
 }
 
