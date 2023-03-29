d5ba4288e8a72a76289602431e50eb00045011d8
PadowYT2 <me@padow.ru>
2023-03-29 14:50:50 +0300

    :tada: Implement dark mode for the installer
    Co-Authored-By: PikaMug <2267126+pikamug@users.noreply.github.com>

--- core/installation/includes/footer.php
+++ core/installation/includes/footer.php
@@ -12,6 +12,22 @@
                 </h4>
             </div>
             <div class="eight wide right aligned column">
+                <span class="item" id="darkmode">
+                    <input type="checkbox" class="darkmode-toggle" id="dark_mode" onclick="switchTheme()" value="0">
+                        <label for="dark_mode" class="darkmode-toggle-label">
+                            <i class="moon icon"></i>
+                            <i class="sun icon"></i>
+                            <div class="darkmode-ball"></div>
+                        </label>
+
+                        <script type="text/javascript">
+                        if (document.body.classList.contains('dark')) {
+                            document.getElementById("dark_mode").checked = true;
+                        } else {
+                            document.getElementById("dark_mode").checked = false;
+                        }
+                    </script>
+                </span>
                 <div class="ui inverted basic labeled scrolling dropdown icon button">
                     <i class="world icon"></i>
                     <span class="text"><?php echo Language::LANGUAGES[$installer_language]['name']; ?></span>
@@ -32,8 +48,39 @@
 </div>
 
 <script src="core/assets/vendor/jquery/dist/jquery.min.js"></script>
+<script src="core/assets/vendor/jquery.cookie/jquery.cookie.js"></script>
 <script src="core/assets/vendor/fomantic-ui/dist/semantic.min.js"></script>
 <script>
+    // Dark and light theme switch
+    var currentPanelTheme = $.cookie("nmc_panel_theme");
+
+    if (currentPanelTheme == null) {
+        $.cookie("nmc_panel_theme", "light", { path: "/" });
+    } else {
+        if (currentPanelTheme == "dark") {
+            $("body").addClass("dark");
+            if ($("#dark_mode").length) {
+                $("#dark_mode").prop("checked", true);
+            }
+        }
+    }
+
+    // Prevents light flicker on dark mode
+    $("body").addClass("visible");
+
+    if ($("#dark_mode").length) {
+        var changeCheckbox = document.querySelector("#dark_mode");
+        changeCheckbox.onchange = function() {
+            if (currentPanelTheme == "dark") {
+                $.cookie("nmc_panel_theme", "light", { path: "/" });
+            };
+            if (currentPanelTheme == "light") {
+                $.cookie("nmc_panel_theme", "dark", { path: "/" });
+            };
+            location.reload();
+            return false;
+        };
+    }
 
     $('.dropdown').dropdown();
 
--- core/installation/includes/header.php
+++ core/installation/includes/header.php
@@ -28,9 +28,17 @@ foreach (Language::LANGUAGES as $short_code => $meta) {
     <link rel="stylesheet" href="core/assets/vendor/fomantic-ui/dist/semantic.min.css">
 
     <style>
-
         body {
             background: #f3f6fa;
+            display: none;
+        }
+
+        body.dark {
+            background: #222;
+        }
+
+        .visible {
+            display: block;
         }
 
         .wrapper {
@@ -87,6 +95,141 @@ foreach (Language::LANGUAGES as $short_code => $meta) {
             margin-top: 1.5rem;
         }
 
+        body.dark .ui.card,
+        body.dark .ui.segment:not(.inverted),
+        body.dark .step {
+            background-color: #303030 !important;
+            border: 1px solid rgba(0, 0, 0, 0.125);
+            -webkit-box-shadow: none;
+            box-shadow: none;
+        }
+
+        body.dark .ui.card>.content>.header a,
+        body.dark .ui.segment a {
+            color: #fff;
+        }
+        
+        body.dark .ui.card .meta,
+        .ui.cards>.card .meta,
+        body.dark .ui.header,
+        body.dark .ui.header .sub.header,
+        body.dark .ui.card>.content>.header,
+        body.dark .ui.cards>.card>.content>.header,
+        body.dark .ui.card>.content>.description .list .item .text,
+        body.dark .ui.dropdown .menu>.item,
+        body.dark .ui.segment:not(.inverted),
+        body.dark .ui.form .field>label,
+        body.dark .ui.list .list>.item .header,
+        body.dark .ui.list>.item .header,
+        body.dark .ui.dropdown .menu>.header:not(.ui),
+        body.dark .ui.segment .ui.message:not(.small),
+        body.dark .step,
+        body.dark .ui.steps .step.active .icon,
+        body.dark .ui.steps .step.active .content .title {
+            color: rgba(255, 255, 255, 0.6) !important;
+        }
+
+        body.dark .ui.segment .ui.message:not(.small) {
+            background-color: #282828;
+            box-shadow: 0 0 0 1px #282828 inset, 0 0 0 0 transparent;
+            -webkit-box-shadow: 0 0 0 1px #282828 inset, 0 0 0 0 transparent;
+        }
+
+        body.dark .ui.form input:not([type]),
+        body.dark .ui.form input[type=datetime-local],
+        body.dark .ui.form input[type=email],
+        body.dark .ui.form input[type=password],
+        body.dark .ui.form input[type=text] {
+            color: #fff;
+        }
+
+        body.dark .ui.image.label,
+        body.dark .ui.selection.dropdown,
+        body.dark .ui.selection.visible.dropdown>.text:not(.default) {
+            background-color: #282828;
+            color: #fff;
+        }
+
+        body.dark .ui.form .field .ui.selection.active.dropdown .menu {
+            border: 1px solid #282828;
+        }
+
+        body.dark .ui.form input:not(.button) {
+            background-color: #303030 !important;
+        }
+
+        body.dark .ui.menu .dropdown.item .menu,
+        body.dark .ui.dropdown .menu {
+            background-color: #222;
+            border: 1px solid #444;
+            color: rgba(255, 255, 255, 0.6);
+        }
+
+        body.dark .ui.menu .ui.dropdown .menu>.item:hover,
+        body.dark .ui.dropdown .menu>.item:hover,
+        body.dark .ui.menu .ui.dropdown .menu>.item.active,
+        body.dark .ui.list>.item a.header {
+            color: #fff !important;
+        }
+
+        body.dark .ui.form .field .ui.dropdown .menu .item {
+            background-color: #282828;
+            border-top: 1px solid #303030;
+            color: #fff;
+        }
+
+        body.dark .ui.menu .ui.dropdown .menu>.item:hover {
+            background-color: rgba(0, 0, 0, .05) !important;
+        }
+
+        body.dark .ui.button {
+            background-color: #282828;
+            color: rgba(255, 255, 255, 0.6);
+        }
+
+        #darkmode {
+            display: inline-block;
+        }
+
+        .darkmode-toggle {
+            opacity: 0;
+            position: absolute;
+        }
+
+        .darkmode-toggle:checked + .darkmode-toggle-label .darkmode-ball {
+            transform: translateX(24px);
+        }
+
+        .darkmode-toggle-label {
+            background-color: #111;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            border-radius: 50px;
+            position: relative;
+            padding: 5px;
+            height: 26px;
+            width: 50px;
+        }
+
+        .darkmode-ball {
+            background-color: #fff;
+            border-radius: 50%;
+            position: absolute;
+            top: 2px;
+            left: 2px;
+            height: 22px;
+            width: 22px;
+            transition: transform 0.2s linear;
+        }
+
+        .moon.icon {
+            color: #f1c400;
+        }
+
+        .sun.icon {
+            color: #f39c00;
+        }
     </style>
 
 </head>
