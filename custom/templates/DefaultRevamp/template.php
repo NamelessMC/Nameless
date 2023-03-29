946a794bc159158a764cbc0b93974a75e530e81d
PadowYT2 <me@padow.ru>
2023-03-29 23:44:25 +0300

    Use CSS instead of the script to put the footer at the bottom

--- custom/templates/DefaultRevamp/template.php
+++ custom/templates/DefaultRevamp/template.php
@@ -139,7 +139,6 @@ class DefaultRevamp_Template extends TemplateBase {
             $this->_template['path'] . 'js/core/core.js?v=203' => [],
             $this->_template['path'] . 'js/core/user.js' => [],
             $this->_template['path'] . 'js/core/pages.js?v=203' => [],
-            $this->_template['path'] . 'js/scripts.js' => [],
         ]);
 
         foreach ($this->_pages->getAjaxScripts() as $script) {
