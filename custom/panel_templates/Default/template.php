1589b76c65d9adac50fcd8fd38dd0f67594ced04
Sam <samerton@users.noreply.github.com>
2023-03-30 18:42:40 +0100

    Queue system (#3274)
    * Add new queue system
    
    * Amend creation of task from new
    
    * Fix unused terms
    
    * Prevent task returning error from running infinitely
    
    * Run tasks in transactions
    
    * Update gitignore and package-lock
    
    * Use PDO helpers for transaction
    
    * WIP convert forum post task
    
    * Trigger forum post conversion during upgrade
    
    * Fix task scheduling and increase forum posts converted to 50
    
    * Rename ONE_OF to IN

--- custom/panel_templates/Default/template.php
+++ custom/panel_templates/Default/template.php
@@ -40,7 +40,7 @@ if (!class_exists('Default_Panel_Template')) {
             $this->addCSSFiles([
                 (defined('CONFIG_PATH') ? CONFIG_PATH : '') . '/custom/panel_templates/Default/assets/css/sb-admin-2.min.css' => [],
                 'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i' => [],
-                (defined('CONFIG_PATH') ? CONFIG_PATH : '') . '/custom/panel_templates/Default/assets/css/custom.css?v=200' => [],
+                (defined('CONFIG_PATH') ? CONFIG_PATH : '') . '/custom/panel_templates/Default/assets/css/custom.css?v=210' => [],
             ]);
 
             $this->addJSFiles([
