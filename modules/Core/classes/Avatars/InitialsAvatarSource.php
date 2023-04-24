0af25540d82700b9d117e6f1b8ed000e6070ce83
Tadhg Boyle <tadhgsmboyle@gmail.com>
2023-04-24 15:54:27 -0700

    use svg for initials when possible

--- modules/Core/classes/Avatars/InitialsAvatarSource.php
+++ modules/Core/classes/Avatars/InitialsAvatarSource.php
@@ -9,6 +9,10 @@ class InitialsAvatarSource extends AvatarSourceBase {
     }
 
     public function get(User $user): string {
-        return "https://api.dicebear.com/5.x/initials/png?seed={$user->data()->username}&size={$this->_size}";
+        // Use PNG when full URL is enabled, so it displays properly in Discord embeds,
+        // otherwise use SVG for slightly better speeds
+        $type = $this->_full_url ? 'png' : 'svg';
+
+        return "https://api.dicebear.com/5.x/initials/{$type}?seed={$user->data()->username}&size={$this->_size}";
     }
 }
