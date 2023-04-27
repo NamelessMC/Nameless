46ae06a92dfb43e8cb296898bd6af941bf5b9b33
Partydragen <admin@partydragen.com>
2023-04-27 22:05:03 +0200

    Fix phpstan

--- core/classes/Events/Cancellable.php
+++ core/classes/Events/Cancellable.php
@@ -2,19 +2,19 @@
 
 trait Cancellable {
 
-    private bool $cancelled = false;
-    private string $reason = '';
+    private bool $_cancelled = false;
+    private string $_reason = '';
 
     public function setCancelled(bool $cancelled, string $reason = ''): void {
-        $this->cancelled = $cancelled;
-        $this->reason = $reason;
+        $this->_cancelled = $cancelled;
+        $this->_reason = $reason;
     }
 
     public function isCancelled(): bool {
-        return $this->cancelled;
+        return $this->_cancelled;
     }
 
     public function getCancelledReason(): string {
-        return $this->reason;
+        return $this->_reason;
     }
 }
