7ff744d3fa0e4874db4c068b7393b68f56ffff55
Sam <samerton@users.noreply.github.com>
2023-06-18 11:26:25 +0100

    Prepare 1.1.1 release (#3399)

--- modules/Core/module.php
+++ modules/Core/module.php
@@ -2,7 +2,7 @@
 /*
  *  Made by Samerton
  *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.1.0
+ *  NamelessMC version 2.1.1
  *
  *  License: MIT
  *
@@ -21,8 +21,8 @@ class Core_Module extends Module {
 
         $name = 'Core';
         $author = '<a href="https://samerton.me" target="_blank" rel="nofollow noopener">Samerton</a>';
-        $module_version = '2.1.0';
-        $nameless_version = '2.1.0';
+        $module_version = '2.1.1';
+        $nameless_version = '2.1.1';
 
         parent::__construct($this, $name, $author, $module_version, $nameless_version);
 
