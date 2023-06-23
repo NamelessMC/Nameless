ef39f4763d6ea6465b47ed3d0f1fc7721b4dac4b
Partydragen <adrian_kili@outlook.com>
2023-06-24 00:28:19 +0200

    Readd deleted term + fix discord oauth link success message (#3406)
    * Revert deleted language term
    
    Removed in https://github.com/NamelessMC/Nameless/commit/a0e9ffb119a55093a5826147bbeeb9a1f6d02832
    
    @tadhgboyle
    
    * Add success message for linking discord

--- modules/Discord Integration/classes/DiscordIntegration.php
+++ modules/Discord Integration/classes/DiscordIntegration.php
@@ -75,6 +75,7 @@ class DiscordIntegration extends IntegrationBase {
         }, $user->getAllGroupIds()));
 
         Discord::updateDiscordRoles($user, $roles, []);
+        Session::flash('connections_success', $this->_language->get('user', 'integration_linked', ['integration' => Output::getClean($this->_name)]));
     }
 
     public function validateUsername(string $username, int $integration_user_id = 0): bool {
