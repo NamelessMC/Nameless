e244d73a6db1b075ab621c39414902216fea1d27
Robin <robin+git@rkslot.nl>
2023-07-10 18:05:16 +0200

    Rewrite release script to fix checksums in upgrade package (#3414)

--- dev/scripts/release.py
+++ dev/scripts/release.py
@@ -27,11 +27,15 @@ INCLUDE_FILES = [
     'rewrite_test.php',
 ]
 
-def create_archives(archive_name: str, archive_source: str = '.'):
-    archive_path: str = Path('release', archive_name).absolute().as_posix()
+def generate_checksums(cwd):
+    subprocess.check_call(['php', 'dev/scripts/generate_checksums.php'],
+                          shell=False,
+                          cwd=cwd)
 
-    print('Archive: Copy files to temp dir')
-    archive_temp = tempfile.mkdtemp()
+
+def create_archive_dir(archive_source: str = '.') -> str:
+    # Copy all files that should be included to a temporary directory
+    archive_temp = tempfile.mkdtemp(prefix='nameless')
 
     for include_file in INCLUDE_FILES:
         include_path = Path(archive_source, include_file)
@@ -43,11 +47,11 @@ def create_archives(archive_name: str, archive_source: str = '.'):
         if include_path.exists():
             shutil.copytree(include_path, Path(archive_temp, include_dir))
 
-    if Path(archive_source, 'vendor').exists():
-        print('Archive: Generate checksums')
-        subprocess.check_call(['php', 'dev/scripts/generate_checksums.php'],
-                              shell=False,
-                              cwd=archive_temp)
+    return archive_temp
+
+
+def create_archives(archive_name, cwd):
+    archive_path: str = Path('release', archive_name).absolute().as_posix()
 
     print('Archive: Creating .zip file')
     zip_command = [
@@ -57,7 +61,7 @@ def create_archives(archive_name: str, archive_source: str = '.'):
         f'{archive_path}.zip',
         '.',
     ]
-    subprocess.check_call(zip_command, shell=False, cwd=archive_temp)
+    subprocess.check_call(zip_command, shell=False, cwd=cwd)
 
     print('Archive: Creating .tar.xz file')
 
@@ -71,35 +75,40 @@ def create_archives(archive_name: str, archive_source: str = '.'):
         '.',
     ]
 
-    subprocess.check_call(tar_command, shell=False, cwd=archive_temp,
+    subprocess.check_call(tar_command, shell=False, cwd=cwd,
                           env={'XZ_DEFAULTS': '-T 0'})  # Enable multithreading
 
-    shutil.rmtree(archive_temp)
 
-
-if __name__ == '__main__':
-    if not Path('.git').exists():
-        print('.git does not exist')
-        sys.exit(1)
-
-    print('Deleting vendor files')
+def regenerate_vendor_files():
+    print('Re-generating vendor files')
     shutil.rmtree('core/assets/vendor', ignore_errors=True)
-    shutil.rmtree('node_modules', ignore_errors=True)
-    shutil.rmtree('vendor', ignore_errors=True)
-
-    # Create base archive
-    create_archives('nameless-base')
-
-    # Run npm and composer (production dependencies only)
-    subprocess.check_call(['npm', 'ci', '-q', '--cache', '.node_cache'])
     subprocess.check_call(['composer', 'update'])
     subprocess.check_call(['composer', 'install', '--no-dev', '--no-interaction'])
 
-    create_archives('nameless-deps-dist')
 
-    # Create archive with files changed since last update
+def create_deps_dist_archive():
+    print('Creating nameless-deps-dist archive')
+    # Copy the required files to a new temporary directory
+    deps_dist_temp = create_archive_dir()
+    # Generate checksums
+    generate_checksums(deps_dist_temp)
+    # Create .zip and .tar.xz archives
+    create_archives('nameless-deps-dist', deps_dist_temp)
+    # Temporary directory can now be deleted
+    shutil.rmtree(deps_dist_temp)
 
-    upgrade_temp = Path('release', 'upgrade_temp')
+
+def always_in_update_package(relative_path: str) -> bool:
+    return relative_path == 'checksums.json' or relative_path.startswith('vendor/') or relative_path.startswith('core/assets/vendor')
+
+
+def create_upgrade_archive():
+    print('Creating update archive')
+
+    # Copy the required files to a new temporary directory
+    upgrade_temp = create_archive_dir()
+    # Generate checksums
+    generate_checksums(upgrade_temp)
 
     # Find previous tag
     previous_tag_command = ['git', 'describe', '--abbrev=0', '--tags']
@@ -109,19 +118,53 @@ if __name__ == '__main__':
 
     # Find all files changed between previous tag and HEAD (current state)
     changed_command = ['git', 'diff', previous_tag, 'HEAD', '--name-only', '--diff-filter=d']
-    changed_files = subprocess.check_output(changed_command, shell=False)[:-1].decode()
+    changed_files_output = subprocess.check_output(changed_command, shell=False)[:-1]
+    changed_files = set(changed_files_output.decode().split('\n'))
 
-    for changed_file in changed_files.split('\n'):
-        changed_file_target = Path(upgrade_temp, changed_file)
-        changed_file_target.parent.mkdir(parents=True, exist_ok=True)
-        shutil.copy2(changed_file, changed_file_target)
+    # Delete any files that have not been changed
+    for path in Path(upgrade_temp).rglob("*"):
+        relative_path = path.as_posix()[len(upgrade_temp)+1:]
+        if (
+            not always_in_update_package(relative_path) and
+            relative_path not in changed_files and
+            path.is_file()
+        ):
+            path.unlink()
 
-    # Vendor files are always included
-    for vendor_dir in ['vendor', 'core/assets/vendor']:
-        shutil.copytree(vendor_dir, Path(upgrade_temp, vendor_dir))
+    # Delete empty directoryes
+    subprocess.check_call(['find', '.', '-type', 'd', '-empty', '-delete'], cwd=upgrade_temp)
 
-    create_archives('upgrade-from-' + previous_tag, archive_source=upgrade_temp.as_posix())
+    # Create .zip and .tar.xz archives
+    create_archives('upgrade-from-' + previous_tag, upgrade_temp)
+
+
+def create_deps_dev_archive():
+    print('Creating nameless-deps-dev archive')
+    # Copy the required files to a new temporary directory
+    deps_dev_temp = create_archive_dir()
+    # Generate checksums
+    generate_checksums(deps_dev_temp)
+    # Create .zip and .tar.xz archives
+    create_archives('nameless-deps-dev', deps_dev_temp)
+    # Temporary directory can now be deleted
+    shutil.rmtree(deps_dev_temp)
+
+
+if __name__ == '__main__':
+    if not Path('.git').exists():
+        print('.git does not exist')
+        sys.exit(1)
+
+    # Re-generate vendor files (without development dependencies)
+    regenerate_vendor_files()
+
+    # Create nameless-deps-dist archive with production dependencies only
+    create_deps_dist_archive()
+
+    # Create update archive (files changed since last tag)
+    create_upgrade_archive()
 
     # Run composer again, to install development dependencies
     subprocess.check_call(['composer', 'install', '--no-interaction'])
-    create_archives('nameless-deps-dev')
+    # Create nameless-deps-dev archive with development dependencies
+    create_deps_dev_archive()
