1d60fcc150ac8e9b09320ab8fda4867c384c5d3f
Robin <robin+git@rkslot.nl>
2023-09-03 20:21:17 +0200

    Ignore group sync request instead of returning error (#3433)
    * Ignore group sync request instead of returning error
    
    * Fix condition
    
    * Remove return

--- modules/Core/includes/endpoints/UpdateGroupsEndpoint.php
+++ modules/Core/includes/endpoints/UpdateGroupsEndpoint.php
@@ -13,25 +13,28 @@ class UpdateGroupsEndpoint extends KeyAuthEndpoint {
         $api->validateParams($_POST, ['server_id', 'player_groups']);
 
         $server_id = $_POST['server_id'];
-        $group_sync_log = [];
 
-        if (Settings::get('mc_integration') && $server_id == Settings::get('group_sync_mc_server')) {
-            $integration = Integrations::getInstance()->getIntegration('Minecraft');
-
-            foreach ($_POST['player_groups'] as $uuid => $groups) {
-                $integrationUser = new IntegrationUser($integration, str_replace('-', '', $uuid), 'identifier');
-                if ($integrationUser->exists()) {
-                    $log = $this->updateGroups($integrationUser, $groups['groups']);
-                    if (count($log)) {
-                        $group_sync_log[] = $log;
-                    }
-                }
-            }
-
-            $api->returnArray(array_merge(['message' => $api->getLanguage()->get('api', 'groups_updates_successfully')], ['log' => $group_sync_log]));
+        if (!Settings::get('mc_integration') || $server_id != Settings::get('group_sync_mc_server')) {
+            $api->returnArray(['message' => $api->getLanguage()->get('api', 'groups_updates_ignored')]);
         }
 
-        $api->throwError(CoreApiErrors::ERROR_INVALID_SERVER_ID, $server_id);
+        $group_sync_log = [];
+        $integration = Integrations::getInstance()->getIntegration('Minecraft');
+
+        foreach ($_POST['player_groups'] as $uuid => $groups) {
+            $integrationUser = new IntegrationUser($integration, str_replace('-', '', $uuid), 'identifier');
+            if ($integrationUser->exists()) {
+                $log = $this->updateGroups($integrationUser, $groups['groups']);
+                if (count($log)) {
+                    $group_sync_log[] = $log;
+                }
+            }
+        }
+
+        $api->returnArray([
+            'message' => $api->getLanguage()->get('api', 'groups_updates_successfully'),
+            'log' => $group_sync_log,
+        ]);
     }
 
     private function updateGroups(IntegrationUser $integrationUser, array $groups): array {
