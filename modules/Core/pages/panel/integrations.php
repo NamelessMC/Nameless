ab536eecf870dd469d885dbd5a8d376a3ae6a4e0
partydragen <admin@partydragen.com>
2024-07-17 15:31:25 +0200

    Set OAuth Credentials before enabling

--- modules/Core/pages/panel/integrations.php
+++ modules/Core/pages/panel/integrations.php
@@ -90,12 +90,13 @@ if (!isset($_GET['integration'])) {
 
                 $client_id = Input::get("client-id");
                 $client_secret = Input::get("client-secret");
+
+                NamelessOAuth::getInstance()->setCredentials($provider_name, $client_id, $client_secret);
                 if ($client_id && $client_secret) {
                     NamelessOAuth::getInstance()->setEnabled($provider_name, Input::get("enable") == 'on' ? 1 : 0);
                 } else {
                     NamelessOAuth::getInstance()->setEnabled($provider_name, 0);
                 }
-                NamelessOAuth::getInstance()->setCredentials($provider_name, $client_id, $client_secret);
 
                 Session::flash('integrations_success', $language->get('admin', 'integration_updated_successfully'));
                 Redirect::to(URL::build('/panel/core/integrations/', 'integration=' . $integration->getName()));
