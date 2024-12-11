c007eb0708f04b77a8a1246a8005d20ac1985009
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2024-12-11 20:41:37 +0100

    Remove seemingly useless sb-admin code
    It looks like this code causes the sidebar to immediately collapse on small screen sizes upon scrolling.

--- custom/panel_templates/Default/assets/js/sb-admin-2.js
+++ custom/panel_templates/Default/assets/js/sb-admin-2.js
@@ -7,20 +7,6 @@
 
 $('[data-toggle="tooltip"]').tooltip({
         trigger: 'hover'
-    })
+})
 
-    !(function (s) {
-        "use strict";
-        s(window).resize(function () {
-                s(window).width() < 768 && s(".sidebar .collapse").collapse("hide"),
-                    s(window).width() < 480 && !s(".sidebar").hasClass("toggled") && (s("body").addClass("sidebar-toggled"), s(".sidebar").addClass("toggled"), s(".sidebar .collapse").collapse("hide"));
-            }),
-            s("body.fixed-nav .sidebar").on("mousewheel DOMMouseScroll wheel", function (e) {
-                if (768 < s(window).width()) {
-                    var o = e.originalEvent,
-                        l = o.wheelDelta || -o.detail;
-                    (this.scrollTop += 30 * (l < 0 ? 1 : -1)), e.preventDefault();
-                }
-            });
-    })(jQuery);
 // @license-end
