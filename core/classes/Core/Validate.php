a9b95745e82f68ffddf4d8ada3ae373d5b34c51b
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2024-12-14 16:54:18 +0100

    Fix users being able to bypass email verification

--- core/classes/Core/Validate.php
+++ core/classes/Core/Validate.php
@@ -299,9 +299,9 @@ class Validate
                         break;
 
                     case self::IS_ACTIVE:
-                        $check = $validator->_db->get('users', [$item, $value]);
+                        $check = $validator->_db->query("SELECT * FROM nl2_users WHERE username = ? OR email = ?", [$value, $value]);
                         if (!$check->count()) {
-                            break;
+                            break; // Something went pretty fricking wrong but okay
                         }
 
                         $isuseractive = $check->first()->active;
