694ca300570541b80920076fae9a41cc945dec96
Sam <samerton@users.noreply.github.com>
2025-03-09 16:09:40 +0000

    feat: provide fake smarty instance for panel compatibility

--- core/templates/backend_init.php
+++ core/templates/backend_init.php
@@ -45,3 +45,6 @@ $template->getEngine()->addVariables([
 
 // Initialise widgets
 $widgets = new Widgets($cache, $language, $template);
+
+// TODO: remove in 2.3.0
+$smarty = new FakeSmarty($template->getEngine());
