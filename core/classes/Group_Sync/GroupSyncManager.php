a17166af97700f23f52f58b542021290096ee700
tadhgboyle <tadhgsmboyle@gmail.com>
2025-05-01 15:09:59 -0700

    revert group sync manager for time being

--- core/classes/Group_Sync/GroupSyncManager.php
+++ core/classes/Group_Sync/GroupSyncManager.php
@@ -147,6 +147,148 @@ final class GroupSyncManager extends Instanceable
     }
 
     /**
+     * Execute respective `addGroup()` or `removeGroup()` function on each of the injectors
+     * synced to the changed group.
+     *
+     * @param User   $user                   NamelessMC user to apply changes to
+     * @param string $sending_injector_class Class name of injector broadcasting this change
+     * @param array  $group_ids              Array of Group IDs native to the sending injector which were added/removed to the user
+     *
+     * @deprecated broadcastGroupChange should be used instead. This function has issues, such as that on the first sync it will replace
+     *             groups on one side with groups from the other side, depending on which side happens to sync first. On the first sync,
+     *             the desired behaviour is for the new roles on both sides to become the union of roles on both sides.
+     *
+     * @return array Array of logs of changed groups
+     */
+    public function broadcastChange(User $user, string $sending_injector_class, array $group_ids): array
+    {
+        $sending_injector = $this->getInjectorByClass($sending_injector_class);
+
+        if ($sending_injector === null) {
+            return [];
+        }
+
+        $logs = [];
+
+        $modified = [];
+
+        $namelessmc_injector = $this->getInjectorByClass(NamelessMCGroupSyncInjector::class);
+        $namelessmc_column = $namelessmc_injector->getColumnName();
+
+        // Get all group sync rules where this injector is not null
+        $rules = DB::getInstance()->query("SELECT * FROM nl2_group_sync WHERE {$sending_injector->getColumnName()} IS NOT NULL")->results();
+        foreach ($rules as $rule) {
+            if ($rule->website_group_id == PRE_VALIDATED_DEFAULT) {
+                // Require atleast 1 group if default group is synced
+                if (count($group_ids) === 0) {
+                    return [];
+                }
+
+                break;
+            }
+        }
+
+        $batched_changes = [];
+        foreach ($rules as $rule) {
+            foreach ($this->getEnabledInjectors() as $injector) {
+                if ($injector == $sending_injector) {
+                    continue;
+                }
+
+                $injector_class = get_class($injector);
+
+                $batchable = $injector instanceof BatchableGroupSyncInjector;
+                if ($batchable && !array_key_exists($injector_class, $batched_changes)) {
+                    $batched_changes[$injector_class] = [
+                        'add' => [],
+                        'remove' => [],
+                    ];
+                }
+
+                $injector_column = $injector->getColumnName();
+                $injector_group_id = $rule->{$injector_column};
+                $sending_group_id = $rule->{$sending_injector->getColumnName()};
+
+                // Skip this injector if it doesn't have a group id setup for this rule
+                if ($injector_group_id === null) {
+                    continue;
+                }
+
+                if (!isset($modified[$injector_column])) {
+                    $modified[$injector_column] = [];
+                }
+
+                // Skip this specific injector for this rule if we have already modified the user
+                // with the same injector group id
+                if (in_array($injector_group_id, $modified[$injector_column])) {
+                    continue;
+                }
+
+                if (in_array($sending_group_id, $group_ids)) {
+                    // Attempt to add group if this group id was sent in the broadcastChange() method
+                    // and if they don't have the namelessmc equivilant of it
+                    $modified[$injector_column][] = $injector_group_id;
+                    if ($batchable) {
+                        $batched_changes[$injector_class]['add'][] = $injector_group_id;
+                    } elseif ($injector->addGroup($user, $injector_group_id)) {
+                        $logs['added'][] = "{$injector_column} -> {$injector_group_id}";
+                    }
+                } else {
+                    foreach ($rules as $item) {
+                        if (in_array($item->{$sending_injector->getColumnName()}, $group_ids)) {
+                            if ($item->{$namelessmc_column} == $rule->{$namelessmc_column}) {
+                                continue 2;
+                            }
+                        }
+                    }
+
+                    // Attempt to remove this group if it doesn't have multiple rules, or if the group ids
+                    // list sent to broadcastChange() was empty - NOT both
+                    $modified[$injector_column][] = $injector_group_id;
+                    if ($batchable) {
+                        $batched_changes[$injector_class]['remove'][] = $injector_group_id;
+                    } elseif ($injector->removeGroup($user, $injector_group_id)) {
+                        $logs['removed'][] = "{$injector_column} -> {$injector_group_id}";
+                    }
+                }
+            }
+        }
+
+        foreach ($batched_changes as $injector_class => $data) {
+            /** @var GroupSyncInjector&BatchableGroupSyncInjector $injector */
+            $injector = $this->getInjectorByClass($injector_class);
+            $injector_column = $injector->getColumnName();
+
+            $add = $data['add'];
+            $remove = $data['remove'];
+
+            if (count($add)) {
+                $result = $injector->batchAddGroups($user, $add);
+                if (is_array($result)) {
+                    foreach ($result as $res) {
+                        if ($res['status'] === 'added') {
+                            $logs['added'][] = "{$injector_column} -> {$res['group_id']}";
+                        }
+                    }
+                }
+            }
+
+            if (count($remove)) {
+                $result = $injector->batchRemoveGroups($user, $remove);
+                if (is_array($result)) {
+                    foreach ($result as $res) {
+                        if ($res['status'] === 'removed') {
+                            $logs['removed'][] = "{$injector_column} -> {$res['group_id']}";
+                        }
+                    }
+                }
+            }
+        }
+
+        return $logs;
+    }
+
+    /**
      * Execute respective `addGroup()` or `removeGroup()` function on each of the injectors (e.g. Nameless itself, Minecraft, Discord)
      * synced to the changed group.
      *
