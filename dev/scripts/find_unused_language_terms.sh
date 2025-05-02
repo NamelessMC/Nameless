ae54c7a37bccdf1a08414f76437ac294686ec078
tadhgboyle <tadhgsmboyle@gmail.com>
2025-05-02 12:12:26 -0700

    add back user tag info

--- dev/scripts/find_unused_language_terms.sh
+++ dev/scripts/find_unused_language_terms.sh
@@ -20,6 +20,7 @@ WHITELISTED_TERMS=(
   "installer/module_forum_description"
   "installer/module_core_description"
   "installer/module_members_description"
+  "user/user_tag_info"
 )
 
 for FILE in "${FILES[@]}"
