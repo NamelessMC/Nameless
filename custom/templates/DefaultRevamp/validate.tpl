e41794db5561bd752100e2c2512f58297a182e2a
partydragen <admin@partydragen.com>
2025-05-04 18:28:00 +0200

    Add ability to change email during validation

--- custom/templates/DefaultRevamp/validate.tpl
+++ custom/templates/DefaultRevamp/validate.tpl
@@ -12,8 +12,41 @@
                 <div class="ui positive message">
                     {$VALIDATE_EMAIL_INFO}
                 </div>
+
+                <div class="ui center aligned">
+                    <form class="ui form" action="" method="post" id="form-sponsor">
+                        <a class="ui primary button" onClick="$('#changeEmailModal').modal('show');">{$CHANGE_EMAIL}</a>
+
+                        <input type="hidden" name="token" value="{$TOKEN}">
+                        <input type="hidden" name="type" value="paypal">
+                        <input type="submit" class="ui primary button" value="Resend Email" name="single" />
+                    </form>
+                </div>
             </div>
         </div>
+    </div>
+</div>
+
+<!-- Change email modal -->
+<div class="ui small modal" id="changeEmailModal">
+    <div class="header">
+        {$CHANGE_EMAIL}
+    </div>
+    <div class="content">
+        <form class="ui form" action="" method="post" id="validate-change-email">
+            <div class="field">
+                <label for="inputEmail">{$EMAIL_ADDRESS}</label>
+                <input type="email" name="email" id="inputEmail" value="{$EMAIL_ADDRESS_VALUE}">
+            </div>
+            <div class="field">
+                <input type="hidden" name="token" value="{$TOKEN}">
+            </div>
+        </form>
+
+    </div>
+    <div class="actions">
+        <a class="ui negative button">{$CANCEL}</a>
+        <a class="ui positive button" onclick="$('#validate-change-email').submit();">{$CHANGE_EMAIL}</a>
     </div>
 </div>
 
--- modules/Core/pages/validate.php
+++ modules/Core/pages/validate.php
@@ -47,11 +47,68 @@ if (isset($_GET['c'])) {
 
     Redirect::to(URL::build('/'));
 } else if (Session::exists('validate_email')) {
+    $email_address = Session::get('validate_email');
+
+    // Handle input
+    if (Input::exists()) {
+        if (Token::check()) {
+
+            $target_user = new User($email_address, 'email');
+            if (Input::get('action') == 'change_email') {
+                // Change email
+                $validation = Validate::check($_POST, [
+                    'email' => [
+                        Validate::REQUIRED => true,
+                        Validate::EMAIL => true,
+                        Validate::UNIQUE => ['users', 'id:' . $target_user->data()->id],
+                    ]
+                ])->messages([
+                    'email' => [
+                        Validate::REQUIRED => $language->get('user', 'email_required'),
+                        Validate::EMAIL => $language->get('general', 'contact_message_email'),
+                        Validate::UNIQUE => $language->get('user', 'email_already_exists'),
+                    ]
+                ]);
+
+                if ($validation->passed()) {
+                    $target_user->update([
+                        'email' => Input::get('email')
+                    ]);
+
+                    // Generate validation code
+                    $code = SecureRandom::alphanumeric();
+
+                    if (Core_Emails::sendRegisterEmail($language, Input::get('email'), $target_user->data()->username, $target_user->data()->id, $code)) {
+                        Session::flash('validate_success', $language->get('admin', 'email_resent_successfully'));
+                    } else {
+                        Session::flash('validate_error', $language->get('admin', 'email_resend_failed'));
+                    }
+
+                    Session::put('validate_email', Output::getClean(Input::get('email')));
+                    Redirect::to(URL::build('/validate'));
+                } else {
+                    $errors = $validation->errors();
+                }
+
+            } else if (Input::get('action') == 'resend_email') {
+                // Resend email
+            }
+        } else {
+            // Invalid form token
+            Session::flash('settings_error', $language->get('general', 'invalid_token'));
+        }
+    }
+
     $template->getEngine()->addVariables([
         'VALIDATE_EMAIL' => $language->get('user', 'validate_email'),
         'VALIDATE_EMAIL_INFO' => $language->get('user', 'validate_email_info', [
             'email' => Output::getClean(Session::get('validate_email'))
         ]),
+
+        'CHANGE_EMAIL' => $language->get('user', 'change_email_address'),
+        'CANCEL' => $language->get('general', 'cancel'),
+        'EMAIL_ADDRESS' => $language->get('user', 'email_address'),
+        'EMAIL_ADDRESS_VALUE' => Output::getClean($email_address),
     ]);
 } else {
     Redirect::to(URL::build('/'));
