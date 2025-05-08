9c7928d1cc4eee128e37835e78d63e3e2b1f9cfe
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2025-05-08 12:36:11 +0300

    Fix styleci

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if (defined('DEBUGGING') && DEBUGGING) {
 } else {
     ini_set('display_startup_errors', 0);
     ini_set('display_errors', 0);
-    error_reporting(NULL);
+    error_reporting(null);
 }
 
 // Ensure PHP version >= 8.2
