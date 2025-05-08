ac56acd7df04ffc6de97993d055e82a10449e7a4
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2025-05-08 12:33:20 +0300

    Make sure error reporting is disabled by default

--- index.php
+++ index.php
@@ -23,6 +23,10 @@ if (defined('DEBUGGING') && DEBUGGING) {
     ini_set('display_startup_errors', 1);
     ini_set('display_errors', 1);
     error_reporting(-1);
+} else {
+    ini_set('display_startup_errors', 0);
+    ini_set('display_errors', 0);
+    error_reporting(NULL);
 }
 
 // Ensure PHP version >= 8.2
