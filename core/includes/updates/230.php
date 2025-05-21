6595d0961f80101a65cb151a11ab736ffb1f2f40
tadhgboyle <tadhgsmboyle@gmail.com>
2025-05-21 16:28:18 -0700

    Update Discord widget config to use Settings system rather than Cache

--- core/includes/updates/230.php
+++ core/includes/updates/230.php
@@ -7,6 +7,12 @@ return new class() extends UpgradeScript {
 
         ConvertProfilePosts::schedule();
 
+        // Convert social_media to use settings table
+        $this->_cache->setCache('social_media');
+        $discord_widget_theme = $this->_cache->retrieve('discord_widget_theme') ?: 'dark';
+        Settings::set('discord_widget_theme', $discord_widget_theme, 'Discord Integration');
+        $this->_cache->eraseAll();
+
         $this->setVersion('2.3.0');
     }
 };
