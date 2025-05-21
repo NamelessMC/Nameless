89d8742bb61abc4e96b9c333307d0ed18e919d4f
tadhgboyle <tadhgsmboyle@gmail.com>
2025-05-21 16:40:04 -0700

    Update Online Users widget config to use Settings system rather than Cache

--- core/includes/updates/230.php
+++ core/includes/updates/230.php
@@ -7,6 +7,14 @@ return new class() extends UpgradeScript {
 
         ConvertProfilePosts::schedule();
 
+        // Convert OnlineUsersWidget to use settings table
+        $this->_cache->setCache('online_members');
+        $use_nickname_show = $this->_cache->fetch('show_nickname_instead', 0);
+        $include_staff = $this->_cache->fetch('include_staff_in_users', 0);
+        Settings::set('online_users_widget_use_nicknames', $use_nickname_show);
+        Settings::set('online_users_widget_include_staff', $include_staff);
+        $this->_cache->eraseAll();
+
         $this->setVersion('2.3.0');
     }
 };
